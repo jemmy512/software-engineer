110d44e67fbdbd8655e3dbbff0a150b9a5dce5cd
JemmyWang <jemmy1029760725@gmail.com>
2017-05-25 15:11:17 +0800

    Update c++派生类数据初始化.cpp

--- C++/OOP/c++程序/c++派生类数据初始化/c++派生类数据初始化.cpp
+++ C++/OOP/c++程序/c++派生类数据初始化/c++派生类数据初始化.cpp
@@ -2,42 +2,42 @@
 using namespace std;
 class A{
 private:
-	int x,y;
+	int x, y;
 public:
-	A(int x=0,int y=0)
+	A(int x = 0, int y = 0)
 	{
 		this->x=x;
 		this->y=y;
 	}
 	void out()
 	{
-		cout<<"x="<<x<<endl;
-		cout<<"y="<<y<<endl;
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
 	}
 };
-class B:public A{
+class B : public A{
 	private:
 		int z;
 		A *pa; 
 	public:
-		B(int i):A(i-4,i-2)
+		B(int i) : A(i-4,i-2)
 		{
-			pa=new A();
-			z=i;
+			pa = new A();
+			z = i;
 		}
 		B()
 		{
-			z=0;
+			z = 0;
 		}
 		void out()
 		{
 			A::out();
-			cout<<"z="<<z<<endl;
+			cout << "z = " << z << endl;
 			pa->out();
 		}
 };
 int main(void)
 {
-	B *pb1=new B(7);
+	B *pb1 = new B(7);
 	pb1->out();
 }
