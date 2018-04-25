7d42130e2231bdcf5dd2462650ab585318ee0457
Jemmy <jemmy1029760725@gmail.com>
2018-04-25 21:33:54 +0800

    leetcode: 198, 204, 234, 278

---
+++ Algorithm/LeetCode/198.HouseRobber_[AAA].cc
@@ -0,0 +1,86 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house 
+has a certain amount of money stashed, the only constraint stopping you from robbing
+ each of them is that adjacent houses have security system connected and it will automatically
+ contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Dynamic Programming
+
+We can easy find the recurive fomulary:
+
+    dp[n] = max( 
+        dp[n-1],            // the previous house has been robbed. 
+        dp[n-2] + money[n]  // the previous house has NOT been robbed.
+    )
+                 
+The initalization is obvious:
+    dp[1] = money[1]
+    dp[2] = max(money[1], money[2])
+
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+/******************************************************************************/
+// 69 test 2ms, beat 100%
+int rob1(vector<int> &money) {
+
+    int n = money.size();
+    if (n ==0 ) return 0;
+
+    vector<int> dp(n, 0);
+    if (n >= 1) dp[0] = money[0];
+    if (n >= 2) dp[1] = max(money[0], money[1]);
+
+    for (int i = 2; i < n; ++i){
+        dp[i] = max(dp[i-1], dp[i-2] + money[i]);
+    }
+    return dp[n-1];
+}
+/*
+ * Acutally, we no need to allocate an additional array for DP.
+ * we can only use several variables to record previous steps
+ */
+
+int rob2(vector<int> &money) {
+    int n2 = 0; // dp[i-2];
+    int n1 = 0; // dp[i-1];
+
+    for (int i = 0; i < money.size(); ++i){
+        int current = max(n1, n2 + money[i]);
+        n2 = n1;
+        n1 = current;
+    }
+    return n1;
+}
+
+/*
+    Combination usage of rob1 and robs will beat 100%,
+    but single usage of rob1 or rob2 just beat 73.93%
+*/
+int rob(vector<int> &num) {
+    if (rand()%2)
+        return rob1(num);
+    return rob2(num);
+}
+
+/******************************************************************************/
+// 69 test cases, 3ms, beat 73.93% 
+int rob(vector<int>& num) {
+    int n = num.size();
+    int a = 0;
+    int b = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == 0)
+            a = max(a + num[i], b);
+        } else {
+            b = max(a, b + num[i]);
+        }
+    }
+
+    return max(a, b);
+}
---
+++ Algorithm/LeetCode/234.PalindromeLinkedList_[AA].cc
@@ -0,0 +1,64 @@
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode *findMiddle(ListNode *head) {
+    ListNode *m = head, *n = head;
+    while (m && m->next) {
+        n = n->next;
+        m = m->next->next;
+    }
+    return n;
+}
+
+ListNode *reverseList(ListNode *head) {
+    ListNode *p = NULL;
+    ListNode *m = NULL;
+    while (head) {
+       m = head->next;
+       head->next = p;
+       p = head;
+       head = m;
+    }
+    return p;
+}
+
+// 26 test cases, 20ms, beat 96.66%
+bool isPalindrome(ListNode* head) {
+    ListNode *h, *p, *middle = findMiddle(head);
+    ListNode *q, *right = reverseList(middle);
+    // head != middle not head != right [head has one more node than right]
+    for (; head != middle; head = head->next, right = right->next) {
+        if (head->val != right->val)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    ListNode a(1);
+    ListNode b(0);
+    ListNode c(0);
+    ListNode d(1);
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = NULL;
+    
+    cout << isPalindrome(&a) << endl;
+}
+
