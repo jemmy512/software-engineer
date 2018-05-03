c290541847e7fb78ede00ec478c397330128aa27
Jemmy <jemmy1029760725@gmail.com>
2018-05-03 17:59:05 +0800

    Summary of Bit-Wise Operation

--- Algorithm/LeetCode/136.SingleNumber.cc
+++ Algorithm/LeetCode/136.SingleNumber.cc
@@ -6,11 +6,30 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 
 Tips:
     The same numbers XOR together will be 0
+    
+Reference:
+    https://www.lijinma.com/blog/2014/05/29/amazing-xor/
+    https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary:-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
+    
+Bit-Wise Operation:
+    Set union A | B
+    Set intersection A & B
+    Set subtraction A & ~B
+    Set negation ALL_BITS ^ A or ~A
+    Set bit A |= 1 << bit
+    Clear bit A &= ~(1 << bit)
+    Test bit (A & 1 << bit) != 0
+    Extract last bit A&-A or A&~(A-1) or x^(x&(x-1))
+    Remove last bit A&(A-1)
+    Get all 1-bits ~0
+    
+    
 */
 #include<iostream>
 #include<vector>
 #include<numeric>    // accumulate
 #include<functional> // bit_xor
+#include<assert.h>
 using namespace std;
 
 // 14ms, beat 85.23%
@@ -28,7 +47,41 @@ int singleNumber_1(vector<int>& nums) {
     return ret;
 }
 
+/******************** there are two single nums ******************************/ 
+// get the location of the first 1 in the lower bits of num
+int getFirstOneBit(int num) { 
+    return  num & ~(num - 1);
+}
+
+void singleNumberTwo(vector<int> nums) {
+    int aXORb = 0;
+    int a = 0;
+    int b = 0;
+    int firstOneBit = 0;
+    int len = nums.size();
+    
+    for (int i = 0; i < len; ++i) {
+        aXORb ^= nums[i];
+    }
+    assert(aXORb != 0);
+    firstOneBit = getFirstOneBit(aXORb);
+    for (int i = 0; i < len; ++i) {
+        if (nums[i] & firstOneBit) {
+            a ^= nums[i];
+        }
+    }
+    b = aXORb & a;
+    cout << a << " : " << b << endl;
+}
+
+/******************** there are three single nums *****************************/ 
+
+int singleNumberThree(vector<int> nums) {
+    
+}
+
 int main() {
-    vector<int> vec = {1, 2, 6, 2, 1};
-    cout << singleNumber(vec) << endl;
+    // vector<int> vec = {1, 2, 6, 2, 1};
+    // cout << singleNumber(vec) << endl;
+    cout << (1 | 2) << endl;
 }
