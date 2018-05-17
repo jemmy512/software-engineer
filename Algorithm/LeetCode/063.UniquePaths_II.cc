2ad24b5b9dd7cb7de0a32659d594e3976a411202
Jemmy <jemmy1029760725@gmail.com>
2018-05-17 18:01:19 +0800

    leetcode: 25, 62, 63

---
+++ Algorithm/LeetCode/063.UniquePaths_II.cc
@@ -0,0 +1,64 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to 
+reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+        start                                          
+        +---------+----+----+----+----+----+                   
+        |----|    |    |    |    |    |    |                   
+        |----|    |    |    |    |    |    |                   
+        +----------------------------------+                   
+        |    |    |    |    |    |    |    |                   
+        |    |    |    |    |    |    |    |                   
+        +----------------------------------+                   
+        |    |    |    |    |    |    |----|                   
+        |    |    |    |    |    |    |----|                   
+        +----+----+----+----+----+---------+                   
+                                    finish 
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> vec(n, 1);
+        
+        for (int i = 0; i < m; ++i) {
+            if (obstacleGrid[i][0])
+                vec[0] = 0;
+            for (int j = 1; j < n; ++j) {
+                if (obstacleGrid[i][j]) {
+                    if (i == 0 && !vec[0]) {
+                        while (j < n) vec[j++] = 0;
+                    } 
+                    vec[j] = 0;                    
+                } else 
+                    vec[j] += vec[j-1];
+            }
+        }
+        
+        return vec.back();
+    }
+};
