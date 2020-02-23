74faa749f4e0c339eed8e58064a9eccfec13da6f
Jemmy <jemmy1029760725@gmail.com>
2020-02-23 21:07:06 +0800

    list head

---
+++ DataStruct/LinkedList/List.h
@@ -0,0 +1,126 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+};
+
+Node* createListHeadInsert(int* args, int size) {
+    Node* head = nullptr;
+    if (args == nullptr ||  size < 0)
+        return head;
+
+    for (; size > 0; --size) {
+        Node* node = new Node{args[size-1], head};
+        head = node;
+    }
+
+    return head;
+}
+
+Node* createListTailInsert(int* args, int size) {
+    if (args == nullptr || size < 0)
+        return nullptr;
+
+    Node *beg = nullptr, *end = nullptr;
+    for (int idx = 0; idx < size; ++idx) {
+        Node* node = new Node{args[idx], nullptr};
+
+        if (beg) {
+            end->next = node;
+            end = end->next;
+        } else {
+            beg = end = node;
+        }
+    }
+
+    return beg;
+}
+
+void releaseList(Node* node) {
+    while (node) {
+        Node* cur = node;
+        node = node->next;
+
+        delete cur;
+    }
+}
+
+Node* deleteNode(Node* node, int val) {
+    Node *beg = node, *end = nullptr;
+    while (beg != nullptr && beg->val != val) {
+        end = beg;
+        beg = beg->next;
+    }
+
+    if (beg == node) {
+        node = beg->next;
+    } else if (beg != nullptr) {
+        end->next = beg->next;
+    } else { 
+        // not found
+    }
+
+    if (beg) {
+        delete beg;
+    }
+
+    return node;
+}
+
+
+void printList(Node* node) {
+    while (node) {
+        std::cout << node->val;
+        if (node->next)
+            std::cout << " -> ";
+
+        node = node->next;
+    }
+
+    std::cout << endl;
+}
+
+/*
+int main() {
+	int node1[1]{1};
+	int node2[2]{1, 2};
+	int node3[3]{1, 2, 3};
+	int node4[4]{1, 2, 3, 4};
+	int node5[5]{1, 2, 3, 4, 5};
+
+	Node* begOne = createListHeadInsert(node1, 1);
+	Node* begTwo = createListHeadInsert(node2, 2);
+	Node* begThree = createListHeadInsert(node3, 3);
+	Node* begFour = createListHeadInsert(node4, 4);
+	Node* begFive = createListHeadInsert(node5, 5);
+
+    printList(begOne);
+    printList(begTwo);
+    printList(begThree);
+    printList(begFour);
+    printList(begFive);
+
+	Node* one = deleteNode(begOne, 1);
+	Node* two = deleteNode(begTwo, 1);
+	Node* three = deleteNode(begThree, 2);
+	Node* four = deleteNode(begFour,3 );
+	Node* five = deleteNode(begFive, 6);
+
+
+    printList(one);
+    printList(two);
+    printList(three);
+    printList(four);
+    printList(five);
+
+	releaseList(one);
+	releaseList(two);
+	releaseList(three);
+	releaseList(four);
+	releaseList(five);
+
+	return 0;
+}
+*/
