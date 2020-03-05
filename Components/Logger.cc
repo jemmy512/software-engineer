7e3e6547f0609c3e610564afe35a8cfdef02d29c
Jemmy <jemmy1029760725@gmail.com>
2020-03-06 00:42:39 +0800

    Update Logger.cc

--- Components/Logger.cc
+++ Components/Logger.cc
@@ -1,33 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <memory>
+#include <thread>
 
-class Logger  {
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+class IThreadPool;
+
+namespace TimeUtil {
+    std::string getCurrentTime() {
+        auto timePoint = std::chrono::system_clock::now();
+        auto timeT = std::chrono::system_clock::to_time_t(timePoint);
+
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&timeT), "%Y-%m-%d %H:%M:%S");
+
+        return ss.str();
+    }
+}
+
+class Logger {
 public:
-    using Ptr = std::shard_ptr<Logger>;
+    using Ptr = std::shared_ptr<Logger>;
 
     virtual void logMessage(const std::string&) = 0;
-    virtual ~Logger() = defualt;
+    virtual void finalize() = 0;
+    virtual ~Logger() = default;
+};
+
+enum class LogLevel {
+    INFO,
+    ERROR,
+    DEBUG
+};
+
+struct LogInfo {
+    const LogLevel& level;
+    const std::string& msg;
+    const std::string& file;
+    int line;
+    const std::string function;
 };
 
 
 class LoggerManager {
 public:
-    enum Level {
-        INFO,
-        ERROR,
-        DEBUG
-    }
+    static LoggerManager& instance();
+    void logMessage(LogInfo&& logInfo);
 
-    static LoggerManger& instance();
-    void logMessage(const Level& level, const std::string& msg, const std::string& file, int line, const std::string function);
-
-    void registerLogger(const Logger::Ptr logger);
-    void unregisterLogger(const Logger::Ptr logger);
+    void registerLogger(const Logger::Ptr& logger);
+    void unregisterLogger(const Logger::Ptr& logger);
 
 private:
-    std::string level2String(const Level&);
-    void filterString(std::string& msg, const std::vector<std::string>& pii);
+    std::string level2String(const LogLevel&);
+    void filterString(std::string& msg, const std::vector<std::string>&& pii);
 
 
 private:
@@ -39,7 +68,16 @@ private:
     static constexpr auto PII_INFO = "********";
 };
 
-void LoggerManager::filterString(std::string& msg, cosnt std::vector<std::string>& pii) {
+std::string LoggerManager::level2String(const LogLevel& level) {
+    switch (level) {
+        case LogLevel::INFO:  return "INFO";  break;
+        case LogLevel::ERROR: return "ERROR"; break;
+        case LogLevel::DEBUG: return "DEBUG"; break;
+        default:    return "";
+    }
+}
+
+void LoggerManager::filterString(std::string& msg, const std::vector<std::string>&& pii) {
     for (const auto& item : pii) {
         auto ite = msg.find(item);
         while (ite != std::string::npos) {
@@ -50,41 +88,32 @@ void LoggerManager::filterString(std::string& msg, cosnt std::vector<std::string
     }
 }
 
-std::string LoggerManagaer::level2String(const Level& level) {
-    switch (level) {
-        case INFO:  return "INFO";  break;
-        case ERROR: return "ERROR"; break;
-        case DEBUG: return "DEBUG"; break;
-        default:    return "";
-    }
-}
-
 LoggerManager& LoggerManager::instance() {
-    static std::shared_ptr<LoggerManager> instance = std::make_shared<LoggerManager>();
+    static LoggerManager instance;
 
     return instance;
 }
 
-void LoggerManager::logMessage(const Level& level, const std::string& msg, const std::string& file, int line, const std::string function) {
+void LoggerManager::logMessage(LogInfo&& logInfo) {
     std::stringstream ss;
-    ss << "2020-2-10 14:23:45 " << "<" << level2String(level) << ">" << "[" << std::this_thread::get_id() << "] " << file << " : " << msg;
+    ss << TimeUtil::getCurrentTime() << " <" << level2String(logInfo.level) << ">" << " [" << std::this_thread::get_id() << "] " << logInfo.file << ":" << logInfo.function << " : " << logInfo.msg;
     auto formatedMessage = ss.str();
-    for (const auto& logger : mLogger) {
+    for (const auto& logger : mLoggers) {
         logger->logMessage(formatedMessage);
     }
 }
 
 void LoggerManager::registerLogger(const Logger::Ptr& logger) {
-    if (std::find(mLoggers.begin(), mLogger.end(), logger) != mLogger.end()) {
+    if (std::find(mLoggers.begin(), mLoggers.end(), logger) == mLoggers.end()) {
         mLoggers.emplace_back(logger);
     }
 }
 
 void LoggerManager::unregisterLogger(const Logger::Ptr& logger) {
-    auto iter = std::find(mLoggers.begin(), mLogger.end(), logger); 
-    if (iter != mLogger.end()) {
-        logger->fanalize();
-        mLogger.erease(iter);
+    auto iter = std::find(mLoggers.begin(), mLoggers.end(), logger); 
+    if (iter != mLoggers.end()) {
+        logger->finalize();
+        mLoggers.erase(iter);
     }
 }
 
@@ -92,7 +121,7 @@ void LoggerManager::unregisterLogger(const Logger::Ptr& logger) {
 {\
     std::ostringstream oss;\
     oss << msg;\
-    LoggerManager.instance().logMessage(LoggerManager::Lever:: level, oss.str(), __FILE__, __LINE__, __FUNCTION__);\
+    LoggerManager::instance().logMessage({LogLevel:: level, oss.str(), __FILE__, __LINE__, __FUNCTION__});\
 }
 
 #define Info(msg)   LogMessage(INFO, msg);
@@ -100,26 +129,39 @@ void LoggerManager::unregisterLogger(const Logger::Ptr& logger) {
 #define Debug(msg)  LogMessage(DEBUG, msg);
 
 
-class FileLogger : Logger {
+class FileLogger : public Logger {
 public:
     void logMessage(const std::string&) override;
-}
+    void finalize() override {}
+};
 
-class DataWareHouseLogger : Logger {
+class DatawareHouseLogger : public Logger {
 public:
     void logMessage(const std::string&) override;
-}
+    void finalize() override {}
+};
 
-void FileLogger::logMessage(const std::string& messsage) {
-
+void FileLogger::logMessage(const std::string& message) {
+    std::cout << "Filer Logger: " << message << std::endl;
 }
 
 void DatawareHouseLogger::logMessage(const std::string& message) {
-
+    std::cout << "Dataware House Logger: " << message << std::endl;
 }
 
 
 int main() {
+    std::shared_ptr<Logger> fileLogger = std::dynamic_pointer_cast<Logger>(std::make_shared<FileLogger>());
+    std::shared_ptr<Logger> dbLogger = std::dynamic_pointer_cast<Logger>(std::make_shared<DatawareHouseLogger>());
+
+    LoggerManager::instance().registerLogger(fileLogger);
+    LoggerManager::instance().registerLogger(dbLogger);
+
+    Info("information");
+
+    Error(404);
+
+    Debug("--- debug info --- " << "it's ok.");
 
     return 0;
 }
