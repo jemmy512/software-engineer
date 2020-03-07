230c5dcaca52b4ffd2c1865aaaa4f119ad40870d
Jemmy <jemmy1029760725@gmail.com>
2020-03-07 16:46:57 +0800

    Create 9.Facade.cc

---
+++ DesignPattern/9.Facade.cc
@@ -0,0 +1,9 @@
+/**
+ * Provide a unified interface to a set of interfaces in a subsystem. 
+ * Facade Pattern defines a higher-level interface that makes the subsystem easier to use.
+ * 
+ * Senario:
+ * 1. Resolve usability issue
+ * 2. Resolve performance issue
+ * 3. Resolve distributed transaction issue
+ */
