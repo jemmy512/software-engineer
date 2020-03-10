349ca382ff6abe6ce9f7710e65412cab92f9cd7d
Jemmy <jemmy1029760725@gmail.com>
2020-03-10 22:45:01 +0800

    Update MergeSort.cc

--- Algorithm/Sort/MergeSort.cc
+++ Algorithm/Sort/MergeSort.cc
@@ -1,43 +1,40 @@
 #include<iostream>
 #include<vector>
-#include<iterator>
 
 using namespace std;
 
 template<typename T>
-void mergeArray(vector<T> &data, int beg, int mid, int end, vector<T> &tmpData) {
-    int lbeg = beg, rbeg = mid;
-    int i = beg;
-    
-    while (lbeg != mid && rbeg != end) {
+void merge(T& data, int beg, int mid, int end) {
+    T tmpData(data.size());
+    int lbeg = beg, rbeg = mid, i = beg;
+
+    while (lbeg < mid && rbeg < end)
         tmpData[i++] = data[lbeg] < data[rbeg] ? data[lbeg++] : data[rbeg++];
-    }
-    
-    while (lbeg != mid)
+
+    while (lbeg < mid) 
         tmpData[i++] = data[lbeg++];
-    while (rbeg != end)
+    
+    while (rbeg < end)
         tmpData[i++] = data[rbeg++];
+
     for (i = beg; i < end; ++i)
         data[i] = tmpData[i];
 }
 
 template<typename T>
-void mergeSort(vector<T> &data, int beg, int end, vector<T> &tmpData) {
-    int mid = (beg + end) / 2;
-    if (mid != beg) {
-        mergeSort(data, beg, mid, tmpData);
-        mergeSort(data, mid, end, tmpData);
-        mergeArray(data, beg, mid, end, tmpData);
+void mergeSort(T& data, int beg, int end) {
+    int mid = (beg+end)/2;
+    if (beg != mid) {
+        mergeSort(data, beg, mid);
+        mergeSort(data, mid, end);
+        merge(data, beg, mid, end);
     }
 }
 
-int main(void) {
-    // vector<int> data = {3, 2, 5, 8, 4, 7, 6, 9};
-    // vector<float> tmpData(data.size());   
+int main(void) { 
     vector<float> data = {3.0, 2.3, 5.5, 8.3, 4.4, 7.6, 6.6, 9.8};
-    vector<float> tmpData(data.size());
-    mergeSort(data, 0, data.size() - 1, tmpData);
-    
-    copy(data.begin(), data.end(), ostream_iterator<int>(cout, " "));
+    mergeSort(data, 0, data.size());
+    copy(data.begin(), data.end(), ostream_iterator<float>(cout, " "));
+
     return 0;
 }
