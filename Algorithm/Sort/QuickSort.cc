33da4a423260d9671df97a8b79ed167a95264ac8
Jemmy <jemmy1029760725@gmail.com>
2020-03-10 22:45:08 +0800

    Update QuickSort.cc

--- Algorithm/Sort/QuickSort.cc
+++ Algorithm/Sort/QuickSort.cc
@@ -5,29 +5,29 @@
 using namespace std;
 
 template<typename T>
-void quickSort(vector<T> &data, int beg, int end) {
-    if (beg >= end)
+void quickSort(T& data, int beg, int end) {
+    if (data.size() == 0 || beg >= end) {
         return;
-
-    int b, e, pivot;
-    b = beg;
-    e = end;
-    pivot = data[beg];
-    while (b < e)  {
-        while (b < e && data[e] > pivot) --e;
-        if (b < e) data[b++] = data[e];
-        while (b < e && data[b] < pivot) ++b;
-        if (b < e) data[e--] = data[b];
     }
 
-    data[b] = pivot;
-    quickSort(data, beg, b - 1);
-    quickSort(data, b + 1, end);
+    int lbeg = beg;
+    int lend = end-1;
+    int pivot = data[beg];
+    while (lbeg < lend) {
+        while (lbeg < lend && data[lend] > pivot) --lend;
+        if (lbeg < lend) data[lbeg++] = data[lend];
+        while (lbeg < lend && data[lbeg] < pivot) ++lbeg;
+        if (lbeg < lend) data[lend--] = data[lbeg];
+    }
+
+    data[lbeg] = pivot;
+    quickSort(data, beg, lbeg-1);
+    quickSort(data, lend+1, end);
 }
 
 int main(void) {
     vector<int> data = {3, 2, 5, 8, 4, 7, 6, 9};
-    quickSort(data, 0, 7);
+    quickSort(data, 0, data.size());
     copy(data.begin(), data.end(), ostream_iterator<int>(cout," "));
     return 0;
 }
