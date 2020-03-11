ba76209b8614ce9578ab10ac0cc0aeb31745ce94
Jemmy <jemmy1029760725@gmail.com>
2020-03-11 22:17:14 +0800

    Update Notification.cc

--- Components/Notification.cc
+++ Components/Notification.cc
@@ -2,55 +2,55 @@
 #include <vector>
 #include <mutex>
 
-class IMessageCallback {
+class IMessageObserver {
 public:
     virtual void onDelete(const std::string& msg) = 0;
     virtual void onSend(const std::string& msg, int id) = 0;
-    virtual ~IMessageCallback() = default;
+    virtual ~IMessageObserver() = default;
 };
 
-template<typename Callback>
+template<typename Observer>
 class Notification {
 public:
-    using CallbackWPtr = std::weak_ptr<Callback>;
-    using CallbackPtr = std::shared_ptr<Callback>;
-    using CallbackList = std::vector<CallbackWPtr>;
+    using ObserverWPtr = std::weak_ptr<Observer>;
+    using ObserverPtr = std::shared_ptr<Observer>;
+    using ObserverList = std::vector<ObserverWPtr>;
 
-    void registerCallback(CallbackPtr callback) {
+    void registerObserver(ObserverPtr observer) {
         std::scoped_lock lock(mMutex);
 
-        if (findCallback(callback) == mCallbacks.cend()) {
-            mCallbacks.emplace_back(callback);
+        if (findObserver(observer) == mObservers.cend()) {
+            mObservers.emplace_back(observer);
         }
     }
 
-    void unregisterCallback(CallbackPtr callback) {
+    void unregisterObserver(ObserverPtr observer) {
         std::scoped_lock lock(mMutex);
 
-        mCallbacks.erase(findCallback(callback));
+        mObservers.erase(findObserver(observer));
     }
 
     template<typename Func, typename... Args>
     void notify(Func func, Args&&... args) {
-        CallbackList liveCallbacks;
-        liveCallbacks.reserve(mCallbacks.size());
+        ObserverList liveObservers;
+        liveObservers.reserve(mObservers.size());
 
-        for (const auto& wcb : mCallbacks) {
-            if (auto cb = wcb.lock()) {
-                std::invoke(func, cb, std::forward<Args>(args)...);
+        for (const auto& wob : mObservers) {
+            if (auto ob = wob.lock()) {
+                std::invoke(func, ob, std::forward<Args>(args)...);
 
-                liveCallbacks.emplace_back(wcb);
+                liveObservers.emplace_back(wob);
             }
         }
 
-        removeExpiredCallbacks(std::move(liveCallbacks));
+        removeExpiredObservers(std::move(liveObservers));
     }
 
 private:
-    auto findCallback(CallbackPtr callback) {
-        return std::find_if(mCallbacks.cbegin(), mCallbacks.cend(), [callback](auto& wcb) {
-                if (auto cb = wcb.lock()) {
-                    return cb == callback;
+    auto findObserver(ObserverPtr observer) {
+        return std::find_if(mObservers.cbegin(), mObservers.cend(), [observer](auto& wob) {
+                if (auto ob = wob.lock()) {
+                    return ob == observer;
                 } else {
                     return false;
                 }
@@ -58,51 +58,51 @@ private:
     }
 
     /**
-     * std::weak_ptr doesn't suppport compare operation, there's no way to get the 
+     * std::weak_ptr doesn't suppport compare operation, there's no way to get the
      * iterator of a weak_ptr and then use erase to delete it from vector.
-     * 
-     * auto ite = std::remove_if(mCallbacks.begin(), mCallbacks.end(), [](const auto& wcb){
-     *  return (auto cb = wcb.lock()) ? true : false;
+     *
+     * auto ite = std::remove_if(mObservers.begin(), mObservers.end(), [](const auto& wob){
+     *  return (auto ob = wob.lock()) ? true : false;
      * });
-     * mCallbacks.erase(ite, mCallbacks.end());
+     * mObservers.erase(ite, mObservers.end());
      */
-    void removeExpiredCallbacks(CallbackList&& liveCallbacks) {
+    void removeExpiredObservers(ObserverList&& liveObservers) {
         std::scoped_lock lock(mMutex);
 
-        if (liveCallbacks.size() < mCallbacks.size() / 3) {
-            mCallbacks = liveCallbacks;
+        if (liveObservers.size() < mObservers.size() / 3) {
+            mObservers = std::move(liveObservers);
         }
     }
 
 private:
     std::mutex mMutex;
-    CallbackList mCallbacks; 
+    ObserverList mObservers;
 };
 
-class MessageManager : public IMessageCallback {
+class MessageManager : public IMessageObserver {
 public:
     void onDelete(const std::string& msg) override {
-        std::cout << "ON delete callback msg: " << msg << std::endl;
+        std::cout << "ON delete observer msg: " << msg << std::endl;
     }
 
     void onSend(const std::string& msg, int id) override {
-        std::cout << "ON Send callback msg: " << msg << ", id: " << id << std::endl;
+        std::cout << "ON Send observer msg: " << msg << ", id: " << id << std::endl;
     }
 };
 
-class Server : public Notification<IMessageCallback> {
+class Server : public Notification<IMessageObserver> {
 public:
     Server() {
         mMsgManager = std::make_shared<MessageManager>();
 
-        registerCallback(mMsgManager);
+        registerObserver(mMsgManager);
     }
     void deleteMessage(const std::string& msg)  {
-        notify(&IMessageCallback::onDelete, msg);
+        notify(&IMessageObserver::onDelete, msg);
     }
 
     void sendMessage(const std::string& msg, int id) {
-        notify(&IMessageCallback::onSend, "msg", id);
+        notify(&IMessageObserver::onSend, "msg", id);
     }
 
 private:
