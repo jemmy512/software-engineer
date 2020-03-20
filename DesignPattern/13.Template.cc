50bca03abf4c23c52acea157784a3351744d4a44
Jemmy <jemmy1029760725@gmail.com>
2020-03-20 10:00:25 +0800

    Create 13.Template.cc

---
+++ DesignPattern/13.Template.cc
@@ -0,0 +1,14 @@
+
+int main() {
+
+    return 0;
+}
+
+/**
+ * Define the skeleton of an algorithm in an operation, deferring somesteps to
+ * subclasses. Template Method lets subclasses redefinecertain steps of an algorithm
+ * without changing the algorithm's structure.
+ *
+ * Callbacks and templates:
+ * 1. Callbacks do not need to define new subclasses for new functions
+ * 2. Callbacks do not need to implement unwanted template functions */
