f8675682e6e814b9f745564008f7b5494f2bfa60
Jemmy <jemmy1029760725@gmail.com>
2020-03-30 19:31:00 +0800

    Create 16.State.cc

---
+++ DesignPattern/16.State.cc
@@ -0,0 +1,108 @@
+#include <iostream>
+
+class MarioStateMachine;
+using IMarioStatePtr = std::shared_ptr<IMarioState>;
+
+class IMarioState {
+public:
+    enum State {
+        Small,
+        Super,
+        Fire,
+        Cape
+    };
+
+    virtual State getName() = 0;
+    virtual void onObtainMushRoom(MarioStateMachinePtr& sm) { };
+    virtual void onObtainFireFlow(MarioStateMachinePtr& sm) { };
+    virtual void onObtainCape(MarioStateMachinePtr& sm) { };
+    virtual void onMeetMonster(MarioStateMachinePtr& sm) { };
+
+    virtual ~IMarioState() = default;
+};
+
+using MarioStateMachinePtr = std::shared_ptr<MarioStateMachine>;
+
+class SmallMario;
+class SuperMario;
+class FireMario;
+class CapeMario;
+
+
+class SmallMario : public IMarioState {
+public:
+    State getName() {
+        return Small;
+    }
+
+    static IMarioStatePtr getInstance() {
+        static const IMarioStatePtr instance = std::make_shared<SmallMario>();;
+
+        return instance;
+    }
+
+    void onObtainMushRoom(MarioStateMachinePtr& sm) override {
+        sm->setState(SuperMario::getInstance());
+        sm->setScore(sm->getScore() + 100);
+    }
+
+    void onObtainFireFlow(MarioStateMachinePtr& sm) override {
+        sm->setState(FireMario::getInstance());
+        sm->setScore(sm->getScore() + 200);
+    }
+
+    void onObtainCape(MarioStateMachinePtr& sm) override {
+        sm->setState(CapeMario::getInstance());
+        sm->setScore(sm->getScore() + 300);
+    }
+};
+
+class MarioStateMachine : public std::enable_shared_from_this<MarioStateMachine> {
+public:
+    int getScore() {
+        return mScore;
+    }
+
+    void setScore(int score) {
+        mScore = score;
+    }
+
+    void setState(IMarioStatePtr state) {
+        mMarioState = state;
+    }
+
+    void onObtainMushRoom() {
+        mMarioState->onObtainMushroom(shared_from_this());
+    }
+
+    void onObtainFireFlow() {
+        mMarioState->onObtainFireFlow(shared_from_this());
+    }
+
+    void onObtainCape() {
+        mMarioState->onObtainCape(shared_from_this());
+    }
+
+    void onMeetMonster() {
+        mMarioState->onMeetMonstrer(shared_from_this());
+    }
+
+private:
+    int mScore{0};
+    IMarioStatePtr mMarioState = SmallMario::getInstance();
+};
+
+int main() {
+
+    return 0;
+}
+
+
+/**
+ * Allow an object to alter its behavior when its internal state changes.The object will appear to change its class.
+ *
+ * Three Implementation ways:
+ * 1. branch logic method
+ * 2. look-up table
+ * 3. state mode: avoids judging branch logic by dividing event-triggered state transitions and action execution
+ *    into different state classes. */
