c86c2cd267987970b44058a32808b6ae0c4cc8cd
Jemmy <jemmy1029760725@gmail.com>
2020-04-04 00:41:49 +0800

    Update RateLimit.cc

--- Algorithm/RateLimit.cc
+++ Algorithm/RateLimit.cc
@@ -27,6 +27,10 @@ private:
     long long rate;
     long long water;
 };
+/**
+ * Because the outflow speed is constant, it can resist burst traffic,
+ * achieve smoother current limiting, and do not allow traffic bursts. */
+
 
 class TokenBucket {
 public:
@@ -53,6 +57,10 @@ private:
     long long rate;
     long long token = 0;
 };
+/**
+ * 1. Can resist burst traffic, because the number of tokens in the bucket will not exceed the given maximum.
+ * 2. A smoother current limit can be achieved, because the token is put in at a uniform speed.
+ * 3. The token bucket algorithm allows a certain degree of traffic burst. (Compared to leaky bucket algorithm) */
 
 int main() {
 
