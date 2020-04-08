7604c88543432be56b897156203404de38739375
Jemmy <jemmy1029760725@gmail.com>
2020-04-08 17:45:22 +0800

    Update 13.Template.cc

--- DesignPattern/13.Template.cc
+++ DesignPattern/13.Template.cc
@@ -9,6 +9,11 @@ int main() {
  * subclasses. Template Method lets subclasses redefinecertain steps of an algorithm
  * without changing the algorithm's structure.
  *
+ * Functionalities:
+ * 1. Reuse: All subclasses can reuse the code of template method provided by parent class.
+ * 2. Extension: Framework provides extension points through template method, so framework users can
+ *    customize framework functions through the extension points without modifiying the framework.
+ *
  * Callbacks and templates:
  * 1. Callbacks do not need to define new subclasses for new functions
  * 2. Callbacks do not need to implement unwanted template functions */
