a2ad7a3efb8bcecf7f5fd2de62590dcce2e4cb7d
Jemmy <jemmy1029760725@gmail.com>
2020-04-18 20:45:16 +0800

    Create Lockable.cc

---
+++ Components/Lockable.cc
@@ -0,0 +1,131 @@
+#include <mutex>
+#include <string>
+#include <vector>
+#include <iostream>
+
+template<typename T>
+class Lockable {
+private:
+    mutable std::recursive_mutex mMutex;
+    T *mData;
+
+public:
+    class Ptr {
+    public:
+        explicit Ptr(Lockable* lockable) : mLockable(lockable), mLock(lockable->mLock) {}
+
+        Ptr(Ptr&& other) : mLockable(other.mLockable), mLock(std::move(other.mLock)) {}
+
+        Ptr(const Ptr&) = delete;
+
+        Ptr& operator==(const Ptr&) = delete;
+
+        T* operator->() {
+            if (mLockable) {
+                return &mLockable->mData;
+            }
+
+            return nullptr;
+        }
+
+        T& operator*() {
+            if (mLockable) {
+                return mLockable->mData;
+            }
+
+            throw("Error");
+        }
+
+        operator bool() {
+            return mLockable != nullptr;
+        }
+
+    private:
+        Lockable *mLockable;
+        std::unique_lock<std::recursive_mutex> mLock;
+    };
+
+    class ConstPtr {
+    public:
+        explicit ConstPtr(const Lockable* lockable) : mLockable(lockable), mLock(lockable->mlock) {}
+
+        ConstPtr(ConstPtr&& other) : mLockable(other.mLockable), mLock(std::move(other.mMutex)) {}
+
+        ConstPtr(const ConstPtr&) = delete;
+
+        ConstPtr& operator==(const ConstPtr&) = delete;
+
+        T* operator->() {
+            if (mLockable) {
+                return &mLockable->mData;
+            }
+
+            return nullptr;
+        }
+
+        T& operator*() {
+            if (mLockable) {
+                return mLockable->mData;
+            }
+
+            throw("Errro");
+        }
+
+        operator bool() {
+            return mLockable != nullptr;
+        }
+
+    private:
+        const Lockable* mLockable;
+        std::unique_lock<std::recursive_mutex> mLock;
+    };
+
+public:
+    template<typename... Args>
+    explicit Lockable(Args... args) : mData(args...) {}
+
+    Ptr lock() {
+        return std::move(Ptr(this));
+    }
+
+    ConstPtr readLock() const {
+        return std::move(ConstPtr(this));
+    }
+};
+
+ void foo() {
+    std::string str = "Hello world!";
+    std::vector<std::string> vecString;
+    std::cout<< "1. " << std::dec << (long)&str << ", " << std::dec << (long)&str[0] << ", " << str << std::endl;
+
+    vecString.push_back(std::move(str));
+    
+    std::cout << "2. " << std::dec << (long)&str << ", " << std::dec << (long)&str[0] << ", " << str << std::endl;
+
+    str.append("w");
+    std::cout << "3. " << std::dec << (long)&str << ", " << std::dec << (long)&str[0] << ", " << str << std::endl;
+    
+    std::cout<< "4. " << std::dec << (long)&(vecString[0]) << ", " << std::dec <<  (long)&(vecString[0][0]) << ", " << std::dec << (long)&vecString << std::endl;
+}
+
+class AB {
+public:
+    AB(){
+        std::cout << "constructor" << std::endl;
+    }
+    AB(const AB& rhs) : age(rhs.age), name(rhs.name) {
+        std::cout << "copy constructor" << std::endl;
+    }
+
+    AB(AB&& ab) : age(std::move(ab.age)), name(std::move(ab.name)) {
+        std::cout << "move constructor" << std::endl;
+    }
+
+    ~AB() {
+        std::cout << "~()" << std::endl;
+    }
+
+    int age = 23;
+    std::string name = "name";
+};
+
