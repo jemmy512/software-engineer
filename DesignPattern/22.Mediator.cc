76482c081d5d8f48b361c1ca8a25cd173d8bb7e6
Jemmy <jemmy1029760725@gmail.com>
2020-05-01 15:46:08 +0800

    Create 22.Mediator.cc

---
+++ DesignPattern/22.Mediator.cc
@@ -0,0 +1,14 @@
+/* Author: Jemmy
+ * Date: 2020-04-20 */
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
+
+/**
+ * Mediator pattern defines a separate (mediator) object that encapsulates the
+ * interaction between a set of objects and the objects delegate their interaction
+ * to a mediator object instead of interacting with each other directly. */
