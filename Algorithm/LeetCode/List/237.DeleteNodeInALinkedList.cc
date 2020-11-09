cc4b221e88fa40da55930820ef60a0509a329c84
Jemmy <jemmy1029760725@gmail.com>
2020-11-09 22:28:01 +0800

    Update 237.DeleteNodeInALinkedList.cc

--- Algorithm/LeetCode/List/237.DeleteNodeInALinkedList.cc
+++ Algorithm/LeetCode/List/237.DeleteNodeInALinkedList.cc
@@ -1,7 +1,7 @@
 /*
 Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
 
-Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list 
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list
     should become 1 -> 2 -> 4 after calling your function.
 */
 #include<iostream>
@@ -15,12 +15,8 @@ struct ListNode {
 
 class Solution {
 public:
-    static void deleteNode(ListNode* head) {
+    static void deleteNode(ListNode* node) {
          node->val = node->next->val;
          node->next = node->next->next;
     }
 };
-
-int main() {
-    
-}
