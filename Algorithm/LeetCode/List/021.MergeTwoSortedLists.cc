e90d3b2b03d011e8af5b91986e5d2a00302bb480
Jemmy <jemmy1029760725@gmail.com>
2020-11-09 22:29:00 +0800

    Update 021.MergeTwoSortedLists.cc

--- Algorithm/LeetCode/List/021.MergeTwoSortedLists.cc
+++ Algorithm/LeetCode/List/021.MergeTwoSortedLists.cc
@@ -1,5 +1,5 @@
 /*
-Merge two sorted linked lists and return it as a new list. The new list should 
+Merge two sorted linked lists and return it as a new list. The new list should
 be made by splicing together the nodes of the first two lists.
 Example:
 Input: 1->2->4, 1->3->4
@@ -22,21 +22,21 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         return l2;
     if (l2 == nullptr)
         return l1;
-    
+
     ListNode head = ListNode(INT_MIN);
     ListNode *tail = &head;
-   
-    while (l1 != nullptr && l2 != nullptr) {
+
+    while (l1 && l2) {
         if (l1->val < l2->val) {
             tail = tail->next = l1;
             l1 = l1->next;
         } else {
             tail = tail->next = l2;
             l2 = l2->next;
-        } 
+        }
     }
     tail->next = l1 ? l1 : l2;
-    
+
     return head.next;
 }
 
@@ -45,29 +45,25 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     ListNode dummy(0);
     ListNode *tail = &dummy;
 
-    while(l1 && l2) {
-        ListNode *& node = l1->val < l2->val ? l1 : l2;
+    while (l1 && l2) {
+        ListNode*& node = l1->val < l2->val ? l1 : l2;
         tail = tail->next = node;
         node = node->next;
     }
     tail->next = l1 ? l1 : l2;
+
     return dummy.next;
 }
 
 ListNode* mergeTwoLists_recursive(ListNode* l1, ListNode* l2) {
-       if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        
-        if(l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l2->next, l1);
-            return l2;
-        }
-    }
+    if(l1 == NULL) return l2;
+    if(l2 == NULL) return l1;
 
-int main(void) {
-    
-    return 0;
+    if(l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l2->next, l1);
+        return l2;
+    }
 }
