52de0bec3ab718f2adebedfce8b2bc4f403da03d
Jemmy <jemmy1029760725@gmail.com>
2020-11-11 23:16:45 +0800

    Update StringUtil.cc

--- Components/StringUtil.cc
+++ Components/StringUtil.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 namespace util::string {
     using std::string;
@@ -56,4 +57,29 @@ namespace util::string {
 
         return result;
     }
+
+    std::vector<std::string> split_(const std::string& str, const std::string& delimiter) {
+        std::vector<std::string> result;
+
+        using size_type = typename std::string::size_type;
+        size_type begin = 0;
+        size_type end = std::string::npos;
+
+        do {
+            end = str.find_first_of(delimiter, begin);
+            auto token = str.substr(begin, end-begin); // std::out_of_range if pos > size()
+            if (!token.empty())
+                result.emplace_back(token);
+            begin = end + 1;
+        } while (end != std::string::npos);
+
+        return result;
+    }
 };
+
+int main() {
+    using namespace util::string;
+
+    auto vec = split_("The quick brown fox jumps over the lazy dog", " ");
+    std::copy(vec.cbegin(), vec.cend(), std::ostream_iterator<std::string>(std::cout, "|\n"));
+}
