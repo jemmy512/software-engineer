f1f129435b6874dfdea7ca2879266c8d5dae0e54
Jemmy <jemmy1029760725@gmail.com>
2020-11-12 01:56:45 +0800

    Update 007.ReverseInteger.cc

--- Algorithm/LeetCode/007.ReverseInteger.cc
+++ Algorithm/LeetCode/007.ReverseInteger.cc
@@ -6,12 +6,26 @@ Example 1:
     Input: -123 Output: -321
     Input: 120  Output: 21
 Note:
-Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. 
-For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows. 
+Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range.
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 
 #include<iostream>
 using namespace std;
+
+// 100%
+int reverse(int x) {
+    int rev = 0;
+    while (x != 0) {
+        int pop = x % 10;
+        // -2147483648 2147483647
+        if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+        if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+        x /= 10;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
 
 // beat 50.41 cpp submission
 int reverse1(int x) {
@@ -19,9 +33,10 @@ int reverse1(int x) {
     while (x) {
         ret = ret * 10 + x % 10;
         x /= 10;
+        if (ret < INT_MIN || ret > INT_MAX)
+            return 0;
     }
-    if (ret < INT_MIN || ret > INT_MAX)
-        return 0;
+
     return ret;
 }
 
@@ -41,6 +56,6 @@ int reverse2(int x) {
 }
 
 int main(void) {
-    
+
     return 0;
 }
