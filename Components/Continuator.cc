530d3a83ac9c0bc5790991793ae71cdffc0cdb9d
Jemmy <jemmy1029760725@gmail.com>
2020-11-17 20:52:40 +0800

    Update Continuator.cc

--- Components/Continuator.cc
+++ Components/Continuator.cc
@@ -1,5 +1,9 @@
 #include "./FunctionUtil.h"
 
+#include <iostream>
+#include <memory>
+#include <string>
+
 template <typename Continuation>
 struct Continuator
 {
@@ -25,10 +29,37 @@ struct Continuator
         return move(action);
     }
 
-    template <typename Callable, typename... Args>
-    Continuator<typename function_traits<Callable>::argument_0> then(const Callable& callable, Args... args) {
-        return [continuable = std::move(action), callable, args...] (const typename function_traits<Callable>::argument_0& continuation) {
+    template <typename Callable, typename Arg0 = typename function_traits<Callable>::template argument<0>>
+    Continuator<Arg0> then(const Callable& callable) {
+        return [continuable = FunctionUtil::move(action), callable](const Arg0& continuation) {
             continuable(function_traits<Callable>::helper::bind(callable, continuation));
         };
     }
 };
+
+class Successor : std::enable_shared_from_this<Successor> {
+public:
+    void onSuccess(int i) {
+        std::cout << "Successor: " << i << std::endl;
+    }
+};
+
+int main() {
+    using OnSuccess = std::function<void(int)>;
+    using OnError = std::function<void(std::string)>;
+
+    // std::function<void(int)>
+    // std::function<void(const OnSuccess&)>
+    Continuator<OnSuccess>([](OnSuccess suc){
+        std::cout << "0" << std::endl;
+        suc(1);
+    }).then([](OnSuccess suc, int j) { // Arg: std::function<void(int)>
+        std::cout << j << std::endl;
+        suc(2);
+    }).then([](OnError suc, int j) { // Arg: std::function<void(int)>
+        std::cout << j << std::endl;
+        suc("hi");
+    }).then([](OnError suc, const std::string& str) { // Arg: std::function<void(std::string)>
+        std::cout << str << std::endl;
+    });
+}
