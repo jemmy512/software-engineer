66605000b144b217c054622810c21341b085bbff
Jemmy <jemmy1029760725@gmail.com>
2020-11-28 19:37:36 +0800

    Create 124.BinaryTreeMaximumPathSum.cc

---
+++ Algorithm/LeetCode/Tree/124.BinaryTreeMaximumPathSum.cc
@@ -0,0 +1,23 @@
+/* Hard
+Given a non-empty binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any node sequence from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 6
+Example 2:
+
+
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 3 * 104].
+-1000 <= Node.val <= 1000 */
