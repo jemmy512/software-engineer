1f88825c4d2b19dbea92a27e770d6b101083de12
Jemmy <jemmy1029760725@gmail.com>
2020-11-30 19:03:03 +0800

    Update combination.cc

--- DataStructure/String/combination.cc
+++ DataStructure/String/combination.cc
@@ -5,16 +5,14 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+using std::string, std::cout, std::endl;
 
-void stringCombination(const string & str) {
-    int len = str.size();
-    int n = 1<<len;
+void stringCombination(const string& str) {
+    int n = 1 << str.size();
     for (int i = 0; i < n; ++i) {
         cout << i << ": ";
-        for (int j = 0; j < len; ++j) {
-            int tmp = i;
-            if (tmp & (1<<j))
+        for (int j = 0; j < str.size(); ++j) {
+            if (i & (1<<j))
                 cout << str[j];
         }
         cout << endl;
@@ -25,5 +23,3 @@ int main() {
     string str = "abc";
     stringCombination(str);
 }
-
-
