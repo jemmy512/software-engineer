05300a00e87683eec90644a4b33c64e884e519f2
Jemmy <jemmy1029760725@gmail.com>
2020-12-01 18:52:31 +0800

    168.ExcelSheetColumTitle.cc

--- Algorithm/LeetCode/Arithmetic/168.ExcelSheetColumTitle.cc
+++ Algorithm/LeetCode/Arithmetic/168.ExcelSheetColumTitle.cc
@@ -1,4 +1,4 @@
-/*
+/* Easy
 Related to question 171.Excel Sheet Column Number
 
 Given a positive integer, return its corresponding column title as appear in an Excel sheet.
@@ -19,33 +19,27 @@ using namespace std;
 // 18 test caes, 2ms, beat 100%
 class Solution {
 public:
+    string convertToTitle(int n) {
+        return base26_int2str(n);
+    }
+
+private:
     string base26_int2str(long n) {
-        string str = "";
-        char ch;
+        string str;
         while (n > 0) {
-            ch = 'A' + (n - 1) % 26;
+            char ch = 'A' + (n - 1) % 26;
             str.insert(str.begin(), ch);
-            n -= (n - 1) % 26;
+            n -= (n - 1) % 26; // n = 26
             n /= 26;
         }
         return str;
     }
-    
+
     long long base26_str2int(string s) {
         long long ret = 0;
-        int tmp;
-        for (int i = 0; i < s.size(); ++i) {
-            tmp = s[i] - 'A' + 1;
-            ret = ret * 26 + tmp;
-        }
+        std::for_each(s.begin(), s.end(), [&ret](const auto& ch) {
+            ret = ret * 26 + (ch - 'A' + 1);
+        });
         return ret;
     }
-    static string convertToTitle(int n) {
-       return base26_int2str(n);
-    }
 };
-
-int main() {
-    string ret = Solution::convertToTitle(52);
-    cout << ret << endl;
-}
