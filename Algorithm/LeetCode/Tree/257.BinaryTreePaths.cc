805aa4e647e8ec4c457667d02cb8d317321b3835
Jemmy <jemmy1029760725@gmail.com>
2020-12-04 00:41:28 +0800

    Update 257.BinaryTreePaths.cc

--- Algorithm/LeetCode/Tree/257.BinaryTreePaths.cc
+++ Algorithm/LeetCode/Tree/257.BinaryTreePaths.cc
@@ -1,4 +1,4 @@
-/*
+/* Easy
 Given a binary tree, return all root-to-leaf paths.
 
 Note: A leaf is a node with no children.
@@ -15,43 +15,84 @@ Input:
 
 Output: ["1->2->5", "1->3"]
 
-Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3 */
 
-*/
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    void traverse(TreeNode *root, vector<string> &vec, string path) {
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if (!root)
+            return {};
+
+        vector<string> pathes;
+        stack<string> pathStack;
+        stack<TreeNode*> nodeStack;
+
+        pathStack.push(to_string(root->val));
+        nodeStack.push(root);
+
+        while (!nodeStack.empty()) {
+            auto* node = nodeStack.top(); nodeStack.pop();
+            auto path = pathStack.top(); pathStack.pop();
+
+            if (!node->left && !node->right)
+                pathes.emplace_back(path);
+
+            if (node->right) {
+                nodeStack.push(node->right);
+                pathStack.push(path + "->" + to_string(node->right->val));
+            }
+
+            if (node->left) {
+                nodeStack.push(node->left);
+                pathStack.push(path + "->" + to_string(node->left->val));
+            }
+        }
+
+        return pathes;
+    }
+};
+
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if (!root)
+            return {};
+
+        vector<string> vec;
+        traverse(root, vec, "");
+
+        return vec;
+    }
+
+private:
+    void traverse(TreeNode* root, vector<string>& vec, string path) {
         if (!root) return;
-        
+
         if (!path.empty())
             path +=  "->";
         path += to_string(root->val);
 
         if (!root->left && !root->right) {
-            vec.push_back(path);
+            vec.emplace_back(path);
         } else {
             traverse(root->left, vec, path);
             traverse(root->right, vec, path);
         }
-    }
-    
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> vec;
-        if (!root)
-            return vec;
-        
-        string path;
-        traverse(root, vec, path);
-        
-        return vec;
     }
 };
