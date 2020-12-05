737f275f59566972f8bf0970e2a0e816ec467ef1
Jemmy <jemmy1029760725@gmail.com>
2020-12-05 17:06:01 +0800

    Update 437.PathSum_III.cc

--- Algorithm/LeetCode/Tree/437.PathSum_III.cc
+++ Algorithm/LeetCode/Tree/437.PathSum_III.cc
@@ -1,9 +1,9 @@
-/*
+/* Medium
 You are given a binary tree in which each node contains an integer value.
 
 Find the number of paths that sum to a given value.
 
-The path does not need to start or end at the root or a leaf, but it must go 
+The path does not need to start or end at the root or a leaf, but it must go
 downwards (traveling only from parent nodes to child nodes).
 
 The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
@@ -25,35 +25,76 @@ Return 3. The paths that sum to 8 are:
 1.  5 -> 3
 2.  5 -> 2 -> 1
 3. -3 -> 11
-*/
 
+Relatives:
+560. Subarray Sum Equals K */
+
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/* Prefix Sum
+ * Complexity: O(N) */
 class Solution {
 public:
-    int helper(TreeNode* root, int sum, unordered_map<int, int>& store, int pre) {
-        if (!root) return 0;
-        root->val += pre;
-        int res = (root->val == sum) + (store.count(root->val - sum) ? store[root->val - sum] : 0);
-        store[root->val]++;
-        res += helper(root->left, sum, store, root->val) + helper(root->right, sum, store, root->val);
-        store[root->val]--;
-        return res;
-    }
     int pathSum(TreeNode* root, int sum) {
-        unordered_map<int, int> store;
-        return helper(root, sum, store, 0);
+        _Sum = sum;
+
+        preOrder(root, 0);
+
+        return _Cnt;
     }
+
+private:
+    void preOrder(TreeNode* node, int curSum) {
+        if (!node)
+            return;
+
+        curSum += node->val;
+        if (curSum == _Sum)
+            ++_Cnt;
+
+        /* can't reorder: sum may 0, so curSum-_Sum == curSum */
+        _Cnt += _HashMap[curSum-_Sum];
+        ++_HashMap[curSum];
+
+        preOrder(node->left, curSum);
+        preOrder(node->right, curSum);
+
+        --_HashMap[curSum];
+    }
+
+private:
+    int _Cnt{0};
+    int _Sum{0};
+    unordered_map<int, int> _HashMap;
 };
 
 class Solution_ {
 public:
-    int path(TreeNode *root, int curSum, int sum) {
-        if (!root) return 0;
-        curSum += root->val; 
-        return (curSum == sum) + path(root->left, curSum, sum) + path(root->right, curSum, sum);
-    }
-    
     int pathSum(TreeNode* root, int sum) {
-        if (!root) return 0;
-        return path(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+        if (!root)
+            return 0;
+
+        return path(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+
+private:
+    int path(TreeNode *root, int curSum) {
+        if (!root)
+            return 0;
+
+        curSum -= root->val;
+
+        return (curSum == 0) + path(root->left, curSum) + path(root->right, curSum);
     }
 };
