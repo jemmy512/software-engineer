7830dc3875c8686ff98bbf7dc85fa3ae085d2657
Jemmy <jemmy1029760725@gmail.com>
2020-12-05 22:05:09 +0800

    Update 111.MinimumDepthofBinaryTree.cc

--- Algorithm/LeetCode/Tree/111.MinimumDepthofBinaryTree.cc
+++ Algorithm/LeetCode/Tree/111.MinimumDepthofBinaryTree.cc
@@ -1,48 +1,42 @@
-/*
-Difficulty: Easy
-
+/* Easy
 Given a binary tree, find its minimum depth.
 
-The minimum depth is the number of nodes along the shortest path from the
- root node down to the nearest leaf node.
+**The minimum depth is the number of nodes along the shortest path from the
+ root node down to the nearest leaf node. **
 
-Note: A leaf is a node with no children.
+Note: **A leaf is a node with no children.**
 
 Example:
 
 Given binary tree [3,9,20,null,null,15,7],
 
-    3
-   / \
-  9  20
-    /  \
-   15   7
-return its minimum depth = 2.
-*/
+     3
+   /   \
+  9     20
+   \   /  \
+    8 15   7
+     |
+     8
+return its minimum depth = 3 [3, 9, 8]. */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (!root) return 0;
-        if (!root->left && !root->right) 
-            return 1;
-        
-        int l1 = INT_MAX;
-        int l2 = INT_MAX;
-        if (root->left)
-            l1 =  minDepth(root->left);
-        if (root->right)
-            l2 = minDepth(root->right);
-        
-        return (l1 < l2 ? l1 + 1 : l2 + 1);
+        if (!root)
+            return 0;
+
+        if (!root->left)
+            return minDepth(root->right) + 1;
+        else if (!root->right)
+            return minDepth(root->left) + 1;
+        else
+            return min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
