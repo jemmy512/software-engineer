48cd7f8b989de93762620fdb237d76de5956470a
Jemmy <jemmy1029760725@gmail.com>
2020-12-05 23:35:57 +0800

    Update 108.ConvertSortedArrayToBinarySearchTree.cc

--- Algorithm/LeetCode/Tree/108.ConvertSortedArrayToBinarySearchTree.cc
+++ Algorithm/LeetCode/Tree/108.ConvertSortedArrayToBinarySearchTree.cc
@@ -1,13 +1,10 @@
-/*
-Difficulty: Easy
-
+/* Easy
 Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
-For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of 
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of
 the two subtrees of every node never differ by more than 1.
 
 Example:
-
 Given the sorted array: [-10,-3,0,5,9],
 
 One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
@@ -16,35 +13,35 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
      / \
    -3   9
    /   /
- -10  5
-*/
+ -10  5 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums, int beg, int end) {
-        if (beg > end) return NULL;
-        if (beg == end) return (new TreeNode(nums[beg]));
-        
-        int mid = (beg + end) >> 1;
-        TreeNode *node = new TreeNode(nums[mid]);
-        node->left = sortedArrayToBST(nums, beg, mid - 1);
-        node->right = sortedArrayToBST(nums, mid + 1, end);
-        
-        return node;
-    }
-    
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if (nums.empty()) return NULL;
-        
-        return sortedArrayToBST(nums, 0, nums.size() - 1);
+        return convert(nums, 0, nums.size());
+    }
+
+private:
+    TreeNode* convert(const vector<int>& nums, int beg, int end) {
+        if (beg >= end)
+            return nullptr;
+
+        int mid = beg + (end - beg) / 2;
+        auto* node = new TreeNode(nums[mid]);
+        node->left = convert(nums, beg, mid);
+        node->right = convert(nums, mid+1, end);
+
+        return node;
     }
 };
