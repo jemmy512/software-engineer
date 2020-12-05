f0bdc8446194d923a7fba1514b70d7094d11b60b
Jemmy <jemmy1029760725@gmail.com>
2020-12-05 18:22:42 +0800

    Update 001.TwoSum.cc

--- Algorithm/LeetCode/Arithmetic/001.TwoSum.cc
+++ Algorithm/LeetCode/Arithmetic/001.TwoSum.cc
@@ -1,12 +1,17 @@
-/*
- * Difficulty: Easy
+/* Easy
  *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * Example:
  * Given nums = [2, 7, 11, 15], target = 9,
  * Because nums[0] + nums[1] = 2 + 7 = 9,return [0, 1].
- */
+ *
+ * Relatives:
+ * 1. Two Sum
+ * 167. Two Sum II - Input array is sorted
+ * 170. Two Sum III - Data structure design
+ * 653. Two Sum IV - Input is a BST
+ * 1214. Two Sum BSTs */
 
 #include <unordered_map>
 #include <vector>
