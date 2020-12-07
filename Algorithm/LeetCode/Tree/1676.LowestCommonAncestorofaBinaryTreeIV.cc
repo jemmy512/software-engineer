63c5e42cfcbad239bf12cf5654140c815603ed9a
Jemmy <jemmy1029760725@gmail.com>
2020-12-07 19:54:23 +0800

    Create 1676.LowestCommonAncestorofaBinaryTreeIV.cc

---
+++ Algorithm/LeetCode/Tree/1676.LowestCommonAncestorofaBinaryTreeIV.cc
@@ -0,0 +1,75 @@
+/* Medium
+Given the root of a binary tree and an array of TreeNode objects nodes, return the lowest common ancestor (LCA) of all the nodes in nodes. All the nodes will exist in the tree, and all values of the tree's nodes are unique.
+
+Extending the definition of LCA on Wikipedia: "The lowest common ancestor of n nodes p1, p2, ..., pn in a binary tree T is the lowest node that has every pi as a descendant (where we allow a node to be a descendant of itself) for every valid i". A descendant of a node x is a node y that is on the path from node x to some leaf node.
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [4,7]
+Output: 2
+Explanation: The lowest common ancestor of nodes 4 and 7 is node 2.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [1]
+Output: 1
+Explanation: The lowest common ancestor of a single node is the node itself.
+
+Example 3:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [7,6,2,4]
+Output: 5
+Explanation: The lowest common ancestor of the nodes 7, 6, 2, and 4 is node 5.
+
+Example 4:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [0,1,2,3,4,5,6,7,8]
+Output: 3
+Explanation: The lowest common ancestor of all the nodes is the root node.
+
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-109 <= Node.val <= 109
+All Node.val are unique.
+All nodes[i] will exist in the tree.
+All nodes[i] are distinct. */
+
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, vector<TreeNode*>& nodes) {
+        _HashSet.reserve(nodes.size());
+        _HashSet.insert(nodes.begin(), nodes.end());
+
+        return lca(root);
+    }
+
+private:
+    TreeNode* lca(TreeNode* root) {
+        if (!root || _HashSet.find(root) != _HashSet.end())
+            return root;
+
+        auto* left = lca(root->left);
+        auto* right = lca(root->right);
+
+        if (!left)
+            return right;
+        else if (!right)
+            return left;
+        else
+            return root;
+    }
+
+private:
+    unordered_set<TreeNode*> _HashSet;
+};
