59db73c730ed0e8e660d5aff735af736d3de8afa
Jemmy <jemmy1029760725@gmail.com>
2020-12-07 19:54:40 +0800

    Update 671.SecondMinimumNodeinaBinaryTree.cc

--- Algorithm/LeetCode/Tree/671.SecondMinimumNodeinaBinaryTree.cc
+++ Algorithm/LeetCode/Tree/671.SecondMinimumNodeinaBinaryTree.cc
@@ -1,15 +1,15 @@
 /*
-Given a non-empty special binary tree consisting of nodes with the non-negative 
+Given a non-empty special binary tree consisting of nodes with the non-negative
 value, where each node in this tree has exactly two or zero sub-node. If the node
  has two sub-nodes, then this node's value is the smaller value among its two sub-nodes.
 
-Given such a binary tree, you need to output the second minimum value in the set 
+Given such a binary tree, you need to output the second minimum value in the set
 made of all the nodes' value in the whole tree.
 
 If no such second minimum value exists, output -1 instead.
 
 Example 1:
-Input: 
+Input:
     2
    / \
   2   5
@@ -18,51 +18,55 @@ Input:
 
 Output: 5
 Explanation: The smallest value is 2, the second smallest value is 5.
+
 Example 2:
-Input: 
+Input:
     2
    / \
   2   2
 
 Output: -1
-Explanation: The smallest value is 2, but there isn't any second smallest value.
-*/
+Explanation: The smallest value is 2, but there isn't any second smallest value. */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
- 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     int findSecondMinimumValue(TreeNode* root) {
-        if (!root) return -1;
-        return minval(root, root->val);
+        return secondMin(root, root->val);
     }
+
 private:
-    int minval(TreeNode* p, int first) {
-        if (p == nullptr) return -1;
-        if (p->val != first) return p->val;
-        int left = minval(p->left, first);
-        int right = minval(p->right, first);
-        // if all nodes of a subtree = root->val, 
+    int secondMin(TreeNode* node, int firstMin) {
+        if (!node)
+            return -1;
+        if (node->val != firstMin)
+            return node->val;
+
+        int left = secondMin(node->left, firstMin);
+        int right = secondMin(node->right, firstMin);
+
+        // if all nodes of a subtree = root->val,
         // there is no second minimum value, return -1
-        if (left == -1) return right;
-        if (right == -1) return left;
-        return min(left, right);
+        if (left == -1)
+            return right;
+        else if (right == -1)
+            return left;
+        else
+            return min(left, right);
     }
 };
- 
+
 class Solution {
 public:
     int findSecondMinimumValue(TreeNode* root) {
         if (!root || !root->left) return -1;
-        
+
         int min = root->val;
         long min_second = LONG_MAX;
         queue<TreeNode *> que;
@@ -74,7 +78,7 @@ public:
             que.pop();
             if (cur->val != min && cur->val < min_second)
                 min_second = cur->val;
-            if (cur->left) 
+            if (cur->left)
                 que.push(cur->left);
             if (cur->right)
                 que.push(cur->right);
