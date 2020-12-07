054bfc47562d306b033794e531f64b8d5e2ac505
Jemmy <jemmy1029760725@gmail.com>
2020-12-07 22:53:03 +0800

    Update 199.BinaryTreeRightSideView.cc

--- Algorithm/LeetCode/Tree/199.BinaryTreeRightSideView.cc
+++ Algorithm/LeetCode/Tree/199.BinaryTreeRightSideView.cc
@@ -1,11 +1,8 @@
-/*
-Difficulty: Medium
-
-Given a binary tree, imagine yourself standing on the right side of it, return 
+/* Medium
+Given a binary tree, imagine yourself standing on the right side of it, return
 the values of the nodes you can see ordered from top to bottom.
 
 Example:
-
 Input: [1,2,3,null,5,null,4]
 Output: [1, 3, 4]
 Explanation:
@@ -15,33 +12,31 @@ Explanation:
 2     3         <---
  \     \
   5     4       <---
-  
-  
-Solution: Level Traversal
-*/
 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+Solution: Level Traversal */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> vec;
         if (!root) return vec;
-        
+
         queue<TreeNode *> que;
         que.push(root);
         TreeNode *node = root, *prevLast = root, *last = root;
-        
+
         while (!que.empty()) {
             node = que.front();
+            que.pop();
+
             if (node->left) {
                 que.push(node->left);
                 last = node->left;
@@ -50,13 +45,13 @@ public:
                 que.push(node->right);
                 last = node->right;
             }
+
             if (node == prevLast) {
                 vec.push_back(node->val);
                 prevLast = last;
             }
-            que.pop();
         }
-        
+
         return vec;
     }
 };
