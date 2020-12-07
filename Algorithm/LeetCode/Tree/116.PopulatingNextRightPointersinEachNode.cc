907042ce1cc481fb73a42c066f6f2d27a9b96b84
Jemmy <jemmy1029760725@gmail.com>
2020-12-07 23:39:53 +0800

    Update 116.PopulatingNextRightPointersinEachNode.cc

--- Algorithm/LeetCode/Tree/116.PopulatingNextRightPointersinEachNode.cc
+++ Algorithm/LeetCode/Tree/116.PopulatingNextRightPointersinEachNode.cc
@@ -6,7 +6,8 @@ struct Node {
   Node *right;
   Node *next;
 }
-Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Populate each next pointer to point to its next right node.
+If there is no next right node, the next pointer should be set to NULL.
 
 Initially, all next pointers are set to NULL.
 
