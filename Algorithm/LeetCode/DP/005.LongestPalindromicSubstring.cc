701b1d46d3ca2d22d285c0a0eeaf2d28fbdb44ff
Jemmy <jemmy1029760725@gmail.com>
2020-12-10 23:19:18 +0800

    Update 005.LongestPalindromicSubstring.cc

--- Algorithm/LeetCode/DP/005.LongestPalindromicSubstring.cc
+++ Algorithm/LeetCode/DP/005.LongestPalindromicSubstring.cc
@@ -26,7 +26,6 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int len = s.size();
-        if (len < 2) return s;
         int beg = 0, cnt = 0;
 
         for (int i = 0; i < len; ++i) {
