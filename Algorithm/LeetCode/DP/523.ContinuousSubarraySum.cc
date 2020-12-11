b296026b0cca47f50ddc067be9831c88f40dac29
Jemmy <jemmy1029760725@gmail.com>
2020-12-11 19:41:22 +0800

    523.ContinuousSubarraySum.cc

--- Algorithm/LeetCode/DP/523.ContinuousSubarraySum.cc
+++ Algorithm/LeetCode/DP/523.ContinuousSubarraySum.cc
@@ -1,37 +1,41 @@
-/*
-Difficulty: Medium
-
-Given a list of non-negative numbers and a target integer k, write a function to 
-check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, 
+/* Medium
+Given a list of non-negative numbers and a target integer k, write a function to
+check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is,
 sums up to n*k where n is also an integer.
 
 Example 1:
 Input: [23, 2, 4, 6, 7],  k=6
 Output: True
 Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+
 Example 2:
 Input: [23, 2, 6, 4, 7],  k=6
 Output: True
 Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+
 Note:
 The length of the array won't exceed 10,000.
 You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
 
-*/
+Realtives:
+523. Continuous Subarray Sum
+560. Subarray Sum Equals K */
+
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size(), sum = 0, pre = 0;
+        int sum = 0, pre = 0;
         unordered_set<int> modk;
-        
-        for (int i = 0; i < n; ++i) {
+
+        for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
             int mod = (k == 0 ? sum : sum % k);
-            if (modk.count(mod)) return true;
+            if (modk.find(mod) != modk.end())
+                return true;
             modk.insert(pre);
             pre = mod;
         }
-        
+
         return false;
     }
 };
@@ -41,7 +45,7 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int len  = nums.size();
         if (len < 2) return false;
-        
+
         int sumMax = nums[0];
         int sumCur = nums[0];
         int beg = 0, end = 0;
@@ -62,7 +66,7 @@ public:
                 }
             }
         }
-        
+
         return false;
     }
 };
