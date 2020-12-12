10c6fd76304bf57915ba4a8711f1ef8bf248b908
Jemmy <jemmy1029760725@gmail.com>
2020-12-13 03:34:32 +0800

    Update 065.ValidNumber.cc

--- Algorithm/LeetCode/Math/065.ValidNumber.cc
+++ Algorithm/LeetCode/Math/065.ValidNumber.cc
@@ -1,4 +1,4 @@
-/*
+/* Hard
 Validate if a given sing is numeric.
 Some examples:
 "0" => true
@@ -6,46 +6,60 @@ Some examples:
 "abc" => false
 "1 a" => false
 "2e10" => true
-Note: It is intended for the problem statement to be ambiguous.
-    You should gather all requirements up front before implementing one.
+
+Note:
+It is intended for the problem statement to be ambiguous.
+You should gather all requirements up front before implementing one.
+
+Constraints:
+1 <= s.length <= 20
+s consists of only English letters, digits, space ' ', plus '+', minus '-', or dot '.'.
 
 Cases need to care:
-    1. digit
-    2. point. prevous and next situation
-    3. 'e'.   prevous and next situation
-*/
+1. digit
+2. point. prevous and next situation
+3. 'e'.   prevous and next situation */
 
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 bool isNumber(string s) {
-
     if (s == "" || s == " ")
         return false;
+
     int len = s.size();
     int i = 0;
     bool isFloat = false;
     bool isE = false;
 
-    while (s[i] == ' ') ++i;            // disgard prefix and postfix space
-    if (s[i] == '+' || s[i] == '-') ++i;
-    if (s[i] == 'e') return false;      // "00" ok
-    while (s[len - 1] == ' ') --len;
+    while (s[i] == ' ')
+        ++i;        // disgard prefix and postfix space
+    if (s[i] == '+' || s[i] == '-')
+        ++i;
+    if (s[i] == 'e')
+        return false;      // "00" ok
+    while (s[len - 1] == ' ')
+        --len;
 
     int head = i;   // position of the first valid digit
     while (i < len) {
-        if (isdigit(s[i])) ++i;
-        else {
+        if (isdigit(s[i])) {
+            ++i;
+        } else {
             if (s[i] == 'e') {
-                if (isE || i == head) return false; // prefix issue
+                if (isE || i == head)
+                    return false;   // prefix issue
                 ++i;
-                if (s[i] == '+' ||s[i] == '-') ++i;
-                if (!isdigit(s[i])) return false;   // postfix issue
+                if (s[i] == '+' ||s[i] == '-')
+                    ++i;
+                if (!isdigit(s[i]))
+                    return false;   // postfix issue
                 isE = true;
             } else if (s[i++] == '.') {
                 if (isFloat || isE || !isdigit(s[i]) && head == i - 1)
-                    return false;                   // prefix and posefix issue
+                    return false;   // prefix and posefix issue
                 isFloat = true;
             } else
                 return false;
