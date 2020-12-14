15c13361a0bad1579cf230b548ebb25d4d262753
Jemmy <jemmy1029760725@gmail.com>
2020-12-15 01:30:17 +0800

    Update 560.SubarraySumEqualsK.cc

--- Algorithm/LeetCode/Array/560.SubarraySumEqualsK.cc
+++ Algorithm/LeetCode/Array/560.SubarraySumEqualsK.cc
@@ -1,18 +1,16 @@
 /* Medium
-Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+Given an array of integers nums and an integer k,
+return the total number of continuous subarrays whose sum equals to k.
 
 Example 1:
-
 Input: nums = [1,1,1], k = 2
 Output: 2
 
 Example 2:
-
 Input: nums = [1,2,3], k = 3
 Output: 2
 
 Constraints:
-
 1 <= nums.length <= 2 * 104
 -1000 <= nums[i] <= 1000
 -107 <= k <= 107
@@ -22,6 +20,7 @@ Relative:
 152. Maximum Product Subarry
 437.PathSum_III
 523. Continuous Subarray Sum
+325. Maximum Size Subarray Sum Equals k
 560. Sub array Sum Equals K */
 
 #include <unordered_map>
@@ -34,16 +33,30 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> hashMap;
-        int curSum = 0;
+        hashMap[0] = 1; // sum == k
+        int sum = 0;
         int cnt = 0;
 
         for (const auto& num : nums) {
-            curSum += num;
+            sum += num;
+            cnt += hashMap[sum-k];
+            ++hashMap[sum];
+        }
 
-            if (curSum == k)
+        return cnt;
+    }
+
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> hashMap;
+        int sum = 0;
+        int cnt = 0;
+
+        for (const auto& num : nums) {
+            sum += num;
+            if (sum == k)
                 ++cnt;
-            cnt += hashMap[curSum-k];
-            ++hashMap[curSum];
+            cnt += hashMap[sum-k];
+            ++hashMap[sum];
         }
 
         return cnt;
