2072fbc5e816f8078541629b5db419da6a7e6069
Jemmy <jemmy1029760725@gmail.com>
2020-12-15 22:54:38 +0800

    Update 304.RangeSumQuery2D_Immutable.cc

--- Algorithm/LeetCode/DP/304.RangeSumQuery2D_Immutable.cc
+++ Algorithm/LeetCode/DP/304.RangeSumQuery2D_Immutable.cc
@@ -1,9 +1,9 @@
-/*
-Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by 
+/* Medium
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by
 its upper left corner (row1, col1) and lower right corner (row2, col2).
 
 Range Sum Query 2D
-The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) 
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1)
 and (row2, col2) = (4, 3), which contains sum = 8.
 
 Example:
@@ -22,57 +22,77 @@ Note:
 You may assume that the matrix does not change.
 There are many calls to sumRegion function.
 You may assume that row1 ≤ row2 and col1 ≤ col2.
-*/
 
-#include<iostream>
-#include<vector>
+Relatives:
+303. Range Sum Query Immutable
+307. Range Sum Query Mutable
+304. Range Sum Query 2D Immutable */
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-// 12 test cases, 24ms, beat 62.05%
+
+/* Construct a 2D array `sums[row+1][col+1]`
+ *
+ * (**notice**: we add additional blank row `sums[0][col+1]={0}` and blank column `sums[row+1][0]={0}`
+ * to remove the edge case checking), so, we can have the following definition
+ *
+ *  `sums[i+1][j+1]` represents the sum of area from `matrix[0][0]` to `matrix[i][j]`
+ *
+ * To calculate sums, the ideas as below
+ *
+ *     +-----+-+-------+     +--------+-----+     +-----+---------+     +-----+--------+
+ *     |     | |       |     |        |     |     |     |         |     |     |        |
+ *     |     | |       |     |        |     |     |     |         |     |     |        |
+ *     +-----+-+       |     +--------+     |     |     |         |     +-----+        |
+ *     |     | |       |  =  |              |  +  |     |         |  -  |              |
+ *     +-----+-+       |     |              |     +-----+         |     |              |
+ *     |               |     |              |     |               |     |              |
+ *     |               |     |              |     |               |     |              |
+ *     +---------------+     +--------------+     +---------------+     +--------------+
+ *
+ *        sums[i][j]      =    sums[i-1][j]    +     sums[i][j-1]    -   sums[i-1][j-1]   +
+ *
+ *                             matrix[i-1][j-1]
+ *
+ * So, we use the same idea to find the specific area's sum.
+ *
+ *     +---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+ *     |               |   |         |    |   |   |           |   |         |    |   |   |          |
+ *     |   (r1,c1)     |   |         |    |   |   |           |   |         |    |   |   |          |
+ *     |   +------+    |   |         |    |   |   |           |   +---------+    |   +---+          |
+ *     |   |      |    | = |         |    | - |   |           | - |      (r1,c2) | + |   (r1,c1)    |
+ *     |   |      |    |   |         |    |   |   |           |   |              |   |              |
+ *     |   +------+    |   +---------+    |   +---+           |   |              |   |              |
+ *     |        (r2,c2)|   |       (r2,c2)|   |   (r2,c1)     |   |              |   |              |
+ *     +---------------+   +--------------+   +---------------+   +--------------+   +--------------+ */
 class NumMatrix {
-private:
-    vector<vector<int>> data;
 public:
     NumMatrix(vector<vector<int>> matrix) {
-        int row = matrix.size();
-        int col = row > 0 ? matrix[0].size() : 0;
-        ++row;
-        ++col;
-        data = vector<vector<int>>(row, vector<int>(col, 0));
-        for (int i = 1; i < row; ++i) {
-            for (int j = 1; j < col; ++j) {
-                data[i][j] = data[i][j-1] + data[i-1][j] + matrix[i-1][j-1] - data[i-1][j-1];
+        if (matrix.empty() || matrix[0].empty())
+            return;
+
+        int rowSize = matrix.size() + 1;
+        int colSize = matrix[0].size() + 1;
+        data = vector<vector<int>>(rowSize, vector<int>(colSize, 0));
+
+        for (int row = 1; row < rowSize; ++row) {
+            for (int col = 1; col < colSize; ++col) {
+                data[row][col] = data[row][col-1] + data[row-1][col] + matrix[row-1][col-1] - data[row-1][col-1];
             }
         }
     }
-    
-    void print() {
-        for (int i = 0; i < data.size(); ++i) {
-            for (int j = 0; j < data[0].size(); ++j) {
-                printf("%4d ", data[i][j]);
-            }
-            cout << endl;
-        }
-    }
-    
+
     int sumRegion(int row1, int col1, int row2, int col2) {
         return data[row2+1][col2+1] - data[row2+1][col1] - data[row1][col2+1] + data[row1][col1];
     }
+
+private:
+    vector<vector<int>> data;
 };
-/**
- * Your NumMatrix object will be instantiated and called as such:
+
+/* Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
- 
-int main() {
-    vector<vector<int>> vec = {
-        {3, 0, 1, 4, 2},
-        {5, 6, 3, 2, 1},
-        {1, 2, 0, 1, 5},
-        {4, 1, 0, 1, 7},
-        {1, 0, 3, 0, 5}
-    };
-    NumMatrix na(vec);
-    na.print();
-}
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2); */
