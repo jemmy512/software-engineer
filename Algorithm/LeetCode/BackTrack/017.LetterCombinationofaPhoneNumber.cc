974c683b7ef9538f313fb6ae65c482b2d1867829
Jemmy <jemmy1029760725@gmail.com>
2020-12-16 23:14:29 +0800

    Update 017.LetterCombinationofaPhoneNumber.cc

--- Algorithm/LeetCode/BackTrack/017.LetterCombinationofaPhoneNumber.cc
+++ Algorithm/LeetCode/BackTrack/017.LetterCombinationofaPhoneNumber.cc
@@ -1,19 +1,20 @@
-/*
+/* Medium
 Given a string containing digits from 2-9 inclusive, return all possible letter
 combinations that the number could represent.
 
 A mapping of digit to letters (just like on the telephone buttons) is given below.
- Note that 1 does not map to any letters.
-
+Note that 1 does not map to any letters.
 
 Example:
-
 Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 Note:
 
 Although the above answer is in lexicographical order, your answer could be in any order you want
-*/
+
+017. Letter Combination of a Phone Number
+020. validParentheses
+022. Generate Parentheses */
 
 #include <string>
 #include <vector>
@@ -26,33 +27,31 @@ using std::unordered_map;
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> vec;
-        string str;
-
-        combine(digits, 0, str, vec);
-
-        return vec;
+        string curStr;
+        combination(digits, 0, curStr);
+        return result;
     }
 
 private:
-    void combine(const string& digits, int idx, string& strCur, vector<string>& vec) {
-        if (idx < digits.size()) {
-            string str = hashMap[digits[idx]];
-            for (int j = 0; j < str.size(); ++j) {
-                strCur += str[j];
-                combine(digits, idx + 1, strCur, vec);
-                if (idx == digits.size() - 1) {
-                    vec.push_back(strCur);
-                }
-                strCur.pop_back();
-            }
+    void combination(const string& digits, int idx, string& curStr) {
+        if (idx >= digits.size())
+            return;
+
+        string button = hashMap[digits[idx]];
+        for (int i = 0; i < button.size(); ++i) {
+            curStr += button[i];
+            combination(digits, idx+1, curStr);
+            if (idx == digits.size()-1)
+                result.emplace_back(curStr);
+            curStr.pop_back();
         }
     }
 
+    vector<string> result;
     unordered_map<char, string> hashMap {
         {'2', "abc"},
         {'3', "def"},
-        {'4', "hgi"},
+        {'4', "ghi"},
         {'5', "jkl"},
         {'6', "mno"},
         {'7', "pqrs"},
