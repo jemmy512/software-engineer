501efd25792ce5cb5b6d5c3726851ef9ab6c3b37
Jemmy <jemmy1029760725@gmail.com>
2020-12-18 00:43:41 +0800

    Update 082.RemoveDuplicatesfromSortedList_II.cc

--- Algorithm/LeetCode/Array/082.RemoveDuplicatesfromSortedList_II.cc
+++ Algorithm/LeetCode/Array/082.RemoveDuplicatesfromSortedList_II.cc
@@ -1,31 +1,55 @@
-/*
-Difficulty: Medium
-
-Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+/* Medium
+Given a sorted linked list, delete all nodes that have duplicate numbers,
+leaving only distinct numbers from the original list.
 
 Example 1:
-
 Input: 1->2->3->3->4->4->5
 Output: 1->2->5
+
 Example 2:
-
 Input: 1->1->1->2->3
-Output: 2->3
-*/
+Output: 2->3 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head)
+            return nullptr;
+
+        ListNode dummy{0, head};
+        ListNode* pre = &dummy, *dup = head, *cur = head;
+
+        while (cur) {
+            if (cur->val != dup->val) {
+                if (dup->next == cur) {
+                    pre = pre->next = dup;
+                    dup = cur;
+                } else {
+                    pre->next = dup = cur;
+                }
+            }
+            cur = cur->next;
+        }
+        if (dup->next != nullptr) // [1, 1]
+            pre->next = nullptr;
+
+        return dummy.next;
+    }
+};
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (!head) return head;
-        
+
         long duplicate;
         ListNode dummy(0);
         ListNode *first = &dummy, *second = head;
@@ -41,7 +65,7 @@ public:
             second = second->next;
         }
         first->next = second;
-        
+
         return dummy.next;
     }
 };
