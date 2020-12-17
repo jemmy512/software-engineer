2e1b5bb7cb47d666222bd61f36c2c030978b96f9
Jemmy <jemmy1029760725@gmail.com>
2020-12-18 00:43:31 +0800

    Update 080.RemoveDuplicatefromSortedArray_II.cc

--- Algorithm/LeetCode/Array/080.RemoveDuplicatefromSortedArray_II.cc
+++ Algorithm/LeetCode/Array/080.RemoveDuplicatefromSortedArray_II.cc
@@ -1,39 +1,60 @@
-/*
-Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+/* Medium
+Given a sorted array nums, remove the duplicates in-place such that
+duplicates appeared at most twice and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+Do not allocate extra space for another array, you must do this
+by modifying the input array in-place with O(1) extra memory.
 
 Example 1:
-
 Given nums = [1,1,1,2,2,3],
-
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
-
 It doesn't matter what you leave beyond the returned length.
+
 Example 2:
-
 Given nums = [0,0,1,1,1,1,2,3,3],
-
-Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+Your function should return length = 7, with the first seven elements of nums being modified
+to 0, 0, 1, 1, 2, 3 and 3 respectively.
 
 It doesn't matter what values are set beyond the returned length.
 Clarification:
-
 Confused why the returned value is an integer but your answer is an array?
-
-Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Note that the input array is passed in by reference, which means modification to the
+input array will be known to the caller as well.
 
 Internally you can think of this:
-
 // nums is passed in by reference. (i.e., without making a copy)
 int len = removeDuplicates(nums);
-
 // any modification to nums in your function would be known by the caller.
 // using the length returned by your function, it prints the first len elements.
 for (int i = 0; i < len; i++) {
     print(nums[i]);
-}
-*/
+} */
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int newIdx = 1;
+        int count = 1;
+
+        for (int idx = 1; idx < nums.size(); ++idx) {
+            if (nums[idx] == nums[idx-1]) {
+                ++count;
+            } else {
+                count = 1;
+            }
+
+            if (count <= 2) {
+                nums[newIdx++] = nums[idx];
+            }
+        }
+
+        return newIdx;
+    }
+};
 
 class Solution {
 public:
@@ -41,7 +62,7 @@ public:
         int len = nums.size(), duplicate = INT_MIN;
         int index = 0;
         for (int i = 0; i < len;) {
-            if (nums[i] == nums[i+1] && nums[i] != duplicate) {
+            if (i+1 < len && nums[i] == nums[i+1] && nums[i] != duplicate) {
                 duplicate = nums[i];
                 nums[index++] = nums[i];
                 nums[index++] = nums[i+1];
