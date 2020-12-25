446644b0b2160a4a667ed1cd1db7c03443c6ee9b
Jemmy <jemmy1029760725@gmail.com>
2020-12-25 20:04:37 +0800

    Update 169.MajorityElement.cc

--- Algorithm/LeetCode/Array/169.MajorityElement.cc
+++ Algorithm/LeetCode/Array/169.MajorityElement.cc
@@ -1,50 +1,48 @@
-/*
-Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+/* Easy
+Given an array of size n, find the majority element.
+The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
 You may assume that the array is non-empty and the majority element always exist in the array.
 
 Credits:
 Special thanks to @ts for adding this problem and creating all test cases.
-*/
-#include<iostream>
-#include<vector>
-#include<map>
+ou may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2 */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
 
-// 44/44 test cases, 19ms, beat 97.24%
-class Solution {
-public:
-    static int majorityElement(vector<int>& nums) {
-        map<int, int> data;
-        int len = nums.size();
-        for (int i = 0; i < len; ++i) {
-            if (++data[nums[i]] > len / 2)
-                return nums[i];
-        }
-    }
-    
-// A Linear Time Majority Vote Algorithm  
-// http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html    
-    static int majorityElement_(vector<int> &nums) {
-        int majority;
-        int cnt = 0;
-        int len = nums.size();
-        for (int i = 0; i < len; ++i) {
-            if (cnt == 0) {
-                majorit = nums[i];
-                ++cnt;
-            } else {
-                majority = nums[i] == majority ? ++cnt : --cnt;
-                // if (cnt > len / 2) return majority;
-            }
-        }
-        
-        return majority;
-    }
-};
+/* Boyer-Moore Voting Algorithm Linear Time Majority Vote Algorithm
+ * http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html */
+int majorityElement(vector<int> &nums) {
+    int majority;
+    int cnt = 0;
 
-int main() {
-    vector<int> vec = {1,2,3,2,2,2,4,2,2,4};
-    vector<int> ve = {1};
-    cout << Solution::majorityElement(vec);
+    for (const auto& n : nums) {
+        if (cnt == 0) {
+            majority = n;
+        }
+        cnt += (n == majority) ? 1 : -1;
+    }
+
+    return majority;
+}
+
+int majorityElement(vector<int>& nums) {
+    unordered_map<int, int> hashMap;
+    int len = nums.size();
+    for (int i = 0; i < len; ++i) {
+        if (++hashMap[nums[i]] > len / 2)
+            return nums[i];
+    }
 }
