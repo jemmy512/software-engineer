f0e5aafccf6d2e441af70e361febbcd8349a3a04
Jemmy <jemmy1029760725@gmail.com>
2020-12-25 20:04:33 +0800

    Update 088.MergeSortedArray.cc

--- Algorithm/LeetCode/Array/088.MergeSortedArray.cc
+++ Algorithm/LeetCode/Array/088.MergeSortedArray.cc
@@ -1,18 +1,45 @@
-/*
+/* Easy
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-Note:
-You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold 
-additional elements from nums2. The number of elements initialized in nums1 and nums2 are
- m and n respectively.
-*/
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<iterator>
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold
+additional elements from nums2. The number of elements initialized in nums1 and nums2 are
+m and n respectively.
+
+Example:
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+Output: [1,2,2,3,5,6]
+
+Constraints:
+-10^9 <= nums1[i], nums2[i] <= 10^9
+nums1.length == m + n
+nums2.length == n */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
+/* Two pointers / Start from the end */
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    nums1.reserve(nums1.size() + nums2.size());
+    int i = m - 1, j = n - 1;
+    int k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+    }
+
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+}
+
+/* Two pointers / Start from the begining */
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     vector<int> tmp(m + n);
     int i = 0;
@@ -35,7 +62,7 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     while (j < n)
         tmp[k++] = nums2[j++];
     nums1.clear();
-    copy(tmp.begin(), tmp.end(), back_inserter(nums1));   
+    copy(tmp.begin(), tmp.end(), back_inserter(nums1));
 }
 
 int main() {
