e343839492b58979cd0ef28a875207542270833f
Jemmy <jemmy1029760725@gmail.com>
2020-12-28 22:56:53 +0800

    Create 253.MeetingRoomsII.cc

---
+++ Algorithm/LeetCode/Queue/253.MeetingRoomsII.cc
@@ -0,0 +1,134 @@
+/* Medium
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+
+Example 2:
+Input: [[7,10],[2,4]]
+Output: 1
+
+Relatives:
+056. Merge Intervals
+252. Meeting Rooms
+253. Metting Rooms II
+452. Minimum Number of Arrows to Burst Balloons
+1094. Car Pooling */
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/* Priority Queue
+ * Time complexity: O(NlogN) */
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return 0;
+
+        std::sort(intervals.begin(), intervals.end(), [](const auto& lhs, const auto& rhs) {
+            return lhs[0] < rhs[0];
+        });
+
+        std::priority_queue<int, vector<int>, std::greater<int>> que;
+
+        for (const auto& inter : intervals) {
+            if (!que.empty() && inter[0] >= que.top()) {
+                que.pop();
+            }
+            que.emplace(inter[1]);
+        }
+
+        return que.size();
+    }
+};
+
+/* Chronological Ordering
+ * Time complexity: O(NlogN) */
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return 0;
+
+        vector<int> startTimes;
+        startTimes.reserve(intervals.size());
+        vector<int> endTimes;
+        endTimes.reserve(intervals.size());
+
+        for (const auto& inter : intervals) {
+            startTimes.emplace_back(inter[0]);
+            endTimes.emplace_back(inter[1]);
+        }
+
+        std::sort(startTimes.begin(), startTimes.end(), std::less<int>());
+        std::sort(endTimes.begin(), endTimes.end(), std::less<int>());
+
+        int start = 0, end = 0;
+        int rooms = 0;
+        while (start < startTimes.size()) {
+            if (startTimes[start] >= endTimes[end]) {
+                ++end;
+            } else {
+                ++rooms;
+            }
+            ++start;
+        }
+
+        return rooms;
+    }
+};
+
+
+/* Iterator all rooms:
+ * Time complexity: O(N^2) */
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return 0;
+
+        std::sort(intervals.begin(), intervals.end(), [](const auto& lhs, const auto& rhs) {
+            return lhs[0] < rhs[0];
+        });
+
+        vector<pair<int, int>> rooms;
+        rooms.reserve(intervals.size());
+        rooms.emplace_back(intervals[0][0], intervals[0][1]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            bool added = false;
+            for (auto& [start, end] : rooms) {
+                if (intervals[i][0] >= end) {
+                    end = intervals[i][1];
+                    added = true;
+                    break;
+                }
+            }
+
+            if (!added) {
+                rooms.emplace_back(intervals[i][0], intervals[i][1]);
+            }
+        }
+
+        return rooms.size();
+    }
+};
+
+int main() {
+    vector<vector<int>> intervals{
+        {1, 10},
+        {2, 7},
+        {3, 19},
+        {8, 12},
+        {10, 20},
+        {11, 30}
+    };
+
+    Solution s;
+    s.minMeetingRooms(intervals);
+}
