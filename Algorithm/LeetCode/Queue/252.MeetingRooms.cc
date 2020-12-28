ecc4871aee60401d90811ad8103c28dc0a4497e6
Jemmy <jemmy1029760725@gmail.com>
2020-12-28 22:56:49 +0800

    Create 252.MeetingRooms.cc

---
+++ Algorithm/LeetCode/Queue/252.MeetingRooms.cc
@@ -0,0 +1,43 @@
+/* Easy
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
+determine if a person could attend all meetings.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+Input: intervals = [[7,10],[2,4]]
+Output: true
+
+Constraints:
+0 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti < endi <= 10^6
+
+Relatives:
+056. Merge Intervals
+252. Meeting Rooms
+253. Metting Rooms II
+452. Minimum Number of Arrows to Burst Balloons */
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](const auto& lhs, const auto& rhs) {
+            return lhs[0] < rhs[0];
+        });
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] < intervals[i-1][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
