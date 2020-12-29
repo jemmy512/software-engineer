bf4663bb6faf9d4850844a470aec6bfc148e8993
Jemmy <jemmy1029760725@gmail.com>
2020-12-30 01:15:43 +0800

    Create 763.PartitionLabels.cc

---
+++ Algorithm/LeetCode/Greedy/763.PartitionLabels.cc
@@ -0,0 +1,49 @@
+/* Medium
+A string S of lowercase English letters is given.
+We want to partition this string into as many parts as possible
+so that each letter appears in at most one part,
+and return a list of integers representing the size of these parts.
+
+Example 1:
+Input: S = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+
+Note:
+S will have length in range [1, 500].
+S will consist of lowercase English letters ('a' to 'z') only.
+
+Relatives:
+56. Merge Intervals */
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        vector<int> index(26);
+        vector<int> result;
+        result.reserve(S.size());
+        int start = 0, end = 0;
+
+        for (int i = 0; i < S.size(); ++i) {
+            index[S[i]-'a'] = i;
+        }
+
+        for (int i = 0; i < S.size(); ++i) {
+            end = max(end, index[S[i]-'a']);
+            if (i == end) {
+                result.emplace_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+};
