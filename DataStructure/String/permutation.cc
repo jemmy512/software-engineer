b85f1a09baa1f241f0cc6bd8cc06ce7b18a2fa20
Jemmy <jemmy1029760725@gmail.com>
2021-03-06 16:07:23 +0800

    Update permutation.cc

--- DataStructure/String/permutation.cc
+++ DataStructure/String/permutation.cc
@@ -1,13 +1,11 @@
-/*
- * Created by by on 2018/8/13.
- * Ref: https://segmentfault.com/a/1190000002710424
- */
+/* Created by by on 2018/8/13.
+ * Ref: https://segmentfault.com/a/1190000002710424 */
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-bool isSwap(const string &str, int beg, int end) {
+bool doSwap(const string &str, int beg, int end) {
     for (; beg < end; ++beg) {
         if (str[beg] == str[end])
             return false;
@@ -15,21 +13,62 @@ bool isSwap(const string &str, int beg, int end) {
     return true;
 }
 
-void stringPermuation(string &str, int beg, int len) {
+void permute(string &str, int beg, int len) {
     if (beg + 1 == len) {
         cout << str << endl;
-    } else {
-        for (int i = beg; i < len; ++i) {
-            if (isSwap(str, beg, i)) {
-                swap(str[beg], str[i]);
-                stringPermuation(str, beg+1, len);
-                swap(str[beg], str[i]);
-            }
+        return;
+    }
+
+    for (int i = beg; i < len; ++i) {
+        if (doSwap(str, beg, i)) {
+            swap(str[beg], str[i]);
+            permute(str, beg+1, len);
+            swap(str[beg], str[i]);
         }
     }
 }
 
 int main() {
     string str = "bbc";
-    stringPermuation(str, 0, str.size());
+    permute(str, 0, str.size());
 }
+
+/*
+beg = 0
+    i = 0
+        s: [a]-b-c
+        p: beg = 1
+            i = 1
+                s: a-[b]-c
+                p: beg = 2  ---> a-b-c
+                s: a-[b]-c
+            i = 2
+                s: a-[c]-[b]
+                p: beg = 2  ---> a-c-b
+                s: a-[b]-[c]
+        s: [a]-b-c
+    i = 1
+        s: [b]-[a]-c
+        p: beg = 1
+            i = 1
+                s: b-[a]-c
+                p: beg = 2  ---> b-a-c
+                s: b-[a]-c
+            i = 2
+                s: b-[c]-[a]
+                p: beg = 2  ---> b-c-a
+                s: [a]-[b]-[c]
+        s: [a]-[b]-c
+    i = 2
+        s: [c]-b-[a]
+        p: beg = 1
+            i = 1
+                s: c-[b]-a
+                p: beg = 2  ---> c-b-a
+                s: c-[b]-a
+            i = 2
+                s: c-[a]-[b]
+                p: beg = 2  ---> c-a-b
+                s: c-[b]-[c]
+        s: [a]-b-[c]
+*/
