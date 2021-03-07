d8c9916be0570d6533c953274a56c385739926bd
Jemmy <jemmy1029760725@gmail.com>
2021-03-07 19:08:16 +0800

    Update 263.UglyNumber.cc

--- Algorithm/LeetCode/Math/263.UglyNumber.cc
+++ Algorithm/LeetCode/Math/263.UglyNumber.cc
@@ -1,19 +1,14 @@
-/*
-Difficulty: Easy
-
-Reletive: 264.UglyNumber_II.cc
-
+/* Easy
 Write a program to check whether a given number is an ugly number.
 
 Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
 
 Example 1:
-
 Input: 6
 Output: true
 Explanation: 6 = 2 × 3
-Example 2:
 
+Example 2:
 Input: 8
 Output: true
 Explanation: 8 = 2 × 2 × 2
@@ -26,19 +21,23 @@ Note:
 
 1 is typically treated as an ugly number.
 Input is within the 32-bit signed integer range: [−231,  231 − 1].
-*/
+
+Reletive:
+264.UglyNumber_II.cc */
+
 #include <vector>
 #include <algorithm>
 
 class Solution {
 public:
     bool isUgly(int num) {
-        if (num < 1) return false;
+        if (num < 1)
+            return false;
 
-        std::vector<int> divisors{2, 3, 5};
-        std::for_each(divisors.begin(), divisors.end(), [](const auto& divisor) {
-            while (num % i == 0) {
-                num /= i;
+        auto divisors = {2, 3, 5};
+        std::for_each(divisors.begin(), divisors.end(), [&num](const auto& div) {
+            while (num % div == 0) {
+                num /= div;
             }
         });
 
