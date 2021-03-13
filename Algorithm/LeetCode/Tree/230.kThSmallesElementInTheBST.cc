d144838ce1d44ea470b2e8276bd67d08553b5453
Jemmy <jemmy1029760725@gmail.com>
2021-03-13 21:08:49 +0800

    Update 230.kThSmallesElementInTheBST.cc

--- Algorithm/LeetCode/Tree/230.kThSmallesElementInTheBST.cc
+++ Algorithm/LeetCode/Tree/230.kThSmallesElementInTheBST.cc
@@ -1,11 +1,10 @@
-/**
- * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+/* Medium
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
 
 Note:
 You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
 
 Example 1:
-
 Input: root = [3,1,4,null,2], k = 1
    3
   / \
@@ -23,18 +22,19 @@ Input: root = [5,3,6,2,4,null,null,1], k = 3
    2   4
   /
  1
-Output: 3
-*/
+Output: 3 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
