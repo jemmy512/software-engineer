1d8b74e649aa6dcf453fafaaf9eb42cae26e2bda
Jemmy <jemmy1029760725@gmail.com>
2021-03-14 19:07:30 +0800

    Update 167.TwoSumII-InputArrayisSoretd.cc

--- Algorithm/LeetCode/Map/167.TwoSumII-InputArrayisSoretd.cc
+++ Algorithm/LeetCode/Map/167.TwoSumII-InputArrayisSoretd.cc
@@ -1,7 +1,10 @@
 /* Easy
-Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+Given an array of integers that is already sorted in ascending order,
+find two numbers such that they add up to a specific target number.
 
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+The function twoSum should return indices of the two numbers such that they add up to the target,
+where index1 must be less than index2. Please note that your returned answers
+(both index1 and index2) are not zero-based.
 
 You may assume that each input would have exactly one solution and you may not use the same element twice.
 
@@ -26,6 +29,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int low = 0, high = numbers.size() - 1;
+
         while (low < high) {
             int sum = numbers[low] + numbers[high];
             if (sum == target)
