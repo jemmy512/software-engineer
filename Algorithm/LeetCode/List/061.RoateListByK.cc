aa612964b2ba344f4e6c84b93e3160f2fe204cc5
Jemmy <jemmy1029760725@gmail.com>
2021-03-19 00:02:48 +0800

    Update 061.RoateListByK.cc

--- Algorithm/LeetCode/List/061.RoateListByK.cc
+++ Algorithm/LeetCode/List/061.RoateListByK.cc
@@ -1,51 +1,65 @@
-/*
+/* Medium
 Given a linked list, rotate the list to the right by k places, where k is non-negative.
 
 Example 1:
-
 Input: 1->2->3->4->5->NULL, k = 2
 Output: 4->5->1->2->3->NULL
 Explanation:
 rotate 1 steps to the right: 5->1->2->3->4->NULL
 rotate 2 steps to the right: 4->5->1->2->3->NULL
-Example 2:
 
+Example 2:
 Input: 0->1->2->NULL, k = 4
 Output: 2->0->1->NULL
 Explanation:
 rotate 1 steps to the right: 2->0->1->NULL
 rotate 2 steps to the right: 1->2->0->NULL
 rotate 3 steps to the right: 0->1->2->NULL
-*/
+rotate 4 steps to the right: 2->0->1->NULL
+
+Constraints:
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 10^9
+
+Relatives:
+061. Roate List By K
+189. Rotate Array
+725. Split Linked List in Parts */
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// 231 test cases, 16ms, beat 33.70%
-ListNode* rotateRight(ListNode* head, int k) {
-    if(!head) return head;
+/* move k steps from right to left == move (len-k) steps from left to right */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head)
+            return head;
 
-    int len = 1; // number of nodes
-    ListNode *newH, *tail;
-    newH = tail = head;
+        auto len = 1; // 0 is error
+        auto* tail = head;
 
-    while(tail->next) { // get the number of nodes in the list
-        tail = tail->next;
-        len++;
+        while (tail->next) {
+            ++len;
+            tail = tail->next;
+        }
+        tail->next = head;
+
+        if (k %= len) {
+            for (auto i = 0; i < len-k; ++i) {
+                tail = tail->next;
+            }
+        }
+        auto* newHead = tail->next;
+        tail->next = nullptr;
+
+        return newHead;
     }
-    tail->next = head; // circle the link
-
-    if (k %= len) {
-        for(int i = 0; i < len - k; i++)
-            tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
-    }
-    newH = tail->next;
-    tail->next = NULL;
-    return newH;
-}
+};
 
 // beat 33.70%
 ListNode* rotateRight_1(ListNode* head, int k) {
