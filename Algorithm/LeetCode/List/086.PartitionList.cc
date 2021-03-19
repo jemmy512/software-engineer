dfea724d6af185bef2ad86b3e9d515455b2ecc43
Jemmy <jemmy1029760725@gmail.com>
2021-03-19 20:38:22 +0800

    Update 086.PartitionList.cc

--- Algorithm/LeetCode/List/086.PartitionList.cc
+++ Algorithm/LeetCode/List/086.PartitionList.cc
@@ -1,41 +1,43 @@
-/*
-Difficulty: Medium
-
-Given a linked list and a value x, partition it such that all nodes less than x 
-    come before nodes greater than or equal to x.
+/* Medium
+Given a linked list and a value x, partition it such that
+all nodes less than x come before nodes greater than or equal to x.
 
 You should preserve the original relative order of the nodes in each of the two partitions.
 
 Example:
-
 Input: head = 1->4->3->2->5->2, x = 3
 Output: 1->2->2->4->3->5
-*/
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+Constraints:
+The number of nodes in the list is in the range [0, 200].
+-100 <= Node.val <= 100
+-200 <= x <= 200 */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode hd(0), tail(0);
-        ListNode *ph = &hd, *pt = &tail, *p = head;
-        while (p) {
-            if (p->val < x) {
-                ph = ph->next = p;
+        ListNode less, great;
+        auto* tailLess = &less;
+        auto* tailGreat = &great;
+
+        while (head) {
+            if (head->val < x) {
+                tailLess = tailLess->next = head;
             } else {
-                pt = pt->next = p;
+                tailGreat = tailGreat->next = head;
             }
-            p = p->next;
+            head = head->next;
         }
-        
-        ph->next = tail.next;
-        pt->next = NULL;
-        return hd.next;
+
+        tailLess->next = great.next;
+        tailGreat->next = nullptr;
+
+        return less.next;
     }
 };
