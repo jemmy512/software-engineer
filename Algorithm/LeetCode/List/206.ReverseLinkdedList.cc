4e2ed3caf21b1d20961b6f1957ad52ccda068616
Jemmy <jemmy1029760725@gmail.com>
2021-03-19 20:38:26 +0800

    Update 206.ReverseLinkdedList.cc

--- Algorithm/LeetCode/List/206.ReverseLinkdedList.cc
+++ Algorithm/LeetCode/List/206.ReverseLinkdedList.cc
@@ -1,11 +1,14 @@
-/*
+/* Easy
 Reverse a singly linked list.
 
 click to show more hints.
 
 Hint:
 A linked list can be reversed either iteratively or recursively. Could you implement both?
-*/
+
+Relatives:
+206. Reverse Linked List
+092. Reverse Linked List II */
 
 struct ListNode {
     int val;
@@ -13,19 +16,35 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-// beat 98.94%
-ListNode* reverseList(ListNode* head) {
-    if (head == NULL) return NULL;
-    ListNode *first = head->next;
-    ListNode *last = NULL;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        auto* fast = head;
+        auto* mid = head;
+        ListNode* slow = nullptr;
 
-    while (first != NULL) {
-        head->next = last;
-        last = head;
-        head = first;
-        first = first->next;
+        while (mid) {
+            fast = mid->next;
+            mid->next = slow;
+            slow = mid;
+            mid = fast;
+        }
+
+        return slow;
     }
-    head->next = last;
+};
 
-    return head;
-}
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        auto* prev = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return prev;
+    }
+};
