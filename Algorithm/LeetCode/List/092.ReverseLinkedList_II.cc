4ba2214c0a606a403a041cb5851b1c171b4ab61e
Jemmy <jemmy1029760725@gmail.com>
2021-03-19 20:38:24 +0800

    Update 092.ReverseLinkedList_II.cc

--- Algorithm/LeetCode/List/092.ReverseLinkedList_II.cc
+++ Algorithm/LeetCode/List/092.ReverseLinkedList_II.cc
@@ -1,16 +1,22 @@
-/*
-Difficulty: Medium
+/* Medium
 
 Reverse a linked list from position m to n. Do it in one-pass.
 
 Note: 1 ≤ m ≤ n ≤ length of list.
 
 Example:
-
 Input: 1->2->3->4->5->NULL, m = 2, n = 4
 Output: 1->4->3->2->5->NULL
 
-*/
+Constraints:
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+
+Relatives:
+206. Reverse Linked List
+092. Reverse Linked List II */
 
 struct ListNode {
     int val;
@@ -27,17 +33,17 @@ public:
 
         ListNode dummy(0);
         dummy.next = head;
-        ListNode *first = dummy.next, *last = &dummy, *node = nullptr;
+        ListNode *fast = dummy.next, *slow = &dummy, *node = nullptr;
 
         while (--m && --n)
-            last = last->next;
-        first = last->next;
+            slow = slow->next;
+        fast = slow->next;
 
         while (--n) {
-            node = first->next;
-            first->next = node->next;
-            node->next = last->next;
-            last->next = node;
+            node = fast->next;
+            fast->next = node->next;
+            node->next = slow->next;
+            slow->next = node;
         }
 
         return dummy.next;
