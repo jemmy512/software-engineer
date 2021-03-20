7cc64589b5cf116a44f948c771b0e0782b2d163b
Jemmy <jemmy1029760725@gmail.com>
2021-03-20 19:27:23 +0800

    Update 141.linkedListCycle.cc

--- Algorithm/LeetCode/List/141.linkedListCycle.cc
+++ Algorithm/LeetCode/List/141.linkedListCycle.cc
@@ -1,29 +1,39 @@
-/*
-Given a linked list, determine if it has a cycle in it.
+/* Easy
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-Follow up:
-Can you solve it without using extra space?
+There is a cycle in a linked list if there is some node in the list
+that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer
+is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Constraints:
+The number of the nodes in the list is in the range [0, 10^4].
+-10^5 <= Node.val <= 10^5
+pos is -1 or a valid index in the linked-list.
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
 Relative:
-142 Linked List Cycle II
-287 Findthe Duplicate Number */
+141. Linked List Cycle
+142. Linked List Cycle II
+287. Findthe Duplicate Number */
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// beat 98.87%
-// take care the cases: m may NULL and m->next may be NULL(ignore this case cause run time error)
 bool hasCycle(ListNode *head) {
-    ListNode *m = head;
-    ListNode *n = head;
-    while (m != NULL && m->next != NULL) {
-        n = n->next;
-        m = m->next->next;
+    auto *fast = head;
+    auto *slow = head;
 
-        if (m == n)
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
             return true;
     }
 
