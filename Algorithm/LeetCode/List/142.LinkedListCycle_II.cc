e8f429f04be5c52113c790be8dc0bb800d7e88b4
Jemmy <jemmy1029760725@gmail.com>
2021-03-20 19:27:26 +0800

    Update 142.LinkedListCycle_II.cc

--- Algorithm/LeetCode/List/142.LinkedListCycle_II.cc
+++ Algorithm/LeetCode/List/142.LinkedListCycle_II.cc
@@ -1,48 +1,60 @@
-/*
-Difficulty: Medium
-
+/* Medium
 Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
-Note: Do not modify the linked list.
+There is a cycle in a linked list if there is some node in the list that can be reached
+again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
 
-Follow up:
-Can you solve it without using extra space?
+Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+
+Follow up: Can you solve it without using extra space?
+
+Constraints:
+The number of the nodes in the list is in the range [0, 10^4].
+-10^5 <= Node.val <= 10^5
+pos is -1 or a valid index in the linked-list.
 
 Relative:
-141 linked List Cycle
-287 Findthe Duplicate Number
+141. Linked List Cycle
+142. Linked List Cycle II
+287. Findthe Duplicate Number
 
 Approches:
-1. Floyd's tortoise and hare
+1. Floyd's slow and fast
 2. Hash table */
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if (!head) return NULL;
+        if (!head)
+            return nullptr;
 
-        ListNode *hare = head, *tortoise = head;
+        auto* fast = head;
+        auto* slow = head;
+
         do {
-            if (hare->next && hare->next->next) {
-                tortoise = tortoise->next;
-                hare = hare->next->next;
+            if (fast && fast->next) {
+                slow = slow->next;
+                fast = fast->next->next;
             } else {
-                return NULL;
+                return nullptr;
             }
-        } while (tortoise != hare);
+        } while (fast != slow);
 
-        hare = head;
-        while (hare != tortoise) {
-            hare = hare->next;
-            tortoise = tortoise->next;
+        fast = head;
+        while (fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
         }
 
-        return hare;
+        return fast;
     }
 };
