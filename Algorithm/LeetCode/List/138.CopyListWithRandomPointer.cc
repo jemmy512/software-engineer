40259c28285f3b6f1a364168d2f662918313df6b
Jemmy <jemmy1029760725@gmail.com>
2021-03-20 19:04:32 +0800

    Update 138.CopyListWithRandomPointer.cc

--- Algorithm/LeetCode/List/138.CopyListWithRandomPointer.cc
+++ Algorithm/LeetCode/List/138.CopyListWithRandomPointer.cc
@@ -1,14 +1,32 @@
-/*
-Difficulty: Medium
+/* Medium
+A linked list of length n is given such that each node contains an additional random pointer,
+which could point to any node in the list, or null.
 
-A linked list is given such that each node contains an additional random pointer
-which could point to any node in the list or null.
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes,
+where each new node has its value set to the value of its corresponding original node.
+Both the next and random pointer of the new nodes should point to new nodes
+in the copied list such that the pointers in the original list and copied list represent
+the same list state. None of the pointers in the new list should point to nodes in the original list.
 
-Return a deep copy of the list.
-*/
+For example, if there are two nodes X and Y in the original list, where X.random --> Y,
+then for the corresponding two nodes x and y in the copied list, x.random --> y.
 
-/*
- *  The idea as below:
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes.
+Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) that the random pointer points to,
+or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+Constraints:
+0 <= n <= 1000
+-10000 <= Node.val <= 10000
+Node.random is null or is pointing to some node in the linked list. */
+
+/* The idea as below:
  *
  *  Consider we have a linked list as below:
  *
@@ -44,29 +62,23 @@ Return a deep copy of the list.
  *
  *        (node1->next) -> random  = (node1->random) -> next;
  *
- *    3) After we take out all of the "NEW" node to finish the copy.
- *
- */
+ *    3) After we take out all of the "NEW" node to finish the copy. */
 
 struct Node {
     int val;
     Node* next;
     Node* random;
-
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
+    Node(int _val) : val(_val), next(nullptr), random(nullptr) {}
 };
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head)
-            return head;
+        if (!head) {
+            return nullptr;
+        }
 
-        Node* cur, *hd, *tail;
+        Node *cur, *hd, *tail;
 
         cur = head;
         while (cur) {
@@ -78,19 +90,20 @@ public:
 
         cur = head;
         while (cur) {
-            if (cur->random)
+            if (cur->random) {
                 cur->next->random = cur->random->next;
+            }
             cur = cur->next->next;
         }
 
         cur = head;
         hd = tail = head->next;
         while (cur) {
-            cur->next = cur->next->next;
-            if (tail->next)
-                tail->next = tail->next->next;
+            cur->next = tail->next;
+            if (tail->next) {
+                tail = tail->next = tail->next->next;
+            }
             cur = cur->next;
-            tail = tail->next;
         }
 
         return hd;
@@ -126,6 +139,4 @@ public:
  *
  *  4) Then we can clone the random pointers.
  *
- *      new->random = v [ map[node->random] ]
- *
- */
+ *      new->random = v [ map[node->random] ] */
