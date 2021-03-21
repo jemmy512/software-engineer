3a25cf8cd6e40e0498305d2d1e0886268a77ac1d
Jemmy <jemmy1029760725@gmail.com>
2021-03-21 21:57:57 +0800

    Update 136.SingleNumber.cc

--- Algorithm/LeetCode/Math/136.SingleNumber.cc
+++ Algorithm/LeetCode/Math/136.SingleNumber.cc
@@ -1,35 +1,43 @@
-/*
+/* Easy
 Given an array of integers, every element appears twice except for one. Find that single one.
 
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 Tips:
-    The same numbers XOR together will be 0
-    
+The same numbers XOR together will be 0
+
 Reference:
-    https://www.lijinma.com/blog/2014/05/29/amazing-xor/
-    https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary:-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
-    
+https://www.lijinma.com/blog/2014/05/29/amazing-xor/
+https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary:-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
+
 Bit-Wise Operation:
-    Set union A | B
-    Set intersection A & B
-    Set subtraction A & ~B
-    Set negation ALL_BITS ^ A or ~A
-    Set bit A |= 1 << bit
-    Clear bit A &= ~(1 << bit)
-    Test bit (A & 1 << bit) != 0
-    Extract last bit A&-A or A&~(A-1) or x^(x&(x-1))
-    Remove last bit A&(A-1)
-    Get all 1-bits ~0
-    
-    
-*/
+Set union A | B
+Set intersection A & B
+Set subtraction A & ~B
+Set negation ALL_BITS ^ A or ~A
+Set bit A |= 1 << bit
+Clear bit A &= ~(1 << bit)
+Test bit (A & 1 << bit) != 0
+Extract last bit A&-A or A&~(A-1) or x^(x&(x-1))
+Remove last bit A&(A-1)
+Get all 1-bits ~0
+
+Relatives:
+136. Single Number
+137. Single Number II
+260. Single Number III
+268. Missing Number
+287. Find the Duplicate Number
+389. Find the Difference
+645. Set Mismatch */
+
 #include<iostream>
 #include<vector>
 #include<numeric>    // accumulate
 #include<functional> // bit_xor
 #include<assert.h>
+
 using namespace std;
 
 // 14ms, beat 85.23%
@@ -43,13 +51,13 @@ int singleNumber_1(vector<int>& nums) {
     int ret = 0;
     for (int i = 0; i < len; ++i)
         ret = ret ^ nums[i];
-    
+
     return ret;
 }
 
-/******************** there are two single nums ******************************/ 
+/******************** there are two single nums ******************************/
 // get the location of the first 1 in the lower bits of num
-int getFirstOneBit(int num) { 
+int getFirstOneBit(int num) {
     return  num & ~(num - 1);
 }
 
@@ -59,7 +67,7 @@ void singleNumberTwo(vector<int> nums) {
     int b = 0;
     int firstOneBit = 0;
     int len = nums.size();
-    
+
     for (int i = 0; i < len; ++i) {
         aXORb ^= nums[i];
     }
@@ -74,10 +82,10 @@ void singleNumberTwo(vector<int> nums) {
     cout << a << " : " << b << endl;
 }
 
-/******************** there are three single nums *****************************/ 
+/******************** there are three single nums *****************************/
 
 int singleNumberThree(vector<int> nums) {
-    
+
 }
 
 int main() {
