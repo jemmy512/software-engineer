ac6b44686f79230564c0f418d2010a59bfcbde9c
Jemmy <jemmy1029760725@gmail.com>
2021-03-22 23:30:50 +0800

    Update 143.ReorderList.cc

--- Algorithm/LeetCode/List/143.ReorderList.cc
+++ Algorithm/LeetCode/List/143.ReorderList.cc
@@ -1,66 +1,69 @@
-/*
-Given a singly linked list L: L0→L1→…→Ln-1→Ln,
-reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+/* Medium
+Given a singly linked list L: L0->L1->...->Ln-1->Ln,
+reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
 
 You may not modify the values in the list's nodes, only nodes itself may be changed.
 
 Example 1:
-
 Given 1->2->3->4, reorder it to 1->4->2->3.
+
 Example 2:
-
 Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
-*/
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+Constraints:
+The number of nodes in the list is in the range [1, 5 * 10^4].
+1 <= Node.val <= 1000 */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
 public:
-
-    ListNode *findMid(ListNode *head) {
-        ListNode *prev = head, *mid = head, *ret = NULL;
-        while (prev && prev->next)  { // odd, even case
-            ret = mid;
-            mid = mid->next;
-            prev = prev->next->next;
-        }
-
-        return ret;
-    }
-
-    ListNode *reverseList(ListNode *head) {
-        ListNode *tail = NULL, *cur = head, *prev;
-        while (cur) {
-            prev = cur->next;
-            cur->next = tail;
-            tail = cur;
-            cur = prev;
-        }
-
-        return tail;
-    }
-
     void reorderList(ListNode* head) {
-        if (!head || !head->next) return;
+        auto* mid = findMiddle(head);
+        auto* lead = reverseList(mid->next);
+        mid->next = nullptr;
 
-        ListNode *mid = findMid(head);
-        ListNode *hd = reverseList(mid->next);
-        mid->next = NULL;
-        ListNode *tail = head, *prev = NULL, *tailPrev;
-        while (tail && hd) {
-            prev = hd->next;
-            hd->next = tail->next;
-            tail->next = hd;
-            tailPrev = tail->next;
-            tail = tail->next->next;
-            hd = prev;
+        auto* headTail = head;
+        auto* leadTail = lead;
+
+        while (headTail && leadTail) {
+            leadTail = lead->next;
+            lead->next = headTail->next;
+            headTail->next = lead;
+            headTail = lead->next;
+            lead = leadTail;
         }
-        tailPrev->next = hd;
+    }
+
+private:
+    ListNode* findMiddle(ListNode* root) {
+        auto* fast = root;
+        auto* slow = root;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* reverseList(ListNode* root) {
+        auto* fast = root;
+        auto* mid = root;
+        ListNode* slow = nullptr;
+
+        while (mid) {
+            fast = mid->next;
+            mid->next = slow;
+            slow = mid;
+            mid = fast;
+        }
+
+        return slow;
     }
 };
