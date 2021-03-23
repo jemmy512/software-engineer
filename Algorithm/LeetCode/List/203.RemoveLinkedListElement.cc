3604e82a8a6d3a000745c99c392cdf872011166d
Jemmy <jemmy1029760725@gmail.com>
2021-03-23 23:57:15 +0800

    Update 203.RemoveLinkedListElement.cc

--- Algorithm/LeetCode/List/203.RemoveLinkedListElement.cc
+++ Algorithm/LeetCode/List/203.RemoveLinkedListElement.cc
@@ -1,11 +1,23 @@
-/*
+/* Easy
 Remove all elements from a linked list of integers that have value val.
 
 Example
 Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5
-*/
+
+Constraints:
+The number of nodes in the list is in the range [0, 10^4].
+1 <= Node.val <= 50
+0 <= k <= 50
+
+Relatives:
+027. Remove Element
+203. Remove Linked List Element
+237. Delete Node in a Linked List
+283. Move Zeroes */
+
 #include <iostream>
+
 using namespace std;
 
 struct ListNode {
@@ -20,15 +32,15 @@ public:
         // add dummy head to handle the case head's val is equal to val
         ListNode dummy(-1);
         dummy.next = head;
-        ListNode *p = &dummy;
+        ListNode *cur = &dummy;
 
-        while (p->next) {
-            if (p->next->val == val) {
-                auto* tar = p->next;
-                p->next = p->next->next;
+        while (cur->next) {
+            if (cur->next->val == val) {
+                auto* tar = cur->next;
+                cur->next = cur->next->next;
                 delete tar;
             } else {
-                p = p->next;
+                cur = cur->next;
             }
         }
 
