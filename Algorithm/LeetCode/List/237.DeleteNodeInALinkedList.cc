fe028a7211166bf54dc341200448b04d1611d344
Jemmy <jemmy1029760725@gmail.com>
2021-03-23 23:57:18 +0800

    Update 237.DeleteNodeInALinkedList.cc

--- Algorithm/LeetCode/List/237.DeleteNodeInALinkedList.cc
+++ Algorithm/LeetCode/List/237.DeleteNodeInALinkedList.cc
@@ -1,22 +1,36 @@
-/*
-Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+/* Easy
+Write a function to delete a node (except the tail) in a singly linked list,
+given only access to that node.
 
-Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list
-    should become 1 -> 2 -> 4 after calling your function.
-*/
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+the linked list should become 1 -> 2 -> 4 after calling your function.
+
+Relatives:
+027. Remove Element
+203. Remove Linked List Element
+237. Delete Node in a Linked List
+283. Move Zeroes
+
+Constraints:
+The number of the nodes in the given list is in the range [2, 1000].
+-1000 <= Node.val <= 1000
+The value of each node in the list is unique.
+The node to be deleted is in the list and is not a tail node */
+
 #include<iostream>
+
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 class Solution {
 public:
-    static void deleteNode(ListNode* node) {
-         node->val = node->next->val;
-         node->next = node->next->next;
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
     }
 };
