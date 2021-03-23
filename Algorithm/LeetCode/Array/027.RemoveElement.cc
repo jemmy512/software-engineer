df527b79e549f3ce62996000295644bc1602725e
Jemmy <jemmy1029760725@gmail.com>
2021-03-23 23:57:09 +0800

    Update 027.RemoveElement.cc

--- Algorithm/LeetCode/Array/027.RemoveElement.cc
+++ Algorithm/LeetCode/Array/027.RemoveElement.cc
@@ -14,57 +14,58 @@ It doesn't matter what you leave beyond the returned length.
 Example 2:
 Given nums = [0,1,2,2,3,0,4,2], val = 2,
 Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
-Note that the order of those five elements can be arbitrary. */
+Note that the order of those five elements can be arbitrary.
+
+Relatives:
+027. Remove Element
+203. Remove Linked List Element
+237. Delete Node in a Linked List
+283. Move Zeroes */
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int removeElement(vector<int> nums, int val) {
-    if (nums.empty())
-        return 0;
+int removeElement(vector<int>& nums, int val) {
+    int i = 0;
+    int j = nums.size()-1;
 
-    int len = nums.size();
-    int i =0;
-    int j = len - 1;
     while (i <= j) {
-        while (nums[i] != val && i < len) ++i;
-        while (nums[j] == val && j >= 0) --j;
+        while (i < nums.size() && nums[i] != val)
+            ++i;
+        while (j >= 0 && nums[j] == val)
+            --j;
         if (i < j)
             swap(nums[i++], nums[j--]);
     }
+
     return i;
 }
 
-int removeElement(vector<int> nums, int val) {
+int removeElement(vector<int>& nums, int val) {
     int i = 0;
-    int n = nums.size();
-    while (i < n) {
+    int len = nums.size();
+
+    while (i < len) {
         if (nums[i] == val) {
-            nums[i] = nums[n - 1];
-            n--; // reduce array size by one
+            nums[i] = nums[--len];
         } else {
-            i++;
+            ++i;
         }
     }
-    return n;
+
+    return len;
 }
 
-// 26.07%
-int removeElement_1(vector<int> &nums, int val) {
+int removeElement(vector<int>& nums, int val) {
     int pos = 0;
-    for (int i=0; i<nums.size(); i++){
-        if (nums[i] != val)
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != val) {
             nums[pos++] = nums[i];
+        }
     }
+
     return pos;
-}
-
-int main() {
-    cout << removeElement({3, 2, 2, 3}, 3) << endl;
-    cout << removeElement({3, 3}, 3) << endl;
-    cout << removeElement({3}, 3) << endl;
-
-    return 0;
 }
