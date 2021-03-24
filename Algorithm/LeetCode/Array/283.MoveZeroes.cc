ea478f39aa9794dd31343ff4eda9663567924173
Jemmy <jemmy1029760725@gmail.com>
2021-03-24 23:40:39 +0800

    Update 283.MoveZeroes.cc

--- Algorithm/LeetCode/Array/283.MoveZeroes.cc
+++ Algorithm/LeetCode/Array/283.MoveZeroes.cc
@@ -1,46 +1,50 @@
-/*
-Difficulty: Easy
+/* Easy
+Given an array nums, write a function to move
+all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+For example, given nums = [0, 1, 0, 3, 12],
+after calling your function, nums should be [1, 3, 12, 0, 0].
 
 Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 
-*/
+Constraints:
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
 
-// 21 test cases, 16ms, beat 99.04%
+Relatives:
+027. Remove Element
+203. Remove Linked List Element
+237. Delete Node in a Linked List
+283. Move Zeroes */
+
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        size_t free = -1;
-        for (size_t i = 0; i < len; ++i) {
-            if (nums[i] == 0) {
-                if (free == -1)
-                    free = i;
-                continue;
-            } else {
-                if (free != -1)
-                    swap(nums[free++], nums[i]);
+        for (int firstZeroPos = 0, curPos = 0; curPos < nums.size(); ++curPos) {
+            if (nums[curPos] != 0) {
+                swap(nums[firstZeroPos++], nums[curPos]);
             }
         }
     }
 };
 
-// 21 test cases, 17ms, beat 80.04%
-class Solution_ {
+class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int len = nums.size();
-        int j = 0;
-        for (; j < len && nums[j]; ++j);
-        for (int i = 0; i < len; ++i) {
-            if (nums[i] != 0 && i > j) {
-                swap(nums[i], nums[j++]);
-                for (; j < len && nums[j]; ++j);
-            } 
+        int pos = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[pos++] = nums[i];
+            }
         }
+
+        std::fill(nums.begin()+pos, nums.end(), 0);
     }
 };
