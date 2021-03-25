ceb8c37e2fa9f0b4a10ab95daa7a98f251099073
Jemmy <jemmy1029760725@gmail.com>
2021-03-25 19:03:09 +0800

    Update 009.PalindromNumber.cc

--- Algorithm/LeetCode/Math/009.PalindromNumber.cc
+++ Algorithm/LeetCode/Math/009.PalindromNumber.cc
@@ -1,24 +1,35 @@
-/*Difficulty: Easy
-
-Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+/* Easy
+Determine whether an integer is a palindrome.
+An integer is a palindrome when it reads the same backward as forward.
 
 Example 1:
-
 Input: 121
 Output: true
-Example 2:
 
+Example 2:
 Input: -121
 Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
+Explanation: From left to right, it reads -121.
+From right to left, it becomes 121-. Therefore it is not a palindrome.
 
+Example 3:
 Input: 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-Follow up:
 
-Coud you solve it without converting the integer to a string? */
+Follow up:
+Coud you solve it without converting the integer to a string?
+
+Relatives:
+009. Palindrome Number
+125. Valid Palindrome
+234. Palindrome Linked List
+680. Valid Palindrome II */
+
+#include <cmath>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
