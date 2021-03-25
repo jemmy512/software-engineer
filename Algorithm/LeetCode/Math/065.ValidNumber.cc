1135d010e8eb78a20491e0fd94a5cec5de9f6ee7
Jemmy <jemmy1029760725@gmail.com>
2021-03-26 00:26:33 +0800

    234.PalindromeLinkedList.cc

--- Algorithm/LeetCode/Math/065.ValidNumber.cc
+++ Algorithm/LeetCode/Math/065.ValidNumber.cc
@@ -1,74 +1,97 @@
 /* Hard
-Validate if a given sing is numeric.
-Some examples:
-"0" => true
-" 0.1 " => true
-"abc" => false
-"1 a" => false
-"2e10" => true
+A valid number can be split up into these components (in order):
+1. A decimal number or an integer.
+2. (Optional) An 'e' or 'E', followed by an integer.
 
-Note:
-It is intended for the problem statement to be ambiguous.
-You should gather all requirements up front before implementing one.
+A decimal number can be split up into these components (in order):
+1. (Optional) A sign character (either '+' or '-').
+2. One of the following formats:
+    1. At least one digit, followed by a dot '.'.
+    2. At least one digit, followed by a dot '.', followed by at least one digit.
+    3. A dot '.', followed by at least one digit.
 
-Constraints:
-1 <= s.length <= 20
-s consists of only English letters, digits, space ' ', plus '+', minus '-', or dot '.'.
+An integer can be split up into these components (in order):
+1. (Optional) A sign character (either '+' or '-').
+2. At least one digit.
 
-Cases need to care:
-1. digit
-2. point. prevous and next situation
-3. 'e'.   prevous and next situation */
+For example, all the following are valid numbers:
+["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7",
+"+6e-1", "53.5e93", "-123.456e789"],
 
-#include<iostream>
-#include<string>
+while the following are not valid numbers:
+["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"]. */
+
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <utility>
 
 using namespace std;
 
-bool isNumber(string s) {
-    if (s == "" || s == " ")
-        return false;
+class Solution {
+public:
+    bool isNumber(string s) {
+        if (s == "" || s == " ")
+            return false;
 
-    int len = s.size();
-    int i = 0;
-    bool isFloat = false;
-    bool isE = false;
+        int len = s.size();
+        int i = 0;
+        bool hasDot = false;
+        bool hasE = false;
 
-    while (s[i] == ' ')
-        ++i;        // disgard prefix and postfix space
-    if (s[i] == '+' || s[i] == '-')
-        ++i;
-    if (s[i] == 'e')
-        return false;      // "00" ok
-    while (s[len - 1] == ' ')
-        --len;
-
-    int head = i;   // position of the first valid digit
-    while (i < len) {
-        if (isdigit(s[i])) {
+        while (s[i] == ' ')
+            ++i;        // disgard prefix and postfix space
+        if (isSign(s[i]))
             ++i;
-        } else {
-            if (s[i] == 'e') {
-                if (isE || i == head)
-                    return false;   // prefix issue
-                ++i;
-                if (s[i] == '+' ||s[i] == '-')
-                    ++i;
-                if (!isdigit(s[i]))
-                    return false;   // postfix issue
-                isE = true;
-            } else if (s[i++] == '.') {
-                if (isFloat || isE || !isdigit(s[i]) && head == i - 1)
-                    return false;   // prefix and posefix issue
-                isFloat = true;
-            } else
-                return false;
-        }
-    }
-    return true;
-}
+        if (isE(s[i]))
+            return false;      // "00" ok
+        while (s[len - 1] == ' ')
+            --len;
 
-#define TEST(s) cout << "\"" << s << "\"" << " : " << isNumber(s) << endl
+        int head = i;   // position of the first valid digit
+        while (i < len) {
+            if (isdigit(s[i])) {
+                ++i;
+            } else {
+                if (isE(s[i])) {
+                    if (hasE || i == head) {
+                        return false;   // prefix issue
+                    }
+                    ++i;
+                    if (isSign(s[i])) {
+                        ++i;
+                    }
+                    if (!isdigit(s[i])) {
+                        return false;   // postfix issue
+                    }
+                    hasE = true;
+                } else if (isDot(s[i++])) {
+                    if (hasDot || hasE || !isdigit(s[i]) && head == i - 1) {
+                        return false;   // prefix and posefix issue
+                    }
+                    hasDot = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+private:
+    bool isDot(const char& chr) {
+        return chr == '.';
+    }
+
+    bool isE(const char& chr) {
+        return chr == 'e' || chr == 'E';
+    }
+
+    bool isSign(const char& chr) {
+        return chr == '+' || chr == '-';
+    }
+};
+
+#define TEST(s) cout << "\"" << s << "\"" << " : " << std::declval<Solution>().isNumber(s) << endl
 
 int main() {
 
@@ -95,6 +118,5 @@ int main() {
     TEST("6e6.5");
     TEST("1e2e3");
     TEST("05047e+6");
-
-    return 0;
+    TEST("05047E+6");
 }
