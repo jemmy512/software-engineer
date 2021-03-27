97564adfd8a9a3423d220a0b7d550d2dead87b92
Jemmy <jemmy1029760725@gmail.com>
2021-03-27 20:08:57 +0800

    Update 007.ReverseInteger.cc

--- Algorithm/LeetCode/Math/007.ReverseInteger.cc
+++ Algorithm/LeetCode/Math/007.ReverseInteger.cc
@@ -1,30 +1,56 @@
-/*
-Given a 32-bit signed integer, reverse digits of an integer.
+/* Easy
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-2^32, 2^32 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
 Example 1:
-    Input: 123  Output:  321
-    Input: -123 Output: -321
-    Input: 120  Output: 21
-Note:
-Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range.
-For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
-*/
+Input: x = 123
+Output: 321
 
-#include<iostream>
-using namespace std;
+Example 2:
+Input: x = -123
+Output: -321
 
-// 100%
+Example 3:
+Input: x = 120
+Output: 21
+
+Example 4:
+Input: x = 0
+Output: 0
+
+Constraints:
+-2^32 <= x <= 2^32 - 1
+
+Relatives:
+007. Reverse Integer
+008. String to Integer (atoi)
+065. Valid Number
+
+190. Reverse Bits */
+
+#include <climits>
+
+/* Time Complexity: O(log(x)). There are roughly log10(x) digits in x.
+ * Space Complexity: O(1). */
 int reverse(int x) {
-    int rev = 0;
+    int ret = 0;
+
     while (x != 0) {
         int pop = x % 10;
-        // -2147483648 2147483647
-        if (rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7)) return 0;
-        if (rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8)) return 0;
+        // 2147483647 -2147483648
+        if ((ret > INT_MAX/10) || (ret == INT_MAX/10 && pop > 7)
+            || (ret < INT_MIN/10) || (ret == INT_MIN/10 && pop < -8))
+        {
+            return 0;
+        }
+
         x /= 10;
-        rev = rev * 10 + pop;
+        ret = ret * 10 + pop;
     }
-    return rev;
+
+    return ret;
 }
 
 // beat 50.41 cpp submission
@@ -53,9 +79,4 @@ int reverse2(int x) {
         x /= 10;
     }
     return ret;
-}
-
-int main(void) {
-
-    return 0;
 }
