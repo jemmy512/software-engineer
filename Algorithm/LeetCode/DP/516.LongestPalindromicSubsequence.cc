fe0a9ef4537c8bcd7201f2b0d2d2a80fc78c9ff9
Jemmy <jemmy1029760725@gmail.com>
2021-03-28 22:51:50 +0800

    Update 516.LongestPalindromicSubsequence.cc

--- Algorithm/LeetCode/DP/516.LongestPalindromicSubsequence.cc
+++ Algorithm/LeetCode/DP/516.LongestPalindromicSubsequence.cc
@@ -26,3 +26,58 @@ Relatives:
 
 214. Shortest Palindrome
 336. Palindrome Pairs */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        vector dp(s.size(), 0);
+
+        for (int i = s.size()-1; i >= 0; --i) {
+            int j_1 = dp[i];
+            dp[i] = 1;
+            for (int j = i+1; j < s.size(); ++j) {
+                if (s[i] == s[j]) {
+                    int tmp = (j_1 + 2);
+                    j_1 = dp[j];
+                    dp[j] = tmp;
+                } else {
+                    j_1 = dp[j];
+                    dp[j] = max(dp[j-1], dp[j]);
+                }
+            }
+        }
+
+        return dp[s.size()-1];
+    }
+};
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        vector dp(s.size(), vector(s.size(), 0));
+
+        for (int i = s.size()-1; i >= 0; --i) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < s.size(); ++j) {
+                dp[i][j] = (s[i] == s[j])
+                    ? (dp[i+1][j-1] + 2)
+                    : max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[0][s.size()-1];
+    }
+};
