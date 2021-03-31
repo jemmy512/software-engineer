9c76e74269c16c8047f4bb1bd2c4ac156d5ae927
Jemmy <jemmy1029760725@gmail.com>
2021-04-01 00:07:08 +0800

    Update 002.AddTwoNumbers.cc

--- Algorithm/LeetCode/Math/002.AddTwoNumbers.cc
+++ Algorithm/LeetCode/Math/002.AddTwoNumbers.cc
@@ -1,13 +1,65 @@
-/**
- * Difficulty: Medium
- *
- * You are given two non-empty linked lists representing two non-negative integers. The digits
- * are stored in reverse order and each of their nodes contain a single digit. Add the two numbers
- * and return it as a linked list.
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
- *
- * Example
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
- * Output: 7 -> 0 -> 8
- * Explanation: 342 + 465 = 807.
- */
+/* Medium
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+Constraints:
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+Relatives:
+002. Add Two Numbers
+445. Add Two Numbers II
+1634. Add Two Polynomials Represented as Linked Lists */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum = 0;
+        ListNode dummy(0);
+        auto* tail = &dummy;
+
+        while (l1 || l2) {
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            tail = tail->next = new ListNode(sum % 10);
+            sum /= 10;
+        }
+
+        if (sum) {
+            tail = tail->next = new ListNode(sum % 10);
+        }
+
+        return dummy.next;
+    }
+};
