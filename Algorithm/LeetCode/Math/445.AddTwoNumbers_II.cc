63f07223b9b72b53c837f06b57182168848b6740
Jemmy <jemmy1029760725@gmail.com>
2021-04-01 00:07:12 +0800

    Update 445.AddTwoNumbers_II.cc

--- Algorithm/LeetCode/Math/445.AddTwoNumbers_II.cc
+++ Algorithm/LeetCode/Math/445.AddTwoNumbers_II.cc
@@ -1,8 +1,6 @@
-/*
-Difficulty: Medium
-
-You are given two non-empty linked lists representing two non-negative integers. 
-The most significant digit comes first and each of their nodes contain a single digit. 
+/* Medium
+You are given two non-empty linked lists representing two non-negative integers.
+The most significant digit comes first and each of their nodes contain a single digit.
 Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
@@ -11,25 +9,30 @@ Follow up:
 What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
 
 Example:
-
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 8 -> 0 -> 7
-*/
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+Relatives:
+002. Add Two Numbers
+445. Add Two Numbers II
+1634. Add Two Polynomials Represented as Linked Lists */
+
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<ListNode *> one;
-        stack<ListNode *> two;
-        
+        stack<ListNode*> one;
+        stack<ListNode*> two;
+
         while (l1) {
             one.push(l1);
             l1 = l1->next;
@@ -38,9 +41,10 @@ public:
             two.push(l2);
             l2 = l2->next;
         }
-        
+
         int sum = 0;
-        ListNode *nd = new ListNode(0);
+        auto* head = new ListNode(0);
+
         while (!one.empty() || !two.empty()) {
             if (!one.empty()) {
                 sum += one.top()->val;
@@ -50,13 +54,49 @@ public:
                 sum += two.top()->val;
                 two.pop();
             }
-            nd->val = sum % 10;
-            ListNode *head = new ListNode(sum / 10);
-            head->next = nd;
-            nd = head;
+            head->val = sum % 10;
+            auto* node = new ListNode(sum/10);
+            node->next = head;
+            head = node;
             sum /= 10;
         }
-        
-        return (nd->val ? nd : nd->next);
+
+        return (head->val ? head : head->next);
     }
 };
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        auto* head = add(l1, l2);
+        return head->val ? head : head->next;
+    }
+
+private:
+    ListNode* add(ListNode* l1, ListNode* l2) {
+        if (!l1 && !l2)
+            return nullptr;
+
+        auto* next = add(l1 ? l1->next : nullptr, l2 ? l2->next : nullptr);
+        int sum = 0;
+        if (next)
+            sum += next->val;
+        if (l1)
+            sum += l1->val;
+        if (l2)
+            sum += l2->val;
+
+        ListNode* node = nullptr;
+        if (next) {
+            node->val = sum % 10;
+        } else {
+            node = new ListNode(sum % 10);
+        }
+        auto* head = new ListNode(sum / 10);
+        head->next = node;
+
+        return head;
+    }
+};
+
+/* [5] [5] */
