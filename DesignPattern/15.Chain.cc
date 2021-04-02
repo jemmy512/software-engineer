835aacaee2658c151dcc0a776ca272a3a4e2530d
Jemmy <jemmy1029760725@gmail.com>
2021-04-02 19:42:03 +0800

    Update 15.Chain.cc

--- DesignPattern/15.Chain.cc
+++ DesignPattern/15.Chain.cc
@@ -41,4 +41,8 @@ int main() {
 /**
  * Avoid coupling the sender of a request to its receiver by giving more than one object
  * a chance to handle the request. Chain the receiving objects and pass the request
- * along the chain until an object handles it. */
+ * along the chain until an object handles it.
+ *
+ * Applicability:
+ * Implement filter, intercepter.
+ * Let user add new functionality without change the framework. */
