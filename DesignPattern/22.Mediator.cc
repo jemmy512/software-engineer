c52180ac9ffbc5f9a0be22f5ae493dcefc657931
Jemmy <jemmy1029760725@gmail.com>
2021-04-02 16:30:05 +0800

    Update 22.Mediator.cc

--- DesignPattern/22.Mediator.cc
+++ DesignPattern/22.Mediator.cc
@@ -11,4 +11,9 @@ int main() {
 /**
  * Mediator pattern defines a separate (mediator) object that encapsulates the
  * interaction between a set of objects and the objects delegate their interaction
- * to a mediator object instead of interacting with each other directly. */
+ * to a mediator object instead of interacting with each other directly.
+ *
+ * Mediator vs Observer
+ * The interaction between objects in:
+ * Observer is one to many
+ * Mediator is many to many, it's much complex than observer. */
