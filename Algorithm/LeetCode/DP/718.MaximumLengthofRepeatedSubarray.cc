7852b478c29c2ac3afa2ce23dfe3b12bf36a6628
Jemmy <jemmy1029760725@gmail.com>
2021-04-06 23:31:58 +0800

    Create 718.MaximumLengthofRepeatedSubarray.cc

---
+++ Algorithm/LeetCode/DP/718.MaximumLengthofRepeatedSubarray.cc
@@ -0,0 +1,42 @@
+/* Medium
+Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
+
+Example 1:
+Input:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+Output: 3
+Explanation:
+The repeated subarray with maximum length is [3, 2, 1].
+
+Note:
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+
+Relatives:
+209. Minimum Size Subarray Sum
+718. Maximum Length of Repeated Subarray */
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        int maxLen = 0;
+        vector dp(A.size()+1, vector(B.size()+1, 0));
+
+        for (int i = A.size()-1; i >=0; --i) {
+            for (int j = B.size()-1; j >=0; --j) {
+                if (A[i] == B[j]) {
+                    dp[i][j] = dp[i+1][j+1] + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
