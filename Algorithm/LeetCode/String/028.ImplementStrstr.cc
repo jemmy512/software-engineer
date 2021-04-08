5f587309b8886784f846cba2db0e72d45070f000
Jemmy <jemmy1029760725@gmail.com>
2021-04-09 00:02:40 +0800

    Update 028.ImplementStrstr.cc

--- Algorithm/LeetCode/String/028.ImplementStrstr.cc
+++ Algorithm/LeetCode/String/028.ImplementStrstr.cc
@@ -1,61 +1,65 @@
-/*
-Implement strStr().
-
+/* Easy
 Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 Example 1:
-
 Input: haystack = "hello", needle = "ll"
 Output: 2
 Example 2:
 
 Input: haystack = "aaaaa", needle = "bba"
-Output: -1
-*/
+Output: -1 */
+
 #include <string>
 #include <vector>
 #include <iostream>
 
-using std::string;
+using namespace std;
 
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        if (needle.empty())
+    int strStr(string str, string pattern) {
+        if (pattern.empty()) {
             return 0;
+        }
 
-        auto next = getNext(needle);
-        for (int i = 0, j = 0; i < haystack.size(); ++i) {
-            while (j > 0 && haystack[i] != needle[j])
-                j = next[j-1] + 1;
+        const auto matchTable = getMatchTable(pattern);
 
-            if (haystack[i] == needle[j])
+        for (int i = 0, j = 0; i < str.size(); ++i) {
+            while (j > 0 && str[i] != pattern[j]) {
+                j = matchTable[j-1] + 1;
+            }
+            if (str[i] == pattern[j]) {
                 ++j;
-
-            if (j == needle.size())
-                return i-j+1;
+            }
+            if (j == pattern.size()) {
+                return i - j + 1;
+            }
         }
 
         return -1;
     }
 
 private:
-    std::vector<int> getNext(const std::string& needle) {
-        std::vector<int> next(needle.size(), 0);
-
-        next[0] = -1;
-        int k = -1;
-        for (int i = 1; i < needle.size(); ++i) {
-            while (k != -1 && needle[k+1] != needle[i])
-                k = next[k];
-
-            if (needle[k+1] == needle[i])
-                ++k;
-
-            next[i] = k;
+    vector<int> getMatchTable(const string& pattern) {
+        if (pattern.empty()) {
+            return {};
         }
 
-        return next;
+        vector matchTable(pattern.size(), 0);
+        matchTable[0] = -1;
+        int k = -1;
+
+        for (int i = 1; i < pattern.size(); ++i) {
+            while (k != -1 && pattern[k+1] != pattern[i]) {
+                k = matchTable[k];
+            }
+            if (pattern[k+1] == pattern[i]) {
+                ++k;
+            }
+            matchTable[i] = k;
+        }
+
+        return matchTable;
     }
 };
 
