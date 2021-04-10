313ae8e8628816bbbb17dc57fe866612354e9db0
Jemmy <jemmy1029760725@gmail.com>
2021-04-10 18:41:15 +0800

    Update 170.TwoSumIII-DataDtructureDesign.cc

--- Algorithm/LeetCode/Map/170.TwoSumIII-DataDtructureDesign.cc
+++ Algorithm/LeetCode/Map/170.TwoSumIII-DataDtructureDesign.cc
@@ -1,11 +1,13 @@
 /* Easy
-Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
+Design a data structure that accepts a stream of integers
+and checks if it has a pair of integers that sum up to a particular value.
 
 Implement the TwoSum class:
 
 TwoSum() Initializes the TwoSum object, with an empty array initially.
 void add(int number) Adds number to the data structure.
-boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+boolean find(int value) Returns true if there exists any pair of numbers
+whose sum is equal to value, otherwise, it returns false.
 
 
 Example 1:
@@ -25,11 +27,15 @@ twoSum.find(4);  // 1 + 3 = 4, return true
 twoSum.find(7);  // No two integers sum up to 7, return false
 
 Relatives:
-1. Two Sum
+001. Two Sum
 167. Two Sum II - Input array is sorted
 170. Two Sum III - Data structure design
 653. Two Sum IV - Input is a BST
-1214. Two Sum BSTs */
+1214. Two Sum BSTs
+
+015. 3 Sum
+016. 3 Sum Closest
+018. 4 Sum */
 
 #include <unordered_map>
 
