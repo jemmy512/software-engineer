8eb3809807ccd090ab2d03284bc24d6f5337b89b
Jemmy <jemmy1029760725@gmail.com>
2021-04-10 18:41:22 +0800

    Update 016.3SumClosest.cc

--- Algorithm/LeetCode/Math/016.3SumClosest.cc
+++ Algorithm/LeetCode/Math/016.3SumClosest.cc
@@ -1,19 +1,33 @@
 /* Medium
-Given an array nums of n integers and an integer target, find three integers
-    in nums such that the sum is closest to target. Return the sum of the three
-    integers. You may assume that each input would have exactly one solution.
+Given an array nums of n integers and an integer target,
+find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
 
 Example:
 
 Given array nums = [-1, 2, 1, -4], and target = 1.
 
 The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
-*/
-#include<iostream>
-#include<climits>
-#include<algorithm>
-#include<vector>
-#include<iterator>
+
+Relatives:
+001. Two Sum
+167. Two Sum II - Input array is sorted
+170. Two Sum III - Data structure design
+653. Two Sum IV - Input is a BST
+1214. Two Sum BSTs
+
+015. 3 Sum
+016. 3 Sum Closest
+018. 4 Sum */
+
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
 using namespace std;
 
 //solution:  http://en.wikipedia.org/wiki/3SUM
@@ -36,13 +50,14 @@ using namespace std;
 //    [-4,-1,(-1),1,2]  target=2  distance=1
 //
 
-// 125/125 test cases, 10ms, beat  89.25%
 class Solution {
 public:
-    static int threeSumClosest(vector<int>& nums, int target) {
+    int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
+
         int len = nums.size();
         int distance = INT_MAX;
+
         for (int i = 0; i < len-2; ++i) {
             int low = i + 1;
             int high = len - 1;
@@ -67,9 +82,3 @@ public:
         return target - distance;
     }
 };
-
-int main() {
-    // vector<int> vec = {1,2,4,8,16,32,64,128};
-    vector<int> vec = {1, 1, -1, -1, 3};
-    cout << Solution::threeSumClosest(vec, 2) << endl;
-}
