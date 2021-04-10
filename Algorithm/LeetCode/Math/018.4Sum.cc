cee2177ae7ea8f8d2a03616b1e7a97f0629c5645
Jemmy <jemmy1029760725@gmail.com>
2021-04-10 18:41:24 +0800

    Update 018.4Sum.cc

--- Algorithm/LeetCode/Math/018.4Sum.cc
+++ Algorithm/LeetCode/Math/018.4Sum.cc
@@ -1,14 +1,13 @@
-/*
-Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+/* Medium
+Given an array nums of n integers and an integer target,
+are there elements a, b, c, and d in nums such that a + b + c + d = target?
+Find all unique quadruplets in the array which gives the sum of target.
 
 Note:
-
 The solution set must not contain duplicate quadruplets.
 
 Example:
-
 Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
-
 A solution set is:
 [
   [-1,  0, 0, 1],
@@ -16,27 +15,94 @@ A solution set is:
   [-2,  0, 0, 2]
 ]
 
+Constraints:
+1 <= nums.length <= 200
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
+
 Idea:
     1. Sort the vector
     2. Traverse the array and solve the problem by using '3Sum' solution
 
-*/
+Relatives:
+001. Two Sum
+167. Two Sum II - Input array is sorted
+170. Two Sum III - Data structure design
+653. Two Sum IV - Input is a BST
+1214. Two Sum BSTs
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
+015. 3 Sum
+016. 3 Sum Closest
+018. 4 Sum */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-// 282 test cases, 15ms, beat 98.09%
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        return kSum(nums, target, 0, 4);
+    }
+
+    vector<vector<int>> kSum(vector<int>& nums, int target, int beg, int k) {
+        vector<vector<int>> result;
+
+        if (beg == nums.size() || nums[beg] * k > target || target > nums.back() * k)
+            return result;
+        if (k == 2)
+            return twoSum(nums, target, beg);
+
+        for (int i = beg; i < nums.size(); ++i) {
+            if (i == beg || nums[i - 1] != nums[i]) {
+                for (auto &set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    result.push_back({nums[i]});
+                    result.back().insert(result.back().end(), set.begin(), set.end());
+                }
+            }
+        }
+
+        return result;
+    }
+
+    vector<vector<int>> twoSum(vector<int>& nums, int target, int beg) {
+        vector<vector<int>> result;
+        int lo = beg, hi = nums.size() - 1;
+
+        while (lo < hi) {
+            int sum = nums[lo] + nums[hi];
+            if (sum == target) {
+                result.push_back({nums[lo++], nums[hi--]});
+            } else if (sum < target) {
+                ++lo;
+            } else {
+                --hi;
+            }
+
+            while (lo < hi && lo > beg && nums[lo-1] == nums[lo])
+                ++lo;
+            while (lo < hi && hi < nums.size()-1 && nums[hi] == nums[hi+1])
+                --hi;
+        }
+
+        return result;
+    }
+};
+
 class Solution {
 public:
     static vector<vector<int>> fourSum(vector<int> &nums, int target) {
-        vector<vector<int>> result;
+       vector<vector<int>> result;
         int len = nums.size();
         if (len < 4)
             return result;
+
         int sum, low, high;
         sort(nums.begin(), nums.end());
+
         for (int i = 0; i < len - 3; ++i) {
             if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
                 break;
@@ -54,13 +120,11 @@ public:
                 while (low < high) {
                     sum = nums[i] + nums[j] + nums[low] + nums[high];
                     if (sum == target) {
-                        result.push_back({nums[i], nums[j], nums[low], nums[high]});
-                        while (low < high && nums[low] == nums[low + 1])
+                        result.push_back({nums[i], nums[j], nums[low++], nums[high--]});
+                        while (low < high && nums[low-1] == nums[low])
                             ++low;
-                        ++low;
-                        while (low < high && nums[high - 1] == nums[high])
+                        while (low < high && nums[high] == nums[high+1])
                             --high;
-                        --high;
                     } else if (sum > target) {
                         --high;
                     } else {
@@ -77,68 +141,3 @@ public:
         return result;
     }
 };
-
-// 282 test cases, 60ms, beat 15.79%
-class Solution_ {
-public:
-    static vector<vector<int>> threeSum(vector<int> &nums, int target) {
-        int len = nums.size();
-        vector<vector<int>> vec;
-        int sum, low, high;
-        for (int i = 0; i < len - 2; ++i) {
-            low = i + 1;
-            high = len - 1;
-            while (low < high) {
-                sum = nums[i] + nums[low] + nums[high];
-                if (sum == target) {
-                    vector<int> v = {nums[i], nums[low], nums[high]};
-                    vec.push_back(v);
-                    while (low < len - 1 && nums[low] == nums[low + 1])
-                        ++low;
-                    ++low;
-                    while (high > 0 && nums[high - 1] == nums[high])
-                        --high;
-                    --high;
-                } else if (sum > target) {
-                    --high;
-                } else {
-                    ++low;
-                }
-            }
-            while (nums[i] == nums[i + 1])
-                ++i;
-        }
-        return vec;
-    }
-
-    static vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> vec;
-        if (nums.size() < 4)
-            return vec;
-        int len = nums.size();
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < len - 3; ++i) {
-            vector<int> tmp(nums.begin() + i + 1, nums.end());
-            vector<vector<int>> v = threeSum(tmp, target - nums[i]);
-            for (int j = 0; j < v.size(); ++j) {
-                v[j].insert(v[j].begin(), nums[i]);
-                vec.push_back(v[j]);
-            }
-            while (nums[i] == nums[i + 1])
-                ++i;
-        }
-
-        return vec;
-    }
-};
-
-int main() {
-    vector<int> vec = {1,0,-1,0,-2,2};
-    vector<vector<int>> ret = Solution::fourSum(vec, 0);
-    for (auto vec : ret) {
-        for (auto i : vec) {
-            cout << i << ", ";
-        }
-        cout << endl;
-    }
-}
