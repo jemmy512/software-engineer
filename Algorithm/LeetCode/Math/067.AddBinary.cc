dc76927b9ecce92b551352ad08e94478449d4d59
Jemmy <jemmy1029760725@gmail.com>
2021-04-13 23:04:53 +0800

    Update 067.AddBinary.cc

--- Algorithm/LeetCode/Math/067.AddBinary.cc
+++ Algorithm/LeetCode/Math/067.AddBinary.cc
@@ -1,20 +1,32 @@
-/*
+/* Easy
 Given two binary strings, return their sum (also a binary string).
 
 The input strings are both non-empty and contains only characters 1 or 0.
 
 Example 1:
-
 Input: a = "11", b = "1"
 Output: "100"
-Example 2:
 
+Example 2:
 Input: a = "1010", b = "1011"
 Output: "10101"
-*/
-#include<iostream>
-#include<string>
-#include<iterator>
+
+Constraints:
+1 <= a.length, b.length <= 10^4
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+
+Relatives:
+002. Add Two Numbers
+043. Multiply Strings
+066. Plus One
+067. Add Binary
+989. Add to Array-Form of Integer */
+
+#include <iostream>
+#include <string>
+#include <iterator>
+
 using namespace std;
 
 string addBinary(string a, string b) {
@@ -36,12 +48,4 @@ string addBinary(string a, string b) {
         ret = "1" + ret;
 
     return ret;
-}
-
-int main() {
-    string str = addBinary(string("101"), string("1"));
-    cout << "~~~~~~~" << endl;
-    for (char c : str)
-        cout << c;
-    cout << endl;
 }
