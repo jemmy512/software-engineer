abe07035689fcac5bfa5fc44c3ced30352e86af5
Jemmy <jemmy1029760725@gmail.com>
2021-04-13 23:04:56 +0800

    Update 415.AddingStrings.cc

--- Algorithm/LeetCode/String/415.AddingStrings.cc
+++ Algorithm/LeetCode/String/415.AddingStrings.cc
@@ -1,20 +1,43 @@
-/*
-Difficulty: Easy
+/* Easy
+Given two non-negative integers, num1 and num2 represented as string,
+return the sum of num1 and num2 as a string.
 
-Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+Example 1:
+Input: num1 = "11", num2 = "123"
+Output: "134"
 
-Note:
+Example 2:
+Input: num1 = "456", num2 = "77"
+Output: "533"
 
-The length of both num1 and num2 is < 5100.
-Both num1 and num2 contains only digits 0-9.
-Both num1 and num2 does not contain any leading zero.
-You must not use any built-in BigInteger library or convert the inputs to integer directly.
-*/
-#include<iostream>
-#include<string>
+Example 3:
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+Constraints:
+1 <= num1.length, num2.length <= 10^4
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself.
+
+Follow up: Could you solve it without using any built-in BigInteger library
+or converting the inputs to integer directly?
+
+Relatives:
+002. Add Two Numbers
+445. Add Two Numbers II
+415. Add Strings
+043. Multiply Strings
+066. Plus One
+067. Add Binary
+371. Sum of Two Integers
+989. Add to Array-Form of Integer
+1634. Add Two Polynomials Represented as Linked Lists */
+
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-// 312 test cases, 12ms, beat 83.64%
 class Solution {
 public:
     string addStrings(string num1, string num2) {
@@ -24,6 +47,7 @@ public:
         int i = num1.size()-1;
         int j = num2.size()-1;
         int cur = ret.size()-1;
+
         while (i >= 0 || j >=0) {
             int vi = i >= 0 ? num1[i--] - '0' : 0;
             int vj = j >= 0 ? num2[j--] - '0' : 0;
@@ -39,70 +63,4 @@ public:
 
         return ret;
     }
-
-// 315 test cases, 12ms, beat 83.94%
-    string addStrings_2(string num1, string num2) {
-        int len1 = num1.size();
-        int len2 = num2.size();
-
-        string ret = string(max(len1, len2), '0');
-        int cnt = ret.size();
-        int i, j, sum, carry = 0;
-        for (i = len1 - 1, j = len2 - 1; i >=0 && j >= 0; --i, --j) {
-            sum = (num1[i] - '0') + (num2[j] - '0') + carry;
-            carry = sum / 10;
-            sum %= 10;
-            ret[--cnt] = (char)(sum + '0');
-            // ret.insert(ret.begin(), (char)(sum + '0'));
-        }
-
-        i = i > j ? i : j;
-        string str = i > j ? num1 : num2;
-        for (; i >= 0; --i) {
-            sum = (str[i] - '0') + carry;
-            carry = sum / 10;
-            sum %= 10;
-            ret[--cnt] = (char)(sum + '0');
-            // ret.insert(ret.begin(), (char)(sum + '0'));
-        }
-        if (carry)
-            // ret.insert(ret.begin(), (char)(carry + '0'));
-            ret = '1' + ret;
-
-        return ret;
-    }
-
-// 312 test cases, 12ms, beat 83.64%
-    string addStrings_3(string &num1, string &num2) const {
-		string result = "0";
-		int l1 = (int)num1.length(), l2 = (int)num2.size();
-		if (l1 > 0) {
-			if (l2 > 0) {
-				if (l1 != 1 || l2 != 1 || num1[0] != '0' || num2[0] != '0') {
-					int carry = 0, maxLen = max(l1, l2);
-					char *p1s = &num1[0], *p1e = p1s + l1 - 1;
-					char *p2s = &num2[0], *p2e = p2s + l2 - 1;
-					result = string(maxLen, '0');
-					char *r = &result[0], *re = r + maxLen - 1;
-					while (p1e >= p1s || p2e >= p2s) {
-						int v1 = p1e >= p1s ? (*p1e-- - '0') : 0;
-						int v2 = p2e >= p2s ? (*p2e-- - '0') : 0;
-						int r = v1 + v2 + carry;
-						if (r >= 10) { carry = 1; r -= 10; }
-						else { carry = 0; }
-						*re-- = '0' + r;
-					}
-					if (carry) { result = '1' + result; }
-				}
-			}
-			else { result = num1; }
-		}
-		else { result = num2; }
-		return result;
-	};
 };
-
-int main() {
-    Solution sl;
-    cout << sl.addStrings("511", "511") << endl;
-}
