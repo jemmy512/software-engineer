43ef78b1d90a1feb0aa3109e06a2fab3ffdb08f1
Jemmy <jemmy1029760725@gmail.com>
2021-04-21 22:58:49 +0800

    Update 171.ExcelSheetColumNumber.cc

--- Algorithm/LeetCode/Math/171.ExcelSheetColumNumber.cc
+++ Algorithm/LeetCode/Math/171.ExcelSheetColumNumber.cc
@@ -1,23 +1,51 @@
 /* Easy
-Related to question 168.Excel Sheet Column Title
-
-Given a column title as appear in an Excel sheet, return its corresponding column number.
+Given a string columnTitle that represents the column title as appear in an Excel sheet,
+return its corresponding column number.
 
 For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
 
-    A -> 1
-    B -> 2
-    C -> 3
-    ...
-    Z -> 26
-    AA -> 27
-    AB -> 28
-*/
+
+Example 1:
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+Input: columnTitle = "ZY"
+Output: 701
+
+Example 4:
+Input: columnTitle = "FXSHRXW"
+Output: 2147483647
+
+Constraints:
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+
+Relatives:
+168. Excel Sheet Column Title
+171. Excel Sheet Column Number */
+
+#include <string>
+
+using std::string;
 
 class Solution {
 public:
     string convertToNumber(int n) {
-        return base26_str2int(n);
+        return base26_int2str(n);
     }
 
 private:
