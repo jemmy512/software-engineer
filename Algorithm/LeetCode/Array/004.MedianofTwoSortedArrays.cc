d1daa6d23f46aebf83e6d4dbddd8ff467a124290
Jemmy <jemmy1029760725@gmail.com>
2021-04-21 23:19:40 +0800

    Update 004.MedianofTwoSortedArrays.cc

--- Algorithm/LeetCode/Array/004.MedianofTwoSortedArrays.cc
+++ Algorithm/LeetCode/Array/004.MedianofTwoSortedArrays.cc
@@ -1,67 +1,127 @@
-/*
-There are two sorted arrays nums1 and nums2 of size m and n respectively.
-Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-*/
-#include<iostream>
-#include<iterator>
-#include<algorithm>
-#include<vector>
+/* Hard
+Given two sorted arrays nums1 and nums2 of size m and n respectively,
+return the median of the two sorted arrays.
+
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+Example 3:
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+
+Example 4:
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+
+Example 5:
+Input: nums1 = [2], nums2 = []
+Output: 2.00000
+
+Constraints:
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-10^6 <= nums1[i], nums2[i] <= 10^6 */
+
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-// 71ms, beat 66.45%. Run time complicity O(log(min(m, n)))
-double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int m = nums1.size(), n = nums2.size();
-    if (m > n) return findMedianSortedArrays(nums2, nums1);
-    int i, j, imin = 0, imax = m, half = (m + n + 1) / 2;
-    while (imin <= imax) {
-        i = (imin & imax) + ((imin ^ imax) >> 1);
-        j = half - i;
-        if (i > 0 && j < n && nums1[i - 1] > nums2[j]) imax = i - 1;
-        else if (j > 0 && i < m && nums2[j - 1] > nums1[i]) imin = i + 1;
-        else break;
-    }
-    int num1;
-    if (!i) num1 = nums2[j - 1];
-    else if (!j) num1 = nums1[i - 1]; 
-    else num1 = max(nums1[i - 1], nums2[j - 1]);
-    if ((m + n) & 1) return num1;
-    int num2;
-    if (i == m) num2 = nums2[j];
-    else if (j == n) num2 = nums1[i];
-    else num2 = min(nums1[i], nums2[j]);
-    return (num1 + num2) / 2.0;
-}
-
-// 89ms, beat 17.00%. Run time complicity O(m + n)
-double findMedianSortedArrays1(vector<int>& nums1, vector<int>& nums2) {
-    int i = 0;
-    int j = 0;
-    double ret;
-    int len1 = nums1.size();
-    int len2 = nums2.size();
-    
-    vector<int> vec;
-    while (i < len1 && j < len2) {
-        if (nums1[i] < nums2[j]) {
-            vec.push_back(nums1[i++]);
-        } else {
-            vec.push_back(nums2[j++]);
+class Solution {
+public:
+    // O(logN) approch : binary search for partition
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        if (a.size() > b.size()) {
+            swap(a, b);
         }
-    }
-    while (i < len1)
-        vec.push_back(nums1[i++]);
-    while (j < len2)
-        vec.push_back(nums2[j++]);
-    
-    int len = vec.size();
-    if (len % 2)
-        return (double)vec[len / 2];
-    else 
-        return (vec[len / 2] + vec[len / 2 - 1] + 0.0) / 2;
-}
 
-int main() {
-    vector<int> vec = {1, 2};
-    vector<int> ve = {3, 4, 5, 6};
-    cout << findMedianSortedArrays(vec, ve) << endl;
-}
+        int x = a.size();
+        int y = b.size();
+        int low = 0;
+        int high = x;
+
+        while (low <= high) {
+            int px = (low + high) / 2;
+            int py = (x + y + 1) / 2 - px;
+
+            int maxleftx = (px == 0) ? INT_MIN : a[px-1];
+            int minrightx = (px == x) ? INT_MAX : a[px];
+
+            int maxlefty = (py == 0) ? INT_MIN : b[py-1];
+            int minrighty = (py == y) ? INT_MAX : b[py];
+
+            if (maxleftx <= minrighty && maxlefty <= minrightx) {
+                if ((x+y)%2)
+                    return double(max(maxleftx, maxlefty));
+                else
+                    return double(max(maxleftx, maxlefty) + min(minrightx, minrighty))/2;
+            } else if (maxleftx > minrighty) {
+                high = px-1;
+            } else {
+                low = px+1;
+            }
+        }
+
+        return 0;
+    }
+
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
+        int m = A.size();
+        int n = B.size();
+        if (m > n) { // to ensure m<=n
+            std::swap(A, B);
+            std::swap(m, n);
+        }
+
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+
+            if (i < iMax && B[j-1] > A[i]){
+                iMin = i + 1;       // i is too small
+            } else if (i > iMin && A[i-1] > B[j]) {
+                iMax = i - 1;       // i is too big
+            } else {                // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j-1];
+                } else if (j == 0) {
+                    maxLeft = A[i-1];
+                } else {
+                    maxLeft = max(A[i-1], B[j-1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+
+        return 0.0;
+    }
+};
