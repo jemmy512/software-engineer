38373f234fcdb9f55d781daf002641adedc35c23
Jemmy <jemmy1029760725@gmail.com>
2021-04-24 23:04:56 +0800

    Update 215.KthLargestElementinanArray.cc

--- Algorithm/LeetCode/Array/215.KthLargestElementinanArray.cc
+++ Algorithm/LeetCode/Array/215.KthLargestElementinanArray.cc
@@ -1,27 +1,26 @@
-/*
-Difficulty: Easy
-
-Find the kth largest element in an unsorted array. Note that it is the kth largest
- element in the sorted order, not the kth distinct element.
+/* Easy
+Find the kth largest element in an unsorted array. 
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 Example 1:
-    Input: [3,2,1,5,6,4] and k = 2
-    Output: 5
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
 
 Example 2:
-    Input: [3,2,3,1,2,4,5,5,6] and k = 4
-    Output: 4
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+
 Note:
-You may assume k is always valid, 1 ≤ k ≤ array's length.
-*/
-#include<iostream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
+You may assume k is always valid, 1 ≤ k ≤ array's length. */
+
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
 using namespace std;
 
 /******************************* Heap Sort Solution ***************************/
-// 32 test cases, 10ms, beat 98.72%
 void heapifyDown(vector<int> &nums, int b, int e) {
     for (int i = 2 * b + 1; i < e; i = 2 * i + 1) {
         if (i + 1 < e && nums[i + 1] < nums[i])
@@ -48,7 +47,6 @@ int findKthLargest(vector<int>& nums, int k) {
 }
 
 /******************************* STL Build in Solution ************************/
-// 32 test cases, 10ms, beat 98.72%
 int findKthLargest_buildin(vector<int>& nums, int k) {
     int len = nums.size();
     nth_element(nums.begin(), nums.end() - k, nums.end());
@@ -56,7 +54,6 @@ int findKthLargest_buildin(vector<int>& nums, int k) {
 }
 
 /******************************* Qiuck Sort Solution **************************/
-// 32 test cases, 74ms, beat 13.94%
 int partition(vector<int> &nums, int left, int right) {
     int pivot = nums[left];
     int l = left + 1, r = right;
@@ -70,7 +67,7 @@ int partition(vector<int> &nums, int left, int right) {
     return r;
 }
 
-int findKthLargest_qsort(vector<int> &nums) {
+int findKthLargest_qsort(vector<int> &nums, int k) {
     int len = nums.size(), pos;
     int left = 0, right = len - 1;
     while (true) {
@@ -82,10 +79,4 @@ int findKthLargest_qsort(vector<int> &nums) {
         else
             right = pos - 1;
     }
-}
-
-int main() {
-    vector<int> nums = {3,2,5,8,4,7,6,9};
-    findKthLargest(nums, 4);
-    copy(nums.begin(), nums.end(), ostream_iterator<int>(cout, " "));
 }
