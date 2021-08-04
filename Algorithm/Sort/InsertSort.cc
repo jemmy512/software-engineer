8f5d9e358edb6aa0a83a987425c3c8b4fd493f7f
Jemmy <jemmy1029760725@gmail.com>
2021-08-05 00:30:26 +0800

    Update InsertSort.cc

--- Algorithm/Sort/InsertSort.cc
+++ Algorithm/Sort/InsertSort.cc
@@ -6,20 +6,19 @@ using namespace std;
 
 template<typename T>
 void insertSort(T& data) {
-    for (int i = 1; i < data.size(); ++i) {
-        int tmp = data[i];
-        int j = i;
-        while (j > 0 && data[j-1] > tmp) {
-            data[j] = data[j-1];
-            --j;
+    for (auto i = 1; i < data.size(); ++i) {
+        for (auto j = i; j > 0; --j) {
+            if (data[j-1] > data[j]) {
+                std::swap(data[j-1], data[j]);
+            } else {
+                break;
+            }
         }
-
-        data[j] = tmp;
     }
 }
 
 int main(void) {
-    vector<int> vec = {3, 2, 5, 8, 4, 7, 6, 9};
+    vector vec{3, 2, 5, 8, 4, 7, 6, 9};
     insertSort(vec);
     copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
 }
