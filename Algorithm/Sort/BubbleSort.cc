c3e46d5fc048089fe27b7552c6cd3ba5c5f697ee
Jemmy <jemmy1029760725@gmail.com>
2021-08-05 00:30:32 +0800

    Update BubbleSort.cc

--- Algorithm/Sort/BubbleSort.cc
+++ Algorithm/Sort/BubbleSort.cc
@@ -12,18 +12,16 @@
 
 using namespace std;
 
-template<typename T>
-void bubble_sort(array<T, 8> &arr, int n) {
-    bool flag = true;                         // optimization
-    int temp;
-    for (int i = 0; i < n - 1 && flag; ++i) { // control the loop number of bubbles
-        flag = false;
-        for (int j = 0; j < n - 1 - i; ++j) { // control each bubble
-            if (arr[j] > arr[j + 1]) {
-                flag = true;
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+template<typename T, size_t N>
+void bubbleSort(std::array<T, N>& data) {
+    auto isUpdated = true;
+
+    for (auto i = 0; i < data.size() - 1 && isUpdated; ++i) {
+        isUpdated = false;
+        for (auto j = 0; j < data.size() - 1 - i; ++j) {
+            if (data[j] > data[j+1]) {
+                isUpdated = true;
+                std::swap(data[j], data[j+1]);
             }
         }
     }
@@ -31,6 +29,6 @@ void bubble_sort(array<T, 8> &arr, int n) {
 
 int main(void) {
     array<int, 8> arr = {3, 2, 5, 8, 4, 7, 6, 9};
-    bubble_sort(arr, 8);
+    bubbleSort<int, 8>(arr);
     copy(arr.begin(), arr.end(), ostream_iterator<int>(cout, " "));
 }
