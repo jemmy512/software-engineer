d931bb077990642df8827a3748f1313d57e35203
Jemmy <jemmy1029760725@gmail.com>
2021-08-05 08:56:22 +0800

    Update MergeSort.cc

--- Algorithm/Sort/MergeSort.cc
+++ Algorithm/Sort/MergeSort.cc
@@ -5,26 +5,28 @@ using namespace std;
 
 template<typename T>
 void merge(T& data, int beg, int mid, int end) {
-    T tmpData(data.size());
+    T vec(data.size());
     int lbeg = beg, rbeg = mid, i = beg;
 
     while (lbeg < mid && rbeg < end)
-        tmpData[i++] = data[lbeg] < data[rbeg] ? data[lbeg++] : data[rbeg++];
+        vec[i++] = data[lbeg] < data[rbeg] ? data[lbeg++] : data[rbeg++];
 
     while (lbeg < mid)
-        tmpData[i++] = data[lbeg++];
+        vec[i++] = data[lbeg++];
 
     while (rbeg < end)
-        tmpData[i++] = data[rbeg++];
+        vec[i++] = data[rbeg++];
 
     for (i = beg; i < end; ++i)
-        data[i] = tmpData[i];
+        data[i] = vec[i];
 }
 
 template<typename T>
 void mergeSort(T& data, int beg, int end) {
-    int mid = beg + (end - beg) / 2; // (beg + end) may overflow
-    if (beg != mid) {
+    // if (beg < end) [0, 1) can cause dead loop
+
+    int mid = beg + (end - beg) / 2;
+    if (beg < mid) {
         mergeSort(data, beg, mid);
         mergeSort(data, mid, end);
         merge(data, beg, mid, end);
@@ -32,9 +34,7 @@ void mergeSort(T& data, int beg, int end) {
 }
 
 int main(void) {
-    vector<float> data = {3.0, 2.3, 5.5, 8.3, 4.4, 7.6, 6.6, 9.8};
+    vector data{3, 5, 2, 3, 8, 4, 7, 5, 3, 6, 7, 2, 5, 7};
     mergeSort(data, 0, data.size());
-    copy(data.begin(), data.end(), ostream_iterator<float>(cout, " "));
-
-    return 0;
+    copy(data.begin(), data.end(), ostream_iterator<int>(cout, " "));
 }
