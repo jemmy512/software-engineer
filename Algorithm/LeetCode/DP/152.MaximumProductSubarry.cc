c57cf71002e8decf13a96f007593504bdf09db91
Jemmy <jemmy1029760725@gmail.com>
2021-08-07 19:47:30 +0800

    Update 152.MaximumProductSubarry.cc

--- Algorithm/LeetCode/DP/152.MaximumProductSubarry.cc
+++ Algorithm/LeetCode/DP/152.MaximumProductSubarry.cc
@@ -20,26 +20,40 @@ Explanation: The result cannot be 2, because [-2,-1] is not a subarray. */
 // when we are at element -2, the maximum product is multiplication of, minimum product ending with -6 and -2.
 
 /* Relatives:
+* 053. Maximum Subarray
 * 121. Best Time to Buy and Sell Stock
 * 152. Maximum Product Subarry
+* 198. House Robber
+* 238. Product of Array Except Self
 * 437. Path Sum III
-* 560. Sub array Sum Equals K */
+* 560. Sub array Sum Equals K
+* 628. Maximum Product of Three Numbers
+* 713. Subarray Product Less Than K
+
+Constraints:
+1 <= nums.length <= 2 * 10^4
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer. */
 
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int len = nums.size();
-        int maxPro = nums[0];
-        int minPro = nums[0];
-        int maxRet = nums[0];
-        for (int i = 1; i < len; ++i) {
-            if (nums[i] < 0)
+       if (nums.empty())
+            return 0;
+
+        auto maxPro = nums[0];
+        auto minPro = nums[0];
+        auto result = nums[0];
+
+        for (auto i = 1; i < nums.size(); ++i) {
+            if (nums[i] < 0) {
                 swap(maxPro, minPro);
+            }
             maxPro = max(nums[i], maxPro *= nums[i]);
             minPro = min(nums[i], minPro *= nums[i]);
-            maxRet = max(maxRet, maxPro);
+            result = max(result, maxPro);
         }
 
-        return maxRet;
+        return result;
     }
 };
