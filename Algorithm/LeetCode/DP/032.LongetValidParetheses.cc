6020dae6364b14e53ec0f7ebde33eadd43fd4b8b
Jemmy <jemmy1029760725@gmail.com>
2021-08-07 14:04:40 +0800

    Update 032.LongetValidParetheses.cc

--- Algorithm/LeetCode/DP/032.LongetValidParetheses.cc
+++ Algorithm/LeetCode/DP/032.LongetValidParetheses.cc
@@ -49,18 +49,20 @@ public:
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int maxans = 0;
+        int result = 0;
         int dp[s.size()];
+
         for (int i = 1; i < s.size(); i++) {
             if (s[i] == ')') {
-                if (s[i - 1] == '(') {
+                if (s[i - 1] == '(') { // ...()
                     dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-                } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') {
+                } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') { // ...(...)((...))
                     dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
                 }
-                maxans = max(maxans, dp[i]);
+                result = max(result, dp[i]);
             }
         }
-        return maxans;
+
+        return result;
     }
 };
