af338bd864bfa1bf90feb30435c56057e88de579
Jemmy <jemmy1029760725@gmail.com>
2021-08-15 00:45:17 +0800

    Update 718.MaximumLengthofRepeatedSubarray.cc

--- Algorithm/LeetCode/DP/718.MaximumLengthofRepeatedSubarray.cc
+++ Algorithm/LeetCode/DP/718.MaximumLengthofRepeatedSubarray.cc
@@ -22,6 +22,8 @@ Relatives:
 
 using namespace std;
 
+/* top down
+ * dp[i][j] = dp[i+1][j+1] + 1 */
 class Solution {
 public:
     int findLength(vector<int>& A, vector<int>& B) {
@@ -32,6 +34,27 @@ public:
             for (int j = B.size()-1; j >=0; --j) {
                 if (A[i] == B[j]) {
                     dp[i][j] = dp[i+1][j+1] + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+/* bottom up
+ * dp[i][j] = dp[i-1][j-1] + 1 */
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        int maxLen = 0;
+        vector dp(A.size()+1, vector(B.size()+1, 0));
+
+        for (auto i = 1; i <= A.size(); ++i) {
+            for (auto j = 1; j <= B.size(); ++j) {
+                if (A[i-1] == B[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
                     maxLen = max(maxLen, dp[i][j]);
                 }
             }
