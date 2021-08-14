2619f62ef6c80e9dd8e936447cc218dc126447a6
Jemmy <jemmy1029760725@gmail.com>
2021-08-15 00:45:27 +0800

    Update 371.SumofTwoIntegers.cc

--- Algorithm/LeetCode/Math/371.SumofTwoIntegers.cc
+++ Algorithm/LeetCode/Math/371.SumofTwoIntegers.cc
@@ -24,7 +24,7 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while (b != 0) {
-            int carry = unsigned(a & b) << 1;
+            auto carry = unsigned(a & b) << 1;
             a = a ^ b;
             b = carry;
         }
