88fcc325674cc0d4a0142457e5337b9366fa17dc
Jemmy <jemmy1029760725@gmail.com>
2021-08-15 00:45:23 +0800

    Update 002.AddTwoNumbers.cc

--- Algorithm/LeetCode/Math/002.AddTwoNumbers.cc
+++ Algorithm/LeetCode/Math/002.AddTwoNumbers.cc
@@ -1,9 +1,9 @@
 /* Medium
 You are given two non-empty linked lists representing two non-negative integers.
 The digits are stored in reverse order, and each of their nodes contains a single digit.
-Add the two numbers and return the sum as a linked list.
+Add the two numbers and return the carry as a linked list.
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+You may ascarrye the two numbers do not contain any leading zero, except the number 0 itself.
 
 Example 1:
 Input: l1 = [2,4,3], l2 = [5,6,4]
@@ -45,25 +45,25 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum = 0;
+        int carry = 0;
         ListNode dummy(0);
         auto* tail = &dummy;
 
         while (l1 || l2) {
             if (l1) {
-                sum += l1->val;
+                carry += l1->val;
                 l1 = l1->next;
             }
             if (l2) {
-                sum += l2->val;
+                carry += l2->val;
                 l2 = l2->next;
             }
-            tail = tail->next = new ListNode(sum % 10);
-            sum /= 10;
+            tail = tail->next = new ListNode(carry % 10);
+            carry /= 10;
         }
 
-        if (sum) {
-            tail = tail->next = new ListNode(sum % 10);
+        if (carry) {
+            tail = tail->next = new ListNode(carry);
         }
 
         return dummy.next;
