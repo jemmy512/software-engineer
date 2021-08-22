a82b4fc47b4d6c8e644167476a471499c840e33b
Jemmy <jemmy1029760725@gmail.com>
2021-08-22 11:38:28 +0800

    Update 222.CountCompleteTreeNodes.cc

--- Algorithm/LeetCode/Tree/222.CountCompleteTreeNodes.cc
+++ Algorithm/LeetCode/Tree/222.CountCompleteTreeNodes.cc
@@ -1,17 +1,13 @@
-/*
-Difficulty: Medium
-
+/* Medium
 Given a complete binary tree, count the number of nodes.
 
 Note:
-
 Definition of a complete binary tree from Wikipedia:
 In a complete binary tree every level, except possibly the last, is completely filled,
-    and all nodes in the last level are as far left as possible. It can have between
-    1 and 2h nodes inclusive at the last level h.
+and all nodes in the last level are as far left as possible. 
+It can have between 1 and 2h nodes inclusive at the last level h.
 
 Example:
-
 Input:
     1
    / \
@@ -19,32 +15,46 @@ Input:
  / \  /
 4  5 6
 
-Output: 6
-*/
+Output: 6 */
 
-// 18 test cases, 81ms, beat 72.31%
+#include <optional>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    //return -1 if it is not.
-    int isCompleteTree(TreeNode* root) {
-        if (!root) return 0;
+    std::optional<int> getCompleteTreeCnt(TreeNode* root) {
+        if (!root) 
+            return {};
 
         int cnt = 1;
         TreeNode *left = root, *right = root;
-        for(; left && right; left = left->left, right = right->right) {
+        for (; left && right; left = left->left, right = right->right) {
             cnt *= 2;
         }
 
-        if (left!=NULL || right!=NULL) {
-            return -1;
+        if (left || right) {
+            return {};
         }
-        return cnt-1;
+        return {cnt-1};
     }
 
     int countNodes(TreeNode* root) {
-        int cnt = isCompleteTree(root);
-        if (cnt != -1) return cnt;
+        if (!root)
+            return 0;
 
+        if (const auto cnt = getCompleteTreeCnt(root); cnt) {
+            return cnt.value();
+        }
+        
         int leftCnt = countNodes(root->left);
         int rightCnt = countNodes(root->right);
         return leftCnt + rightCnt + 1;
