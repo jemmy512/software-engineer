84fec6ce5b83493f09c083a82a9a7e6ceac07f34
Jemmy <jemmy1029760725@gmail.com>
2021-08-30 23:16:27 +0800

    Update 226.InvertBinaryTree.cc

--- Algorithm/LeetCode/Tree/226.InvertBinaryTree.cc
+++ Algorithm/LeetCode/Tree/226.InvertBinaryTree.cc
@@ -23,7 +23,7 @@ This problem was inspired by this original tweet by Max Howell:
 Google: 90% of our engineers use the software you wrote (Homebrew),
 but you can’t invert a binary tree on a whiteboard so f*** off. */
 
-#include <que>
+#include <queue>
 
 using namespace std;
 
