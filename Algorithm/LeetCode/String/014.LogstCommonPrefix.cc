48da68934a2c44de0d1d8bff87023dc59d8283ee
Jemmy <jemmy1029760725@gmail.com>
2021-09-11 16:36:36 +0800

    Update 014.LogstCommonPrefix.cc

--- Algorithm/LeetCode/String/014.LogstCommonPrefix.cc
+++ Algorithm/LeetCode/String/014.LogstCommonPrefix.cc
@@ -1,5 +1,4 @@
-/**
- * Difficulty: Easy
+/* Easy
  *
  * Write a function to find the longest common prefix string amongst an array of strings.
  * If there is no common prefix, return an empty string "".
@@ -13,8 +12,8 @@
  * Output: ""
  * Explanation: There is no common prefix among the input strings.
  * Note:
- * All given inputs are in lowercase letters a-z.
- */
+ * All given inputs are in lowercase letters a-z. */
+
 #include <string>
 #include <vector>
 
@@ -26,17 +25,15 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty())
             return "";
-
-        int len = strs.size();
-        int le = strs[0].size();
-        for (int i = 0; i < le; ++i) {
-            for (int j = 1; j < len; ++j) {
+        
+        for (auto i = 0; i < strs[0].size(); ++i) {
+            for (auto j = 1; j < strs.size(); ++j) {
                 if (strs[0][i] != strs[j][i]) {
                     return strs[0].substr(0, i);
                 }
             }
         }
-
+        
         return strs[0];
     }
 };
