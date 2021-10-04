add286e0900fb691447fee50a6576d4204021861
Jemmy <jemmy1029760725@gmail.com>
2021-10-04 23:32:16 +0800

    Update 238.ProductOfArrayExceptSelf.cc

--- Algorithm/LeetCode/Array/238.ProductOfArrayExceptSelf.cc
+++ Algorithm/LeetCode/Array/238.ProductOfArrayExceptSelf.cc
@@ -12,7 +12,12 @@ Note: Please solve it without division and in O(n).
 
 Follow up:
 Could you solve it with constant space complexity?
-(The output array does not count as extra space for the purpose of space complexity analysis.) */
+(The output array does not count as extra space for the purpose of space complexity analysis.) 
+
+Relatives:
+238. Product of Array Except Self 
+239. Sliding Window Maximum
+123. Best Time to Buy and Sell Stock III */
 
 #include <vector>
 
