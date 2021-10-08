de17c79903ba062bc04ff3d70f315ebf9c42c21d
Jemmy <jemmy1029760725@gmail.com>
2021-10-08 22:22:47 +0800

    Update 034.SearchForaRange.cc

--- Algorithm/LeetCode/Search/034.SearchForaRange.cc
+++ Algorithm/LeetCode/Search/034.SearchForaRange.cc
@@ -1,27 +1,37 @@
-/**
-Difficulty: Medium
-
-Given an array of integers nums sorted in ascending order, find the starting
- and ending position of a given target value.
+/* Medium
+Given an array of integers nums sorted in ascending order,
+find the starting and ending position of a given target value.
 
 Your algorithm's runtime complexity must be in the order of O(log n).
 
 If the target is not found in the array, return [-1, -1].
 
 Example 1:
-
 Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
-Example 2:
 
+Example 2:
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 
-Solution:   Binary Search
+Solution: Binary Search */
 
-*/
+#include <vector>
+
+using namespace std;
 
 class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int low = extremeIndex(nums, target, true);
+        if (low == nums.size() || nums[low] != target)
+            return {-1, -1};
+
+        int high = extremeIndex(nums, target, false) - 1;
+
+        return {low, high};
+    }
+
 private:
     int extremeIndex(vector<int> &nums, int target, bool left) {
         int low = 0;
@@ -36,17 +46,6 @@ private:
         }
 
         return low;
-    }
-
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int low = extremeIndex(nums, target, true);
-        if (low == nums.size() || nums[low] != target)
-            return {-1, -1};
-
-        int high = extremeIndex(nums, target, false) - 1;
-
-        return {low, high};
     }
 };
 
