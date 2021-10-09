94d1fa5284e1a496d1455c6a062e499ab4c58087
Jemmy <jemmy1029760725@gmail.com>
2021-10-09 22:39:00 +0800

    Update 323.NumberofConnectedComponentsinanUndirectedGraph.cc

--- Algorithm/Search/323.NumberofConnectedComponentsinanUndirectedGraph.cc
+++ Algorithm/Search/323.NumberofConnectedComponentsinanUndirectedGraph.cc
@@ -1,6 +1,8 @@
 /* Medium
-Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
-write a function to find the number of connected components in an undirected graph.
+You have a graph of n nodes. You are given an integer n and an array edges 
+where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
 
 Example 1:
 Input: n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
@@ -21,6 +23,14 @@ Note:
 You can assume that no duplicate edges will appear in edges.
 Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
 
+Constraints:
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
+
 Relatives:
 130. Surrounded Regions
 200. Number of Islands
@@ -39,21 +49,21 @@ using namespace std;
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-    vector<int> roots(n);
-    for (int i = 0; i < n; ++i)
-        roots[i] = i;
+        vector<int> roots(n);
+        for (int i = 0; i < n; ++i)
+            roots[i] = i;
 
-    for (const auto& e : edges) {
-        int root1 = find(roots, e[0]);
-        int root2 = find(roots, e[1]);
-        if (root1 != root2) {
-            roots[root1] = root2;  // union
-            --n;
+        for (const auto& e : edges) {
+            int root1 = find(roots, e[0]);
+            int root2 = find(roots, e[1]);
+            if (root1 != root2) {
+                roots[root1] = root2;  // union
+                --n;
+            }
         }
-    }
 
-    return n;
-}
+        return n;
+    }
 
 private:
     int find(vector<int>& roots, int id) {
@@ -64,40 +74,47 @@ private:
     }
 };
 
-// not work
+// DFS
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        visited = vector<bool>(n, false);
+        if (n == 0)
+            return 0;
 
-        for (const auto& node : edges) {
-            hashMap[node[0]].emplace_back(node[1]);
+        _Visited = vector<bool>(n, false);
+        _Adjacency = vector<vector<int>>(n);
+
+        for (const auto& edge : edges) {
+            _Adjacency[edge[0]].emplace_back(edge[1]);
+            _Adjacency[edge[1]].emplace_back(edge[0]);
         }
 
-        for (const auto& node : edges) {
-            if (visited[node[0]] == false) {
-                dfs(edges, node[0]);
-                ++count;
+        int result = 0;
+
+        for (auto i = 0; i < n; ++i) {
+            if (_Visited[i] == false) {
+                ++result;
+                dfs(i);
             }
         }
 
-        return count;
+        return result;
     }
-
+    
 private:
-    void dfs(const vector<vector<int>>& edges, int node) {
-        if (hashMap.find(node) != hashMap.end() && !visited[node]) {
-            for (const auto& point : hashMap[node]) {
-                dfs(edges, point);
+    void dfs(int idx) {
+        _Visited[idx] = true;
+        
+        for (auto i = 0; i < _Adjacency[idx].size(); ++i) {
+            if (_Visited[_Adjacency[idx][i]] == false) {
+                dfs(_Adjacency[idx][i]);
             }
         }
-        visited[node] = true;
     }
-
+    
 private:
-    int count{0};
-    vector<bool> visited;
-    unordered_map<int, vector<int>> hashMap;
+    vector<bool> _Visited;
+    vector<vector<int>> _Adjacency;
 };
 
 /*
