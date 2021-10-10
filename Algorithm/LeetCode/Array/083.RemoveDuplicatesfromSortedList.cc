7ac13e8541fe93a05a2e63d5a753ded7b0c3552e
Jemmy <jemmy1029760725@gmail.com>
2021-10-10 22:30:53 +0800

    Update 083.RemoveDuplicatesfromSortedList.cc

--- Algorithm/LeetCode/Array/083.RemoveDuplicatesfromSortedList.cc
+++ Algorithm/LeetCode/Array/083.RemoveDuplicatesfromSortedList.cc
@@ -3,7 +3,21 @@ Given a sorted linked list, delete all duplicates such that each element appear 
 
 For example,
 Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3. */
+Given 1->1->2->3->3, return 1->2->3. 
+
+Relatives:
+019. Remove Nth Node From End of List
+027. Remove Element
+026. Remove Duplicates from Sorted Array
+080. Remove Duplicates from Sorted Array II
+083. Remove Duplicates from Sorted List
+082. Remove Duplicates from Sorted List II 
+203. Remove Linked List Elements
+237. Delete Node In A Linked List
+283. Move Zeroes
+1474. Delete N Nodes After M Nodes of a Linked List
+1721. Swapping Nodes in a Linked List 
+1836. Remove Duplicates From an Unsorted Linked List */
 
 struct ListNode {
     int val;
