a33b4c02a0aed86486298207ab7d9e0d57306a83
Jemmy <jemmy1029760725@gmail.com>
2021-10-12 00:05:43 +0800

    Update 347.TopKFrequentElements.cc

--- Algorithm/LeetCode/Sort/347.TopKFrequentElements.cc
+++ Algorithm/LeetCode/Sort/347.TopKFrequentElements.cc
@@ -13,10 +13,25 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
-You can return the answer in any order. */
+You can return the answer in any order.
+
+Constraints:
+1 <= nums.length <= 10^5
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+
+Relatives:
+215. Kth Largest Element in an Array
+324. Wiggle Sort II
+347. Top K Frequent Elements
+414. Third Maximum Number
+703. Kth Largest Element in a Stream
+973. K Closest Points to Origin
+1985. Find the Kth Largest Integer in the Array */
 
 #include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
@@ -28,22 +43,71 @@ public:
             ++hashMap[n];
         }
 
+        auto comp = [&hashMap](const auto& lhs, const auto& rhs) {
+            return hashMap[lhs] > hashMap[rhs];
+        };
+
+        priority_queue<int, vector<int>, decltype(comp)> heap(comp);
+
+        for (const auto& [num, cnt] : hashMap) {
+            heap.push(num);
+            if (heap.size() > k)
+                heap.pop();
+        }
+
+        vector<int> top;
+        top.reserve(k);
+
+        while (!heap.empty()) {
+            top.emplace_back(heap.top());
+            heap.pop();
+        }
+
+        return top;
+    }
+};
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> top;
+        top.reserve(k);
+
+        _HashMap.clear();
+        for (const auto& n : nums) {
+            ++_HashMap[n];
+        }
+
+        for (const auto& [num, cnt] : _HashMap) {
+            if (top.size() < k) {
+                top.emplace_back(num);
+                heapifyDown(top, top.size()/2, top.size());
+            } else {
+                if (cnt > _HashMap[top[0]]) {
+                    top[0] = num;
+                    heapifyDown(top, 0, top.size());
+                }
+            }
+        }
+
+        return top;
     }
 
 private:
-    template<typename Iter,
-        typename T = std::iterator_traits<Iter>::value_type,
-        typename Compare = std::less<T>>
-    void heapifyDown(Iter beg, Iter cur, Iter end, Compare comp) {
-        auto parent = cur;
-        auto child = cur;
+    void heapifyDown(vector<int>& nums, int beg, int end) {
+        for (auto parent = beg, child = 2 * parent + 1; child < end; child = 2 * child + 1) {
+            if (child + 1 < end && _HashMap[nums[child+1]] < _HashMap[nums[child]])
+                ++child;
 
-        for (std::advance(child, std::distance(beg, child)+1);
-            child < end;
-            std::advance(child, std::distance(beg, child) + 1))
-        {
-            if (child + 1 < end && comp(*(child+1), *child))
-                child += 1;
+            if (_HashMap[nums[parent]] <= _HashMap[nums[child]])
+                break;
+
+            swap(nums[parent], nums[child]);
+            parent = child;
         }
     }
+
+private:
+    unordered_map<int, int> _HashMap;
+
 };
