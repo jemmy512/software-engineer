bc0349a4249e2d5afc07c05346632ceda82f5a3c
Jemmy <jemmy1029760725@gmail.com>
2021-10-14 23:51:30 +0800

    219.ContainsDuplicate_II.cc

---
+++ Algorithm/LeetCode/Array/219.ContainsDuplicate_II.cc
@@ -0,0 +1,29 @@
+/* Easy
+Given an array of integers and an integer k, find out whether there are
+two distinct indices i and j in the array such that nums[i] = nums[j] and the
+absolute difference between i and j is at most k.
+
+Constraints:
+1 <= nums.length <= 10^5
+-109 <= nums[i] <= 10^9
+0 <= k <= 10^5 */
+
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> hashMap;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (hashMap.find(nums[i]) != hashMap.end() && i - hashMap[nums[i]] <= k)
+                return true;
+            hashMap[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
