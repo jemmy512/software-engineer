dc78baa34cc884630d4b94fcf6aae5de3a993c85
Jemmy <jemmy1029760725@gmail.com>
2021-10-17 23:19:58 +0800

    Update 053.MaximumSubarray.cc

--- Algorithm/LeetCode/DP/053.MaximumSubarray.cc
+++ Algorithm/LeetCode/DP/053.MaximumSubarray.cc
@@ -1,15 +1,20 @@
 /* Easy
-Given an integer array nums, find the contiguous subarray (containing at least
-one number) which has the largest sum and return its sum.
+Given an integer array nums, find the contiguous subarray
+(containing at least one number) which has the largest sum and return its sum.
 
 Example:
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
-Follow up:
 
+Follow up:
 If you have figured out the O(n) solution, try coding another solution using the
 divide and conquer approach, which is more subtle.
+
+Constraints:
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
 
 Relatives:
 053. Maximum Subarray
@@ -37,6 +42,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxSum = nums[0];
+
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i-1] > 0)
                 nums[i] += nums[i-1];
