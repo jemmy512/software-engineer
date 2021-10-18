986f837fa6c54e43513c2ed290055927bc5c13f6
Jemmy <jemmy1029760725@gmail.com>
2021-10-18 22:57:07 +0800

    Update 666.PathSumIV.cc

--- Algorithm/LeetCode/Tree/666.PathSumIV.cc
+++ Algorithm/LeetCode/Tree/666.PathSumIV.cc
@@ -2,15 +2,14 @@
 If the depth of a tree is smaller than 5, then this tree can be represented by a list of three-digits integers.
 
 For each integer in this list:
-
 The hundreds digit represents the depth D of this node, 1 <= D <= 4.
 
-The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8. 
+The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8.
 The position is the same as that in a full binary tree.
 
 The units digit represents the value V of this node, 0 <= V <= 9.
 
-Given a list of ascending three-digits integers representing a binary tree with the depth smaller than 5, 
+Given a list of ascending three-digits integers representing a binary tree with the depth smaller than 5,
 you need to return the sum of all paths from the root towards the leaves.
 
 It's guaranteed that the given list represents a valid connected binary tree.
@@ -34,7 +33,23 @@ The tree that the list represents is:
     3
      \
       1
-The path sum is (3 + 1) = 4. */
+The path sum is (3 + 1) = 4.
+
+Constraints:
+1 <= nums.length <= 15
+110 <= nums[i] <= 489
+nums represents a valid binary tree with depth less than 5.
+
+Relatives:
+053. Maximum Subarray
+112. Path Sum
+113. Path Sum II
+437. Path Sum III
+666. Path Sum IV
+124. Binary Tree Maximum Path Sum
+129. Sum Root to Leaf Numbers
+687. Longest Univalue Path
+1376. Time Needed to Inform All Employees */
 
 #include <vector>
 #include <unordered_map>
@@ -44,6 +59,8 @@ using namespace std;
 class Solution {
 public:
     int pathSum(vector<int>& nums) {
+        _HashMap.reserve(nums.size());
+
         for (const auto& num : nums) {
             _HashMap[num/10] = num % 10;
         }
@@ -65,7 +82,7 @@ private:
         int left = 10 * (depth + 1) + 2 * pos - 1;
         int right = left + 1;
 
-        if (!_HashMap.contains(left) || !_HashMap.contains(right)) {
+        if (!_HashMap.contains(left) && !_HashMap.contains(right)) {
             _Sum += curSum;
         } else {
             dfs(left, curSum);
