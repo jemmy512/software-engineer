5f79f56afe61b4051164ddbefec22792d4a4ded2
Jemmy <jemmy1029760725@gmail.com>
2021-10-23 22:25:38 +0800

    Update 019.RemoveNthNodeFromEndofList.cc

--- Algorithm/LeetCode/List/019.RemoveNthNodeFromEndofList.cc
+++ Algorithm/LeetCode/List/019.RemoveNthNodeFromEndofList.cc
@@ -2,17 +2,20 @@
 Given a linked list, remove the n-th node from the end of list and return its head.
 
 Example:
-
 Given linked list: 1->2->3->4->5, and n = 2.
-
 After removing the second node from the end, the linked list becomes 1->2->3->5.
-Note:
 
+Note:
 Given n will always be valid.
 
 Follow up:
-
 Could you do this in one pass?
+
+Constraints:
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
 
 Relative:
 019. Remove Nth Node From End of List
@@ -20,56 +23,58 @@ Relative:
 026. Remove Duplicates from Sorted Array
 080. Remove Duplicates from Sorted Array II
 083. Remove Duplicates from Sorted List
-082. Remove Duplicates from Sorted List II 
+082. Remove Duplicates from Sorted List II
 203. Remove Linked List Elements
 237. Delete Node In A Linked List
 283. Move Zeroes
 1474. Delete N Nodes After M Nodes of a Linked List
-1721. Swapping Nodes in a Linked List 
+1721. Swapping Nodes in a Linked List
 1836. Remove Duplicates From an Unsorted Linked List */
 
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // beat 35.03%
 ListNode* removeNthFromEnd(ListNode* head, int n) {
-    ListNode *prev = head;
-    ListNode *last = head;
+    auto* fast = head;
+    auto* slow = head;
 
-    while (prev != NULL) {
-        prev = prev->next;
-        if (n-- < 0) {
-            last = last->next;
+    while (fast != nullptr) {
+        fast = fast->next;
+        if (n-- < 0) { // n-- get previous node of the node to be deleted
+            slow = slow->next;
         }
     }
 
-    if (n)
-        last->next = last->next->next;
-    else
+    if (n == 0) // the node to be deleted is head
         head = head->next;
+    else
+        slow->next = slow->next->next;
 
     return head;
 }
 
 // beat 5.87%
 ListNode* removeNthFromEnd_1(ListNode* head, int n) {
-    ListNode *m = head;
-    ListNode *l = head;
+    auto* fast = head;
+    auto* slow = head;
 
-    while (m != NULL && n) {
-        m = m->next;
+    while (fast != nullptr && n) {
+        fast = fast->next;
         --n;
     }
-    while (m != NULL && m->next != NULL) {
-        m = m->next;
-        l = l->next;
+    while (fast != nullptr && fast->next != nullptr) {
+        fast = fast->next;
+        slow = slow->next;
     }
-    if (m == NULL)
+
+    if (fast == nullptr)
         head = head->next;
     else
-        l->next = l->next->next;
+        slow->next = slow->next->next;
+
     return head;
 }
