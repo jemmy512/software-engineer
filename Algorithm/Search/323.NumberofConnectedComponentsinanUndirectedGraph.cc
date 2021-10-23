78ff78caa50a849c098b09b656e8fafcaa6f480b
Jemmy <jemmy1029760725@gmail.com>
2021-10-23 17:56:15 +0800

    Update 323.NumberofConnectedComponentsinanUndirectedGraph.cc

--- Algorithm/Search/323.NumberofConnectedComponentsinanUndirectedGraph.cc
+++ Algorithm/Search/323.NumberofConnectedComponentsinanUndirectedGraph.cc
@@ -49,29 +49,53 @@ using namespace std;
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        vector<int> roots(n);
-        for (int i = 0; i < n; ++i)
-            roots[i] = i;
-
-        for (const auto& e : edges) {
-            int root1 = find(roots, e[0]);
-            int root2 = find(roots, e[1]);
-            if (root1 != root2) {
-                roots[root1] = root2;  // union
-                --n;
-            }
-        }
-
-        return n;
+        init(n, edges);
+        doUnion(edges);
+        
+        return _Count;
     }
 
 private:
-    int find(vector<int>& roots, int id) {
-        while (roots[id] != id) {
-            id = roots[id] = roots[roots[id]];  // optional: path compression
+    void init(int n, const vector<vector<int>>& edges) {
+        _Count = n;
+        _Parent = vector(n, 0);
+        _Size = vector(n, 1);
+
+        for (auto i = 0; i < n; ++i) {
+            _Parent[i] = i;
+        }
+    }
+
+    void doUnion(const const vector<vector<int>>& edges) {
+        for (const auto& edge : edges) {
+            auto rootX = find(edge[0]);
+            auto rootY = find(edge[1]);
+
+            if (rootX != rootY) {
+                if (_Size[rootX] > _Size[rootY]) {
+                    _Parent[rootY] = rootX;
+                    _Size[rootX] += _Size[rootY];
+                } else {
+                    _Parent[rootX] = rootY;
+                    _Size[rootY] += _Size[rootX];
+                }
+
+                --_Count;
+            }
+        }
+    }
+
+    int find(int id) {
+        while (_Parent[id] != id) {
+            id = _Parent[id] = _Parent[_Parent[id]];  // optional: path compression
         }
         return id;
     }
+
+private:
+    int _Count;
+    vector<int> _Parent;
+    vector<int> _Size;
 };
 
 // DFS
