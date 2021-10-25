8c7acfc2f5a989c9080189e66a5760f8b1ba438a
Jemmy <jemmy1029760725@gmail.com>
2021-10-26 01:14:19 +0800

    Update 111.MinimumDepthofBinaryTree.cc

--- Algorithm/LeetCode/Tree/111.MinimumDepthofBinaryTree.cc
+++ Algorithm/LeetCode/Tree/111.MinimumDepthofBinaryTree.cc
@@ -19,6 +19,7 @@ Given binary tree [3,9,20,null,null,15,7],
      8
 return its minimum depth = 3 [3, 9, 8]. */
 
+#include <queue>
 #include <algorithm>
 
 using namespace std;
@@ -30,17 +31,60 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+/* dfs */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+        return dfs(root);
+    }
+
+private:
+    int dfs(TreeNode* root) {
         if (!root)
             return 0;
 
         if (!root->left)
-            return minDepth(root->right) + 1;
+            return dfs(root->right) + 1;
         else if (!root->right)
-            return minDepth(root->left) + 1;
+            return dfs(root->left) + 1;
         else
-            return min(minDepth(root->left), minDepth(root->right)) + 1;
+            return min(dfs(root->left), dfs(root->right)) + 1;
+    }
+};
+
+/* bfs */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        return bfs(root);
+    }
+
+private:
+    int bfs(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int depth = 1;
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while (!que.empty()) {
+            auto size = que.size();
+            for (auto i = 0; i < size; ++i) {
+                auto* node = que.front();
+                que.pop();
+
+                if (!node->left && !node->right)
+                    return depth;
+                if (node->left)
+                    que.push(node->left);
+                if (node->right)
+                    que.push(node->right);
+            }
+
+            ++depth;
+        }
+
+        return depth;
     }
 };
