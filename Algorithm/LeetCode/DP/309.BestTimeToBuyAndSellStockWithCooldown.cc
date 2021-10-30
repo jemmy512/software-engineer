0641f15659577228c0e7b74a4016b40013a3c2b3
Jemmy <jemmy1029760725@gmail.com>
2021-10-30 15:35:12 +0800

    Create 309.BestTimeToBuyAndSellStockWithCooldown.cc

---
+++ Algorithm/LeetCode/DP/309.BestTimeToBuyAndSellStockWithCooldown.cc
@@ -0,0 +1,72 @@
+/* Medium
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like
+(i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+
+Example 1:
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+Example 2:
+Input: prices = [1]
+Output: 0
+
+Constraints:
+1 <= prices.length <= 5000
+0 <= prices[i] <= 1000
+
+Relatives:
+121. Best Time to Buy and Sell Stock
+122. Best Time to Buy and Sell Stock II
+123. Best Time to Buy and Sell Stock III
+188. Best Time to Buy and Sell Stock IV
+309. Best Time to Buy and Sell Stock with Cooldown
+714. Best Time to Buy and Sell Stock with Transaction Fee */
+
+#include <vector>
+
+using namespace std;
+
+/* unified solution */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector(2, 0));
+
+        for (auto i = 0; i < prices.size(); ++i) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+            } else if (i == 1) {
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+                dp[i][1] = max(dp[i-1][1], -prices[i]);
+            } else {
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+                dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i]);
+            }
+        }
+
+        return dp[prices.size()-1][0];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int dp_i_0 = 0;
+        int dp_i_1 = INT_MIN;
+        int dp_pre_0 = 0; // dp[i-2][0]
+
+        for (const auto price : prices) {
+            auto temp = dp_i_0;
+            dp_i_0 = max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = max(dp_i_1, dp_pre_0 - price);
+            dp_pre_0 = temp;
+        }
+
+        return dp_i_0;
+    }
+};
