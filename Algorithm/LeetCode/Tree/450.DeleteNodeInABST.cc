d758c6c18b8b104730d157192728fb13d851c132
Jemmy <jemmy1029760725@gmail.com>
2021-11-02 00:11:22 +0800

    Create 450.DeleteNodeInABST.cc

---
+++ Algorithm/LeetCode/Tree/450.DeleteNodeInABST.cc
@@ -0,0 +1,73 @@
+/* Medium
+Given a root node reference of a BST and a key,
+delete the node with the given key in the BST.
+Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+Example 3:
+
+Input: root = [], key = 0
+Output: []
+
+Constraints:
+The number of nodes in the tree is in the range [0, 10^4].
+-10^5 <= Node.val <= 10^5
+Each node has a unique value.
+root is a valid binary search tree.
+-10^5 <= key <= 10^5 */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root)
+            return nullptr;
+
+        if (root->val == key) {
+            if (!root->left || !root->right) {
+                return root->left ? root->left : root->right;
+            } else {
+                auto minNode = getMin(root->right);
+                root->val = minNode->val;
+                root->right = deleteNode(root->right, minNode->val);
+            }
+        } else if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            root->left = deleteNode(root->left, key);
+        }
+
+        return root;
+    }
+
+private:
+    TreeNode* getMin(TreeNode* node) {
+        while (node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+};
