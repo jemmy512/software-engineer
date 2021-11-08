79b098947fba224cc2412edaa8401fab7e7159f2
Jemmy <jemmy1029760725@gmail.com>
2021-11-08 23:55:12 +0800

    Update 083.RemoveDuplicatesfromSortedList.cc

--- Algorithm/LeetCode/Array/083.RemoveDuplicatesfromSortedList.cc
+++ Algorithm/LeetCode/Array/083.RemoveDuplicatesfromSortedList.cc
@@ -3,7 +3,7 @@ Given a sorted linked list, delete all duplicates such that each element appear 
 
 For example,
 Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3. 
+Given 1->1->2->3->3, return 1->2->3.
 
 Relatives:
 019. Remove Nth Node From End of List
@@ -11,12 +11,12 @@ Relatives:
 026. Remove Duplicates from Sorted Array
 080. Remove Duplicates from Sorted Array II
 083. Remove Duplicates from Sorted List
-082. Remove Duplicates from Sorted List II 
+082. Remove Duplicates from Sorted List II
 203. Remove Linked List Elements
 237. Delete Node In A Linked List
 283. Move Zeroes
 1474. Delete N Nodes After M Nodes of a Linked List
-1721. Swapping Nodes in a Linked List 
+1721. Swapping Nodes in a Linked List
 1836. Remove Duplicates From an Unsorted Linked List */
 
 struct ListNode {
@@ -30,39 +30,20 @@ struct ListNode {
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        auto* cur = head;
-
-        while (cur && cur->next) {
-            if (cur->val == cur->next->val) {
-                cur->next = cur->next->next;
-            } else {
-                cur = cur->next;
-            }
-        }
-
-        return head;
-    }
-};
-
-class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
         if (!head)
             return nullptr;
 
-        auto* tail = head;
-        auto* cur = head->next;
+        auto* slow = head;
+        auto* fast = head;
 
-        while (cur) {
-            if (cur->val != tail->val) {
-                tail = tail->next = cur;
+        while (fast) {
+            if (fast->val != slow->val) {
+                slow = slow->next = fast;
             }
-            cur = cur->next;
+            fast = fast->next;
         }
-
-        tail->next = nullptr;
+        slow->next = nullptr;
 
         return head;
-
     }
 };
