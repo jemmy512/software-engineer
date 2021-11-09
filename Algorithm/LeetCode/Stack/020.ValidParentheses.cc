42992ef6bc37237bcb1e202dcbe326e08acd3717
Jemmy <jemmy1029760725@gmail.com>
2021-11-10 01:03:14 +0800

    Update 020.ValidParentheses.cc

--- Algorithm/LeetCode/Stack/020.ValidParentheses.cc
+++ Algorithm/LeetCode/Stack/020.ValidParentheses.cc
@@ -1,17 +1,26 @@
 /* Easy
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
- * determine if the input string is valid.
- * The brackets must close in the correct order, "()" and "()[]{}" are all valid
- * but "(]" and "([)]" are not
- *
- * Relatives:
- * 017. Letter Combination of a Phone Number
- * 020. validParentheses
- * 022. Generate Parentheses*/
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+The brackets must close in the correct order, "()" and "()[]{}" are all valid
+but "(]" and "([)]" are not
 
-#include <iostream>
-#include <iterator>
-#include <unordered_map>
+Relatives:
+0017. Letter Combination of a Phone Number
+0020. Valid Parentheses
+0022. Generate Parentheses
+0032. Longest Valid Parentheses
+0241. Different Ways to Add Parentheses
+0301. Remove Invalid Parentheses
+0856. Score of Parentheses
+0921. Minimum Add to Make Parentheses Valid
+1021. Remove Outermost Parentheses
+1111. Maximum Nesting Depth of Two Valid Parentheses Strings
+1190. Reverse Substrings Between Each Pair of Parentheses
+1249. Minimum Remove to Make Valid Parentheses
+1541. Minimum Insertions to Balance a Parentheses String
+1614. Maximum Nesting Depth of the Parentheses */
+
+#include <string>
 #include <stack>
 
 using namespace std;
@@ -20,53 +29,26 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        
+
         for (const auto& chr : s) {
-            if (_HashMap.find(chr) != _HashMap.end()) {
-                const auto top = stk.empty() ? '#' : stk.top();
-                if (top != _HashMap.at(chr)) {
-                    return false;
-                } else if (top == '#') {
-                    stk.push(chr);
-                } else {
-                    stk.pop();
-                }
+            if (chr == '(' || chr == '{' || chr == '[') {
+                stk.emplace(chr);
+            } else if (!stk.empty() && leftOf(chr) == stk.top()) {
+                stk.pop();
             } else {
-                stk.push(chr);
+                return false;
             }
         }
-        
+
         return stk.empty();
     }
-    
+
 private:
-    const unordered_map<char, char> _HashMap {
-      {')', '('},
-      {'}', '{'},
-      {']', '['}  
-    };
-};
-
-class Solution {
-public:
-    bool isValid(string s) {
-        std::stack<char> stack;
-
-        for (const auto& chr : s) {
-            if (stack.empty()) {
-                stack.push(chr);
-            } else {
-                if (stack.top() == '[' && chr == ']')
-                    stack.pop();
-                else if (stack.top() == '{' && chr == '}')
-                    stack.pop();
-                else if (stack.top() == '(' && chr == ')')
-                    stack.pop();
-                else
-                    stack.push(chr);
-            }
+    char leftOf(char chr) {
+        switch (chr) {
+            case '}': return '{';
+            case ')': return '(';
+            default:  return '[';
         }
-
-        return stack.empty();
     }
 };
