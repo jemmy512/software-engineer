5d4e4828334883f05946fd95dd5163777df6ab28
Jemmy <jemmy1029760725@gmail.com>
2021-11-13 20:16:52 +0800

    Update 134.GasStation.cc

--- Algorithm/LeetCode/Greedy/134.GasStation.cc
+++ Algorithm/LeetCode/Greedy/134.GasStation.cc
@@ -1,12 +1,12 @@
 /* Medium
 There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
 
-You have a car with an unlimited gas tank and it costs cost[i] of gas 
-to travel from the ith station to its next (i + 1)th station. 
+You have a car with an unlimited gas tank and it costs cost[i] of gas
+to travel from the ith station to its next (i + 1)th station.
 You begin the journey with an empty tank at one of the gas stations.
 
-Given two integer arrays gas and cost, return the starting gas station's index 
-if you can travel around the circuit once in the clockwise direction, 
+Given two integer arrays gas and cost, return the starting gas station's index
+if you can travel around the circuit once in the clockwise direction,
 otherwise return -1. If there exists a solution, it is guaranteed to be unique
 
 
@@ -38,7 +38,7 @@ Constraints:
 gas.length == n
 cost.length == n
 1 <= n <= 10^5
-0 <= gas[i], cost[i] <= 10^4 
+0 <= gas[i], cost[i] <= 10^4
 
 Key point: if can't go to j from i, then any node between [i, j) can not go to j */
 
@@ -65,5 +65,27 @@ public:
         }
 
         return tankSum < 0 ? -1 : start % gas.size();
+    }
+};
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        auto sum = 0;
+        auto sumMin = 0;
+        auto start = 0;
+
+        for (auto i = 0; i < gas.size(); ++i) {
+            sum += gas[i] - cost[i];
+            if (sum < sumMin) {
+                start = i + 1;
+                sumMin = sum;
+            }
+        }
+
+        if (sum < 0)
+            return -1;
+
+        return start % gas.size();
     }
 };
