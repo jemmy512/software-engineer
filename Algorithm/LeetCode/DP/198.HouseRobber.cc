e12a950f7419a278be9df80a145014c2614f1185
Jemmy <jemmy1029760725@gmail.com>
2021-11-27 22:43:19 +0800

    Update 198.HouseRobber.cc

--- Algorithm/LeetCode/DP/198.HouseRobber.cc
+++ Algorithm/LeetCode/DP/198.HouseRobber.cc
@@ -7,6 +7,10 @@ contact the police if two adjacent houses were broken into on the same night.
 Given a list of non-negative integers representing the amount of money of each house,
 determine the maximum amount of money you can rob tonight without alerting the police.
 
+Constraints:
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+
 Dynamic Programming
 We can easy find the recurive fomulary:
 
@@ -42,58 +46,52 @@ Relatives:
 
 using namespace std;
 
-/* Acutally, we no need to allocate an additional array for DP.
- * we can only use several variables to record previous steps */
-int rob2(vector<int> &money) {
-    std::vector dp{0, 0};
+class Solution {
+public:
+    int rob(vector<int>& money) {
+        int dp_i_1 = 0;
+        int dp_i_2 = 0;
 
-    for (const auto& mon : money) {
-        auto curMax = std::max(dp[1], dp[0] + mon);
-        dp[0] = dp[1];
-        dp[1] = curMax;
-    }
-
-    return dp[1];
-}
-
-// 69 test 2ms, beat 100%
-int rob1(vector<int> &money) {
-
-    int n = money.size();
-    if (n ==0 ) return 0;
-
-    vector<int> dp(n, 0);
-    if (n >= 1) dp[0] = money[0];
-    if (n >= 2) dp[1] = max(money[0], money[1]);
-
-    for (int i = 2; i < n; ++i){
-        dp[i] = max(dp[i-1], dp[i-2] + money[i]);
-    }
-    return dp[n-1];
-}
-
-/* Combination usage of rob1 and robs will beat 100%,
- * but single usage of rob1 or rob2 just beat 73.93% */
-int rob(vector<int> &num) {
-    if (rand()%2)
-        return rob1(num);
-    return rob2(num);
-}
-
-/******************************************************************************/
-// 69 test cases, 3ms, beat 73.93%
-int rob(vector<int>& num) {
-    int n = num.size();
-    int a = 0;
-    int b = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0)
-            a = max(a + num[i], b);
-        } else {
-            b = max(a, b + num[i]);
+        for (const auto mon : money) {
+            auto curMax = std::max(dp_i_1, mon + dp_i_2);
+            dp_i_2 = dp_i_1;
+            dp_i_1 = curMax;
         }
-    }
 
-    return max(a, b);
-}
+        return dp_i_1;
+    }
+};
+
+/* dont need to handle special case: i == 1, i == 2
+ * rob money in order of i-1, i-2, i-3 ... 0 */
+class Solution {
+public:
+    int rob(vector<int>& money) {
+        const auto size = money.size();
+        vector dp(size+2, 0);
+
+        for (int i = size - 1; i >= 0; --i) {
+            dp[i] = max(dp[i+1], money[i] + dp[i+2]);
+        }
+
+        return dp[0];
+    }
+};
+
+// need to handle special cases: i == 1, i == 2
+class Solution {
+public:
+    int rob(vector<int>& money) {
+        const auto size = money.size();
+
+        vector<int> dp(size, 0);
+        if (size >= 1) dp[0] = money[0];
+        if (size >= 2) dp[1] = max(money[0], money[1]);
+
+        for (int i = 2; i < size; ++i){
+            dp[i] = max(dp[i-1], dp[i-2] + money[i]);
+        }
+
+        return dp[size-1];
+    }
+};
