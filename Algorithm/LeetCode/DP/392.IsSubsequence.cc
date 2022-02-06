f9159bf5410fbdc3941f81e35a35bf60bd60c142
Jemmy <jemmy1029760725@gmail.com>
2022-02-06 16:39:40 +0800

    Update 392.IsSubsequence.cc

--- Algorithm/LeetCode/DP/392.IsSubsequence.cc
+++ Algorithm/LeetCode/DP/392.IsSubsequence.cc
@@ -40,6 +40,21 @@ using namespace std;
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j])
+                ++i;
+            ++j;
+        }
+
+        return i == s.size();
+    }
+};
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
         int beg = 0;
 
         for (const auto& chr : s) {
