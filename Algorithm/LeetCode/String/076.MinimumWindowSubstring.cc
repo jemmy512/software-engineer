e5e9e2fd964e75273d692c6e709054534fe2d2a3
Jemmy <jemmy1029760725@gmail.com>
2022-02-06 18:01:36 +0800

    Update 076.MinimumWindowSubstring.cc

--- Algorithm/LeetCode/String/076.MinimumWindowSubstring.cc
+++ Algorithm/LeetCode/String/076.MinimumWindowSubstring.cc
@@ -30,56 +30,59 @@ n == t.length
 1 <= m, n <= 10^5
 s and t consist of uppercase and lowercase English letters.
 
-Follow up: Could you find an algorithm that runs in O(m + n) time? */
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+Rletiveness:
+030. Substring with Concatenation of All Words
+209. Minimum Size Subarray Sum
+239. Sliding Window Maximum
+567. Permutation in String
+632. Smallest Range Covering Elements from K Lists
+727. Minimum Window Subsequence */
 
 #include <climits>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    string minWindow(string src, string tar) {
+    string minWindow(string s, string t) {
+        int minLen = INT_MAX;
+        string_view retView;
+        int beg = 0, end = 0;
+        int windowSize = 0;
         unordered_map<char, int> dict, window;
-        dict.reserve(tar.size());
-        window.reserve(tar.size());
 
-        for (char chr : tar) {
+        for (auto chr : t) {
             ++dict[chr];
         }
 
-        int beg = 0, end = 0;
-        int winSize = 0;
-        int start = 0, len = INT_MAX;
-
-        while (end < src.size()) {
-            char chr = src[end];
-            ++end;
+        while (end < s.size()) {
+            auto chr = s[end++];
             if (dict.count(chr)) {
-                ++window[chr];
-                if (window[chr] == dict[chr]) {
-                    ++winSize;
+                if (++window[chr] == dict[chr]) {
+                    ++windowSize;
                 }
             }
 
-            while (winSize == dict.size()) {
-                if (end - beg < len) {
-                    start = beg;
-                    len = end - beg;
+            while (windowSize == dict.size()) {
+                if (end - beg < minLen) {
+                    minLen = end - beg;
+                    retView = string_view(&s[beg], end-beg);
                 }
 
-                chr = src[beg];
-                ++beg;
+                auto chr = s[beg++];
                 if (dict.count(chr)) {
-                    if (window[chr] == dict[chr]) {
-                        --winSize;
+                    if (window[chr]-- == dict[chr]) {
+                        --windowSize;
                     }
-                    --window[chr];
                 }
             }
         }
 
-        return len == INT_MAX ? "" : src.substr(start, len);
+        return string(retView);
     }
 };
