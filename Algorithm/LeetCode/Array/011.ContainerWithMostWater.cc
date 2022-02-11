320e34901f462dfc78277d3cfcd63b72f2ed396e
Jemmy <jemmy1029760725@gmail.com>
2022-02-11 19:51:02 +0800

    Update 011.ContainerWithMostWater.cc

--- Algorithm/LeetCode/Array/011.ContainerWithMostWater.cc
+++ Algorithm/LeetCode/Array/011.ContainerWithMostWater.cc
@@ -1,10 +1,18 @@
 /* Medium
-Given n non-negative integers a1, a2, ..., an, where each represents a point
- at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
- of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis
- forms a container, such that the container contains the most water.
+Given n non-negative integers a1, a2, ..., an,
+where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+Find two lines, which together with x-axis forms a container,
+such that the container contains the most water.
 
-Note: You may not slant the container and n is at least 2. */
+Note: You may not slant the container and n is at least 2.
+
+Relativeness:
+011. Container With Most Water
+042. Trapping Rain Water
+407. Trapping Rain Water II
+238. Product of Array Except Self
+755. Pour Water */
 
 #include <iostream>
 #include <algorithm>
