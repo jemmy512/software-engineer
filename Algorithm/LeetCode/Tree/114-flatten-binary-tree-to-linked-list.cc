049e109814b0580a44b41d18c008f5916422c1f3
Jemmy <jemmy1029760725@gmail.com>
2022-02-20 17:43:38 +0800

    Update 114-flatten-binary-tree-to-linked-list.cc

--- Algorithm/LeetCode/Tree/114-flatten-binary-tree-to-linked-list.cc
+++ Algorithm/LeetCode/Tree/114-flatten-binary-tree-to-linked-list.cc
@@ -33,7 +33,37 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode sentinel(0, nullptr, root);
+        dfs(&sentinel);
+    }
+
+private:
+    TreeNode* dfs(TreeNode* node) {
+        if (!node)
+            return nullptr;
+
+        if (!node->left && !node->right)
+            return node;
+
+        auto* leftTail = dfs(node->left);
+        auto* rightTail = dfs(node->right);
+
+        if (leftTail) {
+            leftTail->right = node->right;
+            node->right = node->left;
+            node->left = nullptr;
+        }
+
+        return rightTail ? rightTail : leftTail;
+    }
 };
 
 class Solution {
