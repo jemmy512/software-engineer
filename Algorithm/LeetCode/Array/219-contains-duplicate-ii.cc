ca2a41a237d9575d75980a011b122bfdf9e1d5a3
Jemmy <jemmy1029760725@gmail.com>
2022-04-09 20:12:17 +0800

    Update 219-contains-duplicate-ii.cc

--- Algorithm/LeetCode/Array/219-contains-duplicate-ii.cc
+++ Algorithm/LeetCode/Array/219-contains-duplicate-ii.cc
@@ -10,8 +10,29 @@ Constraints:
 
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> hashSet;
+
+        for (auto i = 0; i < nums.size(); ++i) {
+            if (hashSet.count(nums[i]))
+                return true;
+
+            hashSet.emplace(nums[i]);
+
+            if (hashSet.size() > k) {
+                hashSet.erase(nums[i-k]);
+            }
+        }
+
+        return false;
+    }
+};
 
 class Solution {
 public:
