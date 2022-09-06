66a82a5a302e8c3979198ad5e70566dba3250200
Jemmy <jemmy1029760725@gmail.com>
2022-09-06 22:35:07 +0800

    404-sum-of-left-leaves.cc

--- Algorithm/LeetCode/Tree/404-sum-of-left-leaves.cc
+++ Algorithm/LeetCode/Tree/404-sum-of-left-leaves.cc
@@ -1,6 +1,4 @@
-/*
-Difficulty: Easy
-
+/* Easy
 Find the sum of all left leaves in a given binary tree.
 
 Example:
@@ -11,37 +9,35 @@ Example:
     /  \
    15   7
 
-There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
-*/
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24. */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    void leftCount(TreeNode *root, int lr, int &ret) {
-        // lr indicate left or ri
+    void leftCount(TreeNode *root, bool isLeft, int& ret) {
+        if (!root)
+            return;
 
-        if (!root->left && !root->right && lr)
+        if (isLeft && !root->left && !root->right) {
             ret += root->val;
-        if (root->left) 
-            leftCount(root->left, 1, ret);
-        if (root->right) 
-            leftCount(root->right, 0, ret);
+        }
+
+        leftCount(root->left, true, ret);
+        leftCount(root->right, false, ret);
     }
-    
+
     int sumOfLeftLeaves(TreeNode* root) {
         if (!root) return 0;
-        
+
         int ret = 0;
         leftCount(root, 0, ret);
-        
+
         return ret;
     }
 };
